356c9cd23ebf4953af169f38126fd521b49ca6c4
hhursev <hhursev@gmail.com>
2015-09-14 18:35:40 +0300

    Add docstring to the methods structure in the abstract class

--- recipe_scrapers/_abstract.py
+++ recipe_scrapers/_abstract.py
@@ -13,22 +13,29 @@ class AbstractScraper():
             self.soup = BeautifulSoup(request.urlopen(url).read(), "html.parser")
 
     def host(self):
+        """ get the host of the url, so we can use the correct scraper (check __init__.py) """
         raise NotImplementedError("This should be implemented.")
 
     def publisher_site(self):
+        """ the original url of the publisher site """
         raise NotImplementedError("This should be implemented.")
 
     def title(self):
+        """ title of the recipe itself """
         raise NotImplementedError("This should be implemented.")
 
     def total_time(self):
+        """ total time it takes to preparate the recipe in minutes """
         raise NotImplementedError("This should be implemented.")
 
     def ingredients(self):
+        """ list of ingredients needed for the recipe """
         raise NotImplementedError("This should be implemented.")
 
     def directions(self):
+        """ directions provided on the recipe link """
         raise NotImplementedError("This should be implemented.")
 
     def social_rating(self):
+        """ social rating of the recipe in 0 - 100 scale """
         raise NotImplementedError("This should be implemented.")
