b6d08abf7bc4aafaeec59944bdcdf8ae4a9352d5
hhursev <hhursev@gmail.com>
2015-09-15 23:14:28 +0300

    Update time_regex captcher so to work with more sites

--- recipe_scrapers/consts.py
+++ recipe_scrapers/consts.py
@@ -2,7 +2,7 @@ import re
 
 
 TIME_REGEX = re.compile(
-    r'\A(\s*(?P<hours>\d+)\s{1}(hours|hrs|hr|h))?((?P<minutes>\s*\d+)\s{1}(minutes|mins|min|m))?\Z'
+    r'\A(\s*(?P<hours>\d+)\s*(hours|hrs|hr|h))?(\s*(?P<minutes>\d+)\s*(minutes|mins|min|m))?\Z'
 )
 
 
