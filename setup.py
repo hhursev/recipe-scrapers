3825e3be62f3d18f8499409cd5e3f09228b35f23
hhursev <hhursev@gmail.com>
2015-09-21 05:20:44 +0300

    Test against different versions of python in travis

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
     ],
 )
