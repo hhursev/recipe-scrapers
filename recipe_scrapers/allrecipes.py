eb0e0f191fbed74de87482821f82abad3e438d5d
Zachary Vance <za3k@za3k.com>
2017-06-15 17:49:13 -0700

    Add allrecipes.com specific information

--- recipe_scrapers/allrecipes.py
+++ recipe_scrapers/allrecipes.py
@@ -1,7 +1,6 @@
 from ._abstract import AbstractScraper
 from ._utils import get_minutes, normalize_string
 
-
 class AllRecipes(AbstractScraper):
 
     @classmethod
@@ -13,6 +12,12 @@ class AllRecipes(AbstractScraper):
 
     def total_time(self):
         return get_minutes(self.soup.find('span', {'class': 'ready-in-time'}))
+
+    def prep_time(self):
+        return get_minutes(self.soup.find('time', {'itemprop': 'cookTime'}))
+
+    def cook_time(self):
+        return get_minutes(self.soup.find('time', {'itemprop': 'prepTime'}))
 
     def ingredients(self):
         ingredients_html = self.soup.findAll('li', {'class': "checkList__line"})
@@ -30,3 +35,24 @@ class AllRecipes(AbstractScraper):
             normalize_string(instruction.get_text())
             for instruction in instructions_html
         ])
+
+    def footnotes(self):
+        footnotes_html_container = self.soup.find('section', {'class': 'recipe-footnotes'})
+        if footnotes_html_container is None:
+            return ""
+        footnotes_html = footnotes_html_container.findAll('li')
+
+        return '\n'.join([
+            normalize_string(footnote.get_text())
+            for footnote in footnotes_html
+            if footnote.get_text(strip=True) not in ('Tip', 'Editor\'s Note')
+        ])
+
+    def photo_url(self):
+        return self.soup.find('img', {'class': 'rec-photo'})['src']
+
+    def review_count(self):
+        return int(self.soup.find('meta', {'itemprop': 'reviewCount'})['content'])
+
+    def rating_stars(self):
+        return float(self.soup.find('meta', {'itemprop': 'ratingValue'})['content'])
