50296130a0f7f82300e7afbbcc0931b58d73b50d
Zachary Vance <za3k@za3k.com>
2017-06-15 17:46:23 -0700

    Update allrecipes.com to ignore new advertisement text

--- recipe_scrapers/allrecipes.py
+++ recipe_scrapers/allrecipes.py
@@ -18,9 +18,9 @@ class AllRecipes(AbstractScraper):
         ingredients_html = self.soup.findAll('li', {'class': "checkList__line"})
 
         return [
-            normalize_string(ingredient.get_text())
+            normalize_string(ingredient.get_text().replace("ADVERTISEMENT",""))
             for ingredient in ingredients_html
-            if ingredient.get_text(strip=True) not in ('Add all ingredients to list', '')
+            if ingredient.get_text(strip=True) not in ('Add all ingredients to list', '', 'ADVERTISEMENT')
         ]
 
     def instructions(self):
