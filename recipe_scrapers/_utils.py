81db5ddd393a0dccf61c37414679a1938b7310cd
Avalanche <root@avalanche.za3k.com>
2017-07-04 01:12:02 -0700

    Improve cookstr.com recipe scraper

--- recipe_scrapers/_utils.py
+++ recipe_scrapers/_utils.py
@@ -1,10 +1,14 @@
 import re
+import bs4
 
 
 TIME_REGEX = re.compile(
     r'(\D*(?P<hours>\d+)\s*(hours|hrs|hr|h|Hours|H))?(\D*(?P<minutes>\d+)\s*(minutes|mins|min|m|Minutes|M))?'
 )
 
+SERVING_REGEX = re.compile(
+    r'(\d+)\s*servings|(?:serves|makes)\s*(\d+)?|^\s*(\d+)\s*$'
+)
 
 def get_minutes(dom_element):
     try:
@@ -18,6 +22,21 @@ def get_minutes(dom_element):
     except AttributeError:  # if dom_element not found or no matched
         return 0
 
+def get_servings(dom_element):
+    try:
+        if isinstance(dom_element, bs4.element.Tag):
+            tstring = dom_element.get_text()
+        else:
+            tstring = str(dom_element)
+        matched = SERVING_REGEX.search(tstring)
+        text = None
+        for i in range(1,4):
+            if matched.group(i):
+                text = matched.group(i)
+        servings = int(text or 0)
+        return servings
+    except AttributeError:
+        return 0
 
 def normalize_string(string):
     return re.sub(
