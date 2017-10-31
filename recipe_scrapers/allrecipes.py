f12c3ac125294ed597130240871111b6a494d8bf
hhursev <hhursev@gmail.com>
2017-10-31 15:14:16 +0200

    Remove unexpected extra functionality from the allrecipes scraper

--- recipe_scrapers/allrecipes.py
+++ recipe_scrapers/allrecipes.py
@@ -14,12 +14,6 @@ class AllRecipes(AbstractScraper):
     def total_time(self):
         return get_minutes(self.soup.find('span', {'class': 'ready-in-time'}))
 
-    def prep_time(self):
-        return get_minutes(self.soup.find('time', {'itemprop': 'cookTime'}))
-
-    def cook_time(self):
-        return get_minutes(self.soup.find('time', {'itemprop': 'prepTime'}))
-
     def ingredients(self):
         ingredients_html = self.soup.findAll('li', {'class': "checkList__line"})
 
@@ -36,24 +30,3 @@ class AllRecipes(AbstractScraper):
             normalize_string(instruction.get_text())
             for instruction in instructions_html
         ])
-
-    def footnotes(self):
-        footnotes_html_container = self.soup.find('section', {'class': 'recipe-footnotes'})
-        if footnotes_html_container is None:
-            return ""
-        footnotes_html = footnotes_html_container.findAll('li')
-
-        return '\n'.join([
-            normalize_string(footnote.get_text())
-            for footnote in footnotes_html
-            if footnote.get_text(strip=True) not in ('Tip', 'Editor\'s Note')
-        ])
-
-    def photo_url(self):
-        return self.soup.find('img', {'class': 'rec-photo'})['src']
-
-    def review_count(self):
-        return int(self.soup.find('meta', {'itemprop': 'reviewCount'})['content'])
-
-    def rating_stars(self):
-        return float(self.soup.find('meta', {'itemprop': 'ratingValue'})['content'])
