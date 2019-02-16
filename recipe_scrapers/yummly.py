a8a097dcec6a8bc269477d25b6808beacce787db
hhursev <hhursev@gmail.com>
2019-02-16 16:49:37 +0200

    Slight refactor of Yummly scraper. This with PR #55 closes #54

--- recipe_scrapers/yummly.py
+++ recipe_scrapers/yummly.py
@@ -1,6 +1,5 @@
 from ._abstract import AbstractScraper
 from ._utils import get_minutes, normalize_string
-import json
 
 
 class Yummly(AbstractScraper):
@@ -22,26 +21,15 @@ class Yummly(AbstractScraper):
             'li',
             {'class': "IngredientLine"}
         )
-        ingredients_data = []
-        for ingredient in ingredients:
-            # Add if amount found
-            amount = ingredient.find('span', {'class': 'amount'})
-            amount_str = normalize_string(amount.get_text()) if amount else ''
-            # Add if unit found
-            unit = ingredient.find('span', {'class': 'unit'})
-            unit_str = ' {}'.format(normalize_string(unit.get_text())) if unit else ''
-            # Add if ingredient name found
-            ingredient_name = ingredient.find('span', {'class': 'ingredient'})
-            ingredient_name_str = ' {}'.format(normalize_string(ingredient_name.get_text())) if ingredient_name else ''
 
-            ingredients_data.append(
-                '{amount}{unit}{ingredient_name}'.format(
-                    amount=amount_str, unit=unit_str, ingredient_name=ingredient_name_str,
-                )
-            )
-
-        return ingredients_data
+        return [
+            normalize_string(ingredient.get_text()) for
+            ingredient in ingredients
+        ]
 
     def instructions(self):
-        # Yummly does not provide instructions on it's page. It has a link that redirects to original website.
+        """
+        Yummly does not provide instructions on it's page.
+        It has a link that redirects to original website.
+        """
         return ''
