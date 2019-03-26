8043e2b5f4ff08343dc3cedbd0bc3a1b1a8155a8
Gunnar Thor <ornolfsson@gmail.com>
2019-03-26 16:17:56 +0000

    Add yummly instructions scraping (#57)
    * Add yummly instructions scraping
    
    I found that yummly do in fact include instructions on their website. I added code that should return a list of strings which comprise the instructions.
    
    * Fixed missing self
    
    My code was missing a self. in front of the soup call
    
    * Added condition for no instructions case
    
    It seems to vary between recipes. Now it should return an empty string if there are no instructions but otherwise output the instructions.

--- recipe_scrapers/yummly.py
+++ recipe_scrapers/yummly.py
@@ -34,8 +34,8 @@ class Yummly(AbstractScraper):
         ]
 
     def instructions(self):
-        """
-        Yummly does not provide instructions on it's page.
-        It has a link that redirects to original website.
-        """
-        return ''
+        instructions = self.soup.find('div', attrs={'class': 'directions-wrapper'})
+        return '\n'.join([
+            normalize_string(instr.get_text())
+            for instr in instructions.findAll('span', attrs={'class': 'step'})
+        ]) if instructions is not None else ''
