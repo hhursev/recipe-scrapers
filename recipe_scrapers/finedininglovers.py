159e94edfd2eddeaa187bcb5f525245b26463d9f
Marco Lussetti <marco@marcolussetti.com>
2019-10-15 22:19:51 -0700

    Fix finediningloves #73
    They redid the entire page structure it looks like, and made it a bit
    more unpleasant to scrape. Not sure if it's worth going to this much
    trouble to add images in terms of brittleness.

--- recipe_scrapers/finedininglovers.py
+++ recipe_scrapers/finedininglovers.py
@@ -10,41 +10,57 @@ class FineDiningLovers(AbstractScraper):
 
     def title(self):
         return self.soup.find(
-            'h3',
-            {'itemprop': 'name'}
-        ).get_text()
+            'div',
+            {'class': 'recipe-detail'}
+        ).find('h3').get_text()
 
     def total_time(self):
         return get_minutes(self.soup.find(
-            'time',
-            {'itemprop': 'prepTime'})
+            'div',
+            {'class': 'timing'})
         )
-
 
     def yields(self):
-        return get_yields(self.soup.find(
-            'span',
-            {'itemprop': 'recipeYield'})
+        yields = self.soup.find(
+            'div',
+            {'class': 'field--name-field-recipe-serving-num'}
         )
 
+        return get_yields("{} servings".format(yields))
+
     def ingredients(self):
-        ingredients = self.soup.findAll(
-            'li',
-            {'itemprop': "ingredients"}
+        ingredients_parent = self.soup.find(
+            'div',
+            {'class': 'ingredients-box'}
+        )
+        ingredients = ingredients_parent.findAll(
+            'div',
+            {'class': "paragraph--type--recipe-ingredient"}
         )
 
         return [
-            normalize_string(ingredient.find('a').get_text())
+            normalize_string(ingredient.get_text())
             for ingredient in ingredients
         ]
 
     def instructions(self):
-        instructions = self.soup.findAll(
+        instructions_parent = self.soup.find(
             'div',
-            {'itemprop': 'recipeInstructions'}
+            {'class': 'field--name-field-recipe-para-steps'}
+        )
+        instructions = instructions_parent.findAll(
+            'div',
+            {'class': "paragraph--type--recipe-step"}
         )
 
         return '\n'.join([
             normalize_string(instruction.get_text())
             for instruction in instructions
         ])
+
+    def image(self):
+        image = self.soup.select_one('.image-zone picture img')
+        image_url = image['data-src'].split("?")[0]
+        image_base_url = "https://www.finedininglovers.com"
+
+        return "{}{}".format(image_base_url, image_url) if image else None
