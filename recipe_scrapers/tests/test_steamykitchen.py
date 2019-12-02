2241a25a146f07056d7f14d687b5e2d9925166d3
James Addison <james@reciperadar.com>
2019-12-02 16:04:42 +0000

    Partial image change revert (#92)
    * Revert "Default to using og:image when fetching image as proposed by @jayaddison"
    
    This reverts commit ccf3c759f387d572652c5ddf7729f206166591b6.
    
    * Partially revert og:image change
    
    * Restore foodnetwork from master

--- recipe_scrapers/tests/test_steamykitchen.py
+++ recipe_scrapers/tests/test_steamykitchen.py
@@ -40,7 +40,7 @@ class TestSteamyKitchenScraper(unittest.TestCase):
 
     def test_image(self):
         self.assertEqual(
-            'https://steamykitchen.com/wp-content/uploads/2012/11/pork-paprikash-recipe-featured-9514.jpg',
+            'https://steamykitchen.com/wp-content/uploads/2012/11/pork-paprikash-recipe-featured-9514-180x220.jpg',
             self.harvester_class.image()
         )
 
