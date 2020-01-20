0a490a409fe426c57053dbbf4ccbbf36eb65f5b5
Greg Walters <greg.gregwa@gmail.com>
2020-01-20 09:21:59 -0600

    Support for BettyCrocker.com Website

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ from recipe_scrapers.tests.test_allrecipes import *
 from recipe_scrapers.tests.test_allrecipes_fallback_format import *
 from recipe_scrapers.tests.test_bbcfood import *
 from recipe_scrapers.tests.test_bbcgoodfood import *
+from recipe_scrapers.tests.test_bettycrocker import *
 from recipe_scrapers.tests.test_bonappetit import *
 from recipe_scrapers.tests.test_budgetbytes import *
 from recipe_scrapers.tests.test_closetcooking import *
