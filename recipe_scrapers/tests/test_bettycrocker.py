fad0da7e2643e9b368fa809a62365f65737865db
Greg Walters <greg.gregwa@gmail.com>
2020-01-20 10:49:04 -0600

    Fixed test_bettycrocker.py class name from TestAllRecipeScraper to TestBettyCrocker

--- recipe_scrapers/tests/test_bettycrocker.py
+++ recipe_scrapers/tests/test_bettycrocker.py
@@ -4,7 +4,7 @@ import unittest
 from recipe_scrapers.bettycrocker import BettyCrocker
 
 
-class TestAllRecipesScraper(unittest.TestCase):
+class TestBettyCrocker(unittest.TestCase):
     def setUp(self):
         # tests are run from tests.py
         with open(os.path.join(
