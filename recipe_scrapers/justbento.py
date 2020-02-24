54a4d12c009841ef1de982fb3b0cf9d046569712
James Addison <james@reciperadar.com>
2020-02-24 19:46:51 +0000

    Combine title & ingredients preconditions for instructions match

--- recipe_scrapers/justbento.py
+++ recipe_scrapers/justbento.py
@@ -43,7 +43,7 @@ class JustBento(AbstractScraper):
         elements_after_title = self.soup.find(
             'div',
             {'class': 'field-name-body'}
-        ).find('ul').find_next_siblings()
+        ).find('h3').find_next_sibling('ul').find_next_siblings()
 
         instructions = []
         for element in elements_after_title:
