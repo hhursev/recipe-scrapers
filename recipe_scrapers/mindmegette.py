fd9f213685e8f775047f6373592946da55991457
ildi <ildiko.eigel@ibm.com>
2020-03-28 16:32:59 +0100

    Added image and yield fields to mindmegette

--- recipe_scrapers/mindmegette.py
+++ recipe_scrapers/mindmegette.py
@@ -1,6 +1,5 @@
 from ._abstract import AbstractScraper
-from ._utils import get_minutes, normalize_string
-import json
+from ._utils import get_minutes, normalize_string, get_yields
 
 
 class Mindmegette(AbstractScraper):
@@ -14,6 +13,10 @@ class Mindmegette(AbstractScraper):
 
     def total_time(self):
         return get_minutes(self.soup.find('span', {'class': 'time'}))
+
+    def image(self):
+        image_relative_url = self.soup.find('img', {'itemprop': 'photo', 'src': True})['src']
+        return f"http://{self.host()}{image_relative_url}" if image_relative_url else None
 
     def ingredients(self):
         ingredients_html = self.soup.findAll('li', {'itemprop': "ingredient"})
@@ -30,3 +33,9 @@ class Mindmegette(AbstractScraper):
             normalize_string(instruction.get_text())
             for instruction in instructions
         ])
+
+    def yields(self):
+        return get_yields(self.soup.find(
+            'span',
+            {'class': 'portion'})
+        )
