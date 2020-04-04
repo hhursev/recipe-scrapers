21ef53f55f0b1cfbda11a89bf77937fed2384a24
ildi <ildiko.eigel@ibm.com>
2020-04-04 11:20:10 +0200

    Fix instructions section for mindmegette

--- recipe_scrapers/tests/test_mindmegette.py
+++ recipe_scrapers/tests/test_mindmegette.py
@@ -25,54 +25,36 @@ class TestMindmegetteScraper(unittest.TestCase):
     def test_title(self):
         self.assertEqual(
             self.harvester_class.title(),
-            'Jókai-bableves'
+            'Tepsis krumpli céklával és répával'
         )
 
     def test_total_time(self):
         self.assertEqual(
-            90,
+            45,
             self.harvester_class.total_time()
         )
 
     def test_ingredients(self):
         self.assertListEqual(
             [
-                '18 dkg szárazbab (vagy 30 dkg friss fejtett bab)',
-                '1 db füstölt sertéscsülök',
-                '8 dkg petrezselyemgyökér',
-                '10 dkg sárgarépa',
-                '1 db babérlevél',
-                '1 gerezd fokhagyma',
-                '15 dkg zöldpaprika',
-                '7 dkg friss paradicsom',
+                '6 db nagyobb burgonya',
+                '2 db cékla',
+                '8 db közepes méretű répa',
+                '4 db lilahagyma',
+                '6 gerezd fokhagyma',
+                'friss rozmaring',
                 'só',
-                '30 dkg kolbász',
-                '4 dkg zsír',
-                '3 dkg liszt',
-                '3 dkg vöröshagyma',
-                '1/2 dkg fűszerpaprika',
-                '1/2-1 csokor petrezselyem zöldje',
-                '1,5 dl tejföl (+ a tálaláshoz)',
-                '3 dkg liszt',
-                'csipetke'
+                'bors'
             ],
             self.harvester_class.ingredients()
         )
 
     def test_instructions(self):
         return self.assertEqual(
-            "A szárazbabot jól megmossuk és előző este beáztatjuk. (Fejtett bab esetén erre nincs szükség.) " +
-            "A csülköt kb. 1 1/2 l vízben vajpuhára főzzük.\n" +
-            "Másnap a karikára vágott gyökérzöldséget a főzőlé tetején megfagyott s arról villával leszedett zsírján " +
-            "megpirítjuk. Ha barnulni kezd, az áztatólével együtt hozzáöntjük a babot, és felöntjük a füstölt ízű " +
-            "lével. Hozzáadjuk a babérlevelet, az összezúzott fokhagymát, a kis kockára vágott zöldpaprikát és " +
-            "paradicsomot. A csülöklé sótartalmától függően kevés sóval ízesítjük, és puhára főzzük.\n" +
-            "Közben a kolbászt a zsíron megsütjük, majd kivesszük, és vékony karikákra vágjuk. Ha a bab megpuhult, " +
-            "a kolbászzsírral és a liszttel világos rántást készítünk az apróra vágott vöröshagymával. " +
-            "Az utolsó pillanatban meghintjük a fűszerpaprikával és aprított petrezselyemzölddel.\n" +
-            "Ha a leves a rántással felforrt, a tejföllel behabarjuk. Végül csipetkét főzünk bele, és a " +
-            "kolbászkarikákkal együtt felforraljuk. Tálalás előtt apró kockákra vágjuk a csülökhúst, és rámerjük " +
-            "a forró levest. Egy kevés tejföllel díszítve kínáljuk.",
+            "A zöldségeket megpucoljuk és megmossuk.\n" +
+            "A burgonyát vastagabb szeletekre vágjuk, a répákat hosszában elfelezzük, a lilahagymát félbe vagy negyedbe vágjuk, a céklát szeleteljük, a fokhagymát egészben hagyjuk.\n" +
+            "Az összes, előkészített zöldséget egy tepsibe tesszük, meglocsoljuk olívaolajjal, sózzuk és borsozzuk, kézzel összeforgatjuk az egészet, majd friss rozmaringot teszünk rá.\n" +
+            "Lefedjük alufóliával, 180 fokon 20 percig sütjük, majd fólia nélkül, amíg minden zöldség meg nem puhul. Grill funkció esetén az utolsó 5 percben meg is piríthatjuk. Sült húsok mellé kiváló, laktató köret.",
             self.harvester_class.instructions()
         )
 
@@ -84,6 +66,6 @@ class TestMindmegetteScraper(unittest.TestCase):
 
     def test_image(self):
         self.assertEqual(
-            'http://mindmegette.hu/images/163/O/crop_201609051236_jokai_bableves.jpg',
+            'http://mindmegette.hu/images/283/O/tepsis-ceklas.jpg',
             self.harvester_class.image()
         )
