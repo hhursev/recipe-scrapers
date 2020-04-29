8f066001ea4731b497d16cbc569cffbdb37939b3
James Addison <jay@jp-hosting.net>
2020-04-29 13:50:05 +0100

    Support Giallo Zafferano test

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     install_requires=[
         "beautifulsoup4>=4.6.0",
         "requests>=2.19.1",
+        "tldextract==2.2.2",
     ],
     packages=find_packages(),
     package_data={"": ["LICENSE"]},
