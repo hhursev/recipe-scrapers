d0b66afd02edd65b311816de02821fdfc363fe63
James Addison <jay@jp-hosting.net>
2020-04-29 17:56:48 +0100

    Remove 'os' import dependency

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,3 @@
-import os
 import unittest
 
 from tldextract import TLDExtract
@@ -9,9 +8,5 @@ class ScraperTest(unittest.TestCase):
     def setUp(self):
         tldextract = TLDExtract(suffix_list_urls=None)
         url_info = tldextract(self.scraper_class.host())
-        with open(os.path.join(
-            os.path.dirname(os.path.realpath(__file__)),
-            'test_data',
-            '{}.testhtml'.format(url_info.domain)
-        )) as file_opened:
-            self.harvester_class = self.scraper_class(file_opened, test=True)
+        with open('tests/test_data/{}.testhtml'.format(url_info.domain)) as f:
+            self.harvester_class = self.scraper_class(f, test=True)
