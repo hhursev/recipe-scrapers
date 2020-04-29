195354be6bf69396e48f258faeb7205f956d3a02
James Addison <jay@jp-hosting.net>
2020-04-29 13:50:05 +0100

    Support Giallo Zafferano test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,13 +1,16 @@
 import os
 import unittest
 
+from tldextract import TLDExtract
+
 class ScraperTest(unittest.TestCase):
 
     def setUp(self):
-        domain = self.scraper_class.host().split('.')[0]
+        tldextract = TLDExtract(suffix_list_urls=None)
+        url_info = tldextract(self.scraper_class.host())
         with open(os.path.join(
             os.path.dirname(os.path.realpath(__file__)),
             'test_data',
-            '{}.testhtml'.format(domain)
+            '{}.testhtml'.format(url_info.domain)
         )) as file_opened:
             self.harvester_class = self.scraper_class(file_opened, test=True)
