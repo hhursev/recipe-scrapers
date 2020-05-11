f95e7f6bedff7515a16548b6ab734a2938b0f857
Tobia Ghiraldini <tobia.ghiraldini@ninjabit.com>
2020-05-11 11:42:20 +0200

    Declaring vegolosi scraper

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -54,6 +54,7 @@ from .thevintagemixer import TheVintageMixer
 from .tineno import TineNo
 from .tudogostoso import TudoGostoso
 from .twopeasandtheirpod import TwoPeasAndTheirPod
+from .vegolosi import Vegolosi
 from .whatsgabycooking import WhatsGabyCooking
 from .wikicookbook import WikiCookbook
 from .yummly import Yummly
@@ -113,6 +114,7 @@ SCRAPERS = {
     TineNo.host(): TineNo,
     TudoGostoso.host(): TudoGostoso,
     TwoPeasAndTheirPod.host(): TwoPeasAndTheirPod,
+    Vegolosi.host(): Vegolosi,
     WhatsGabyCooking.host(): WhatsGabyCooking,
     WikiCookbook.host(): WikiCookbook,
     Yummly.host(): Yummly,
