3f5f9b0dbe4e03194003212f64f6052ad8b46ffd
patrickpierce <pierce.p@icloud.com>
2020-05-12 12:54:47 -0600

    add motherthyme exception for missing time and rating

--- recipe_scrapers/motherthyme.py
+++ recipe_scrapers/motherthyme.py
@@ -3,57 +3,49 @@ from ._utils import get_minutes, normalize_string, get_yields
 
 
 class MotherThyme(AbstractScraper):
-
     @classmethod
     def host(self):
-        return 'motherthyme.com'
+        return "motherthyme.com"
 
     def title(self):
-        return self.soup.find(
-            'h2',
-            {'class': 'wprm-recipe-name'}
-        ).get_text()
+        return self.soup.find("h2", {"class": "wprm-recipe-name"}).get_text()
 
     def total_time(self):
-        return get_minutes(self.soup.find(
-            'span',
-            {'class': 'wprm-recipe-total_time'}).parent
-        )
+        try:
+            return get_minutes(
+                self.soup.find("span", {"class": "wprm-recipe-total_time"}).parent
+            )
+        except Exception:
+            return 0
 
     def yields(self):
-        yields = self.soup.find(
-            'span',
-            {'class': 'wprm-recipe-servings'}
-        ).get_text()
+        yields = self.soup.find("span", {"class": "wprm-recipe-servings"}).get_text()
 
         return get_yields("{} servings".format(yields))
 
     def ingredients(self):
-        ingredients = self.soup.findAll(
-            'li',
-            {'class': 'wprm-recipe-ingredient'}
-        )
+        ingredients = self.soup.findAll("li", {"class": "wprm-recipe-ingredient"})
 
-        return [
-            normalize_string(ingredient.get_text())
-            for ingredient in ingredients
-        ]
+        return [normalize_string(ingredient.get_text()) for ingredient in ingredients]
 
     def instructions(self):
         instructions = self.soup.findAll(
-            'div',
-            {'class': 'wprm-recipe-instruction-text'}
+            "div", {"class": "wprm-recipe-instruction-text"}
         )
 
-        return '\n'.join([
-            normalize_string(instruction.get_text())
-            for instruction in instructions
-        ])
+        return "\n".join(
+            [normalize_string(instruction.get_text()) for instruction in instructions]
+        )
 
     def ratings(self):
-        return round(float(
-            self.soup.find(
-                "span",
-                {"class": "wprm-recipe-rating-average"}
-            ).get_text()), 2
-        )
+        try:
+            return round(
+                float(
+                    self.soup.find(
+                        "span", {"class": "wprm-recipe-rating-average"}
+                    ).get_text()
+                ),
+                2,
+            )
+        except Exception:
+            return -1
