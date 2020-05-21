20cdd71cbe851065fb0ecbb002368d234c1ae4b7
patrickpierce <pierce.p@icloud.com>
2020-05-21 14:15:13 -0600

    remove f-string for python 3.5 support

--- recipe_scrapers/cookieandkate.py
+++ recipe_scrapers/cookieandkate.py
@@ -26,7 +26,7 @@ class CookieAndKate(AbstractScraper):
             {'class': 'tasty-recipes-yield'}
         ).get_text()
 
-        return get_yields(f"{yields} servings")
+        return get_yields("{} servings".format(yields))
 
     def ingredients(self):
         ingredients = self.soup.find(
