727c21439cdca9ce5bf06e49a050b3e0f75ef7dc
hhursev <hhursev@gmail.com>
2020-05-22 23:30:40 +0300

    Update copykat so to solve its issues, refs #162

--- tests/test_copykat.py
+++ tests/test_copykat.py
@@ -14,7 +14,7 @@ class TestCopyKat(ScraperTest):
         self.assertEqual(self.harvester_class.title(), "Make Tender Beef Tips in Gravy")
 
     def test_total_time(self):
-        self.assertEqual("40", self.harvester_class.total_time())
+        self.assertEqual(40, self.harvester_class.total_time())
 
     def test_yields(self):
         self.assertEqual("10 serving(s)", self.harvester_class.yields())
@@ -35,7 +35,7 @@ class TestCopyKat(ScraperTest):
                 "48 ounces beef stock",
                 "1 tablespoon vegetable oil",
                 "2 teaspoons gravy master",
-                "1 cup onions chopped",
+                "1 cup onions (chopped)",
                 "16 ounces noodles",
             ],
             self.harvester_class.ingredients(),
@@ -43,12 +43,12 @@ class TestCopyKat(ScraperTest):
 
     def test_instructions(self):
         return self.assertEqual(
-            "In a small bowl add flour, salt, and pepper. Stir the salt and pepper into the flour. Cut and trim roast into small bite-sized pieces. Dredge beef pieces in seasoned flour shake off excess flour.\nSet the Instant Pot to saute, add oil. When the oil has heated drop in several pieces of the beef. Cook seasoned beef on all sides until lightly browned. Cook beef in small batches. When all of the beef is cooked add it back to the Instant Pot.\nAdd 1 cup of onion, two teaspoons of Gravy Master, and beef stock. Place lid on high and cook for 15 minutes on high pressure. Release pot after cooking with either a quick release or a natural release.Slow cooker directionsPlease use the same ingredients as listed below. For this recipe season your flour as mentioned in the recipe, then brown the meat in a large skillet in small batches with some vegetable oil. Add the beef broth you will simmer for 4 to 6 hours on low. If the liquid hasn't thickened up to your desire, you can thicken it up by mixing 1 tablespoon of butter and one tablespoon of flour that has been mixed together. Stir this in to the beef broth, and it will thicken up the liquid in the slow cooker.",
+            "In a small bowl add flour, salt, and pepper. Stir the salt and pepper into the flour. Cut and trim roast into small bite-sized pieces. Dredge beef pieces in seasoned flour shake off excess flour.\nSet the Instant Pot to saute, add oil. When the oil has heated drop in several pieces of the beef. Cook seasoned beef on all sides until lightly browned. Cook beef in small batches. When all of the beef is cooked add it back to the Instant Pot.\nAdd 1 cup of onion, two teaspoons of Gravy Master, and beef stock. Place lid on high and cook for 15 minutes on high pressure. Release pot after cooking with either a quick release or a natural release.\nSlow cooker directions\nPlease use the same ingredients as listed below. For this recipe season your flour as mentioned in the recipe, then brown the meat in a large skillet in small batches with some vegetable oil. Add the beef broth you will simmer for 4 to 6 hours on low. If the liquid hasn't thickened up to your desire, you can thicken it up by mixing 1 tablespoon of butter and one tablespoon of flour that has been mixed together. Stir this in to the beef broth, and it will thicken up the liquid in the slow cooker.",
             self.harvester_class.instructions(),
         )
 
     def test_ratings(self):
-        self.assertEqual("5 from 1 vote", self.harvester_class.ratings())
+        self.assertEqual(5, self.harvester_class.ratings())
 
     def test_description(self):
         self.assertEqual(
