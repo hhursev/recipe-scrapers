1221c3c0edf3e47246c7504d82aa4d418020400d
patrickpierce <pierce.p@icloud.com>
2020-05-30 15:25:33 -0600

    fix-testhtml

--- tests/test_myrecipes.py
+++ tests/test_myrecipes.py
@@ -17,7 +17,7 @@ class TestMyRecipesScraper(ScraperTest):
         self.assertEqual(20, self.harvester_class.total_time())
 
     def test_yields(self):
-        self.assertEqual("2 serving(s)", self.harvester_class.yields())
+        self.assertEqual("Serves 2 (serving size: 1 1/2 cups)", self.harvester_class.yields())
 
     def test_ingredients(self):
         self.assertCountEqual(
@@ -33,9 +33,9 @@ class TestMyRecipesScraper(ScraperTest):
 
     def test_instructions(self):
         return self.assertEqual(
-            "\nAdd pasta to a large skillet over high; cover with water, and add 1 teaspoon of the salt. Bring to a boil, and cook, stirring occasionally, until nearly tender, about 6 minutes.\nMeanwhile, heat oil in a medium skillet over medium; stir in black pepper, and cook until toasted, about 1 minute. Remove from heat.\nWhisk 3 tablespoons of the pasta cooking water into oil and pepper. Using tongs, transfer pasta into oil and pepper mixture, reserving pasta cooking water in skillet. Cook over low, stirring constantly, while sprinkling in cheese.\nAdd more cooking water as needed, 1 tablespoon at a time, to create a creamy sauce. Stir in remaining 1/2 teaspoon salt. Divide pasta between 2 bowls, and garnish with pepper and cheese. Serve immediately.",
+            "Add pasta to a large skillet over high; cover with water, and add 1 teaspoon of the salt. Bring to a boil, and cook, stirring occasionally, until nearly tender, about 6 minutes. Meanwhile, heat oil in a medium skillet over medium; stir in black pepper, and cook until toasted, about 1 minute. Remove from heat. Whisk 3 tablespoons of the pasta cooking water into oil and pepper. Using tongs, transfer pasta into oil and pepper mixture, reserving pasta cooking water in skillet. Cook over low, stirring constantly, while sprinkling in cheese. Add more cooking water as needed, 1 tablespoon at a time, to create a creamy sauce. Stir in remaining 1/2 teaspoon salt. Divide pasta between 2 bowls, and garnish with pepper and cheese. Serve immediately.",
             self.harvester_class.instructions(),
         )
 
     def test_ratings(self):
-        self.assertEqual(5, self.harvester_class.ratings())
+        self.assertEqual(-1, self.harvester_class.ratings())
