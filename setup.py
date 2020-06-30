37d9ad2f61fdb79b782d9703c1d40e66bb7e7b09
hhursev <hhursev@gmail.com>
2020-06-30 12:40:29 +0300

    Bump version to 7.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name="recipe_scrapers",
     url="https://github.com/hhursev/recipe-scrapers/",
-    version="7.2.0",
+    version="7.3.0",
     author="Hristo Harsev",
     author_email="r+pypi@hharsev.com",
     description="Python package, scraping recipes from all over the internet",
