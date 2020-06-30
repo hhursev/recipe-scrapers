02a562e723362117cf096a49836225e3aef9d874
Altan Tutar <alttutar@gmail.com>
2020-06-30 12:34:44 +0300

    Scraping functionality for acouplecooks.com (#173)
    * added the recipe scraper for acouplecook.com with tests
    
    * deleted unit testing for the pull request
    
    * added the setup file
    
    * Update test_acouplecooks.py
    
    * Re - update test_acouplecooks.py
    
    Co-authored-by: hhursev <hhursev@users.noreply.github.com>

---
+++ recipe_scrapers/acouplecooks.py
@@ -0,0 +1,39 @@
+from ._abstract import AbstractScraper
+from ._utils import get_minutes, normalize_string, get_yields
+
+
+class ACoupleCooks(AbstractScraper):
+    @classmethod
+    def host(self):
+        return 'acouplecooks.com'
+
+    def title(self):
+        return self.soup.find(
+             'h2',
+             {'class': 'tasty-recipes-title'}
+        ).get_text()
+    
+    def total_time(self):
+        return get_minutes(self.soup.find(
+            'span',
+            {'class': 'tasty-recipes-cook-time'})
+        )
+
+    def yields(self):
+        return get_yields(self.soup.find(
+            'span',
+            {'class': 'tasty-recipes-yield'}).span
+        )
+    
+    def ingredients(self):
+        return [normalize_string(child.text) for child in self.soup.find(
+            'div',
+            {'class':'tasty-recipes-ingredients'}).ul.children]
+    
+    def instructions(self):
+        return "\n".join([instruction.text for instruction in self.soup.find(
+            'div',
+            {'class':'tasty-recipes-instructions'}).ol.children
+        ])
+        
+
---
+++ tests/test_acouplecooks.py
@@ -0,0 +1,55 @@
+from tests import ScraperTest
+
+from recipe_scrapers.acouplecooks import ACoupleCooks
+
+
+# test recipe's URL
+# https://www.acouplecooks.com/garlic-butter-shrimp/
+class TestACoupleCooks(ScraperTest):
+
+     scraper_class = ACoupleCooks
+
+     def test_host(self):
+         self.assertEqual(
+             'acouplecooks.com',
+             self.harvester_class.host()
+         )
+
+     def test_title(self):
+         self.assertEqual(
+             'Garlic Butter Shrimp (Fast & Easy Dinner!)',
+             self.harvester_class.title()
+         )
+
+     def test_total_time(self):
+         self.assertEqual(
+             3,
+             self.harvester_class.total_time()
+         )
+
+     def test_yields(self):
+         self.assertEqual(
+             '4 serving(s)',
+             self.harvester_class.yields()
+         )
+
+     def test_ingredients(self):
+         self.assertEqual(
+             [
+                 '1 pound large shrimp, deveined (peeled or unpeeled)',
+                 '3 garlic cloves',
+                 '1/2 teaspoon kosher salt',
+                 '3 tablespoons butter',
+                 '2 lemon wedges',
+                 'Fresh cilantro or parsley, for garnish'
+             ],
+             self.harvester_class.ingredients()
+         )
+
+     def test_instructions(self):
+        self.assertEqual(
+             'If frozen, thaw the shrimp (see the notes above).\nMince the garlic.\nPat the shrimp dry. In a medium bowl, mix the shrimp with the garlic and salt.\nIn a large skillet, heat the butter on medium high heat. Cook the shrimp for 1 to 2 minutes per side until opaque and cooked through, turning them with tongs.\nSpritz with juice of the lemon wedges and serve immediately.',
+             self.harvester_class.instructions()
+        )
+
+
