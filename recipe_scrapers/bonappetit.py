4584fd1e3de3a96685f2194b519f9491578a4024
prough21 <41131390+prough21@users.noreply.github.com>
2020-08-09 17:21:48 -0500

    Update bonappetit.py
    Updating to grab all instructions when there is more than one steps-wrapper.  -example url: https://www.bonappetit.com/recipe/sticky-barbecue-chicken

--- recipe_scrapers/bonappetit.py
+++ recipe_scrapers/bonappetit.py
@@ -29,8 +29,8 @@ class BonAppetit(AbstractScraper):
         return [normalize_string(ingredient.get_text()) for ingredient in ingredients]
 
     def instructions(self):
-        instructions = [txt.findAll("li") for txt in scraper.soup.find_all("div", {"class": "steps-wrapper"})]
-        instructions = [item for sublist in instructions for item in sublist]
+        instructions = [div.findAll("li") for div in self.soup.find_all("div", {"class": "steps-wrapper"})]
+        instructions = [li for sublist in instructions for li in sublist]
 
         return "\n".join(
             [normalize_string(instruction.get_text()) for instruction in instructions]
