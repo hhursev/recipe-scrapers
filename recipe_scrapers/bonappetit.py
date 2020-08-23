c55c60b424e6b050dbdfdb612ec1be6dad50d9eb
Jim Simon <jksimoniii@users.noreply.github.com>
2020-08-22 20:17:38 -0400

    Implements Schema Scraper for Bon Appetit Scraper (#213)

--- recipe_scrapers/bonappetit.py
+++ recipe_scrapers/bonappetit.py
@@ -1,5 +1,4 @@
 from ._abstract import AbstractScraper
-from ._utils import normalize_string, get_yields
 
 
 class BonAppetit(AbstractScraper):
@@ -8,29 +7,16 @@ class BonAppetit(AbstractScraper):
         return "bonappetit.com"
 
     def title(self):
-        return self.soup.find("h1", {"itemprop": "name"}).get_text()
+        return self.schema.title()
 
     def total_time(self):
         return 0
 
     def yields(self):
-        return get_yields(
-            self.soup.find(
-                "span",
-                {
-                    "class": "recipe__header__servings recipe__header__servings--basically"
-                },
-            ).find("span")
-        )
+        return self.schema.yields()
 
     def ingredients(self):
-        ingredients = self.soup.findAll("li", {"class": "ingredient"})
-
-        return [normalize_string(ingredient.get_text()) for ingredient in ingredients]
+        return self.schema.ingredients()
 
     def instructions(self):
-        instructions = self.soup.find("div", {"class": "steps-wrapper"}).findAll("li")
-
-        return "\n".join(
-            [normalize_string(instruction.get_text()) for instruction in instructions]
-        )
+        return self.schema.instructions()
