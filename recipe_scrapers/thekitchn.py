6fd2a90b6fcab04787dba2e995d21a4a93b45c97
bajisci <bajisci@gmail.com>
2020-08-26 21:39:15 -0400

    updated testhtml for the kitchn.com, fixed yield and time scraping.

--- recipe_scrapers/thekitchn.py
+++ recipe_scrapers/thekitchn.py
@@ -11,11 +11,11 @@ class TheKitchn(AbstractScraper):
         return self.soup.find("h2", {"class": "Recipe__title"}).get_text()
 
     def total_time(self):
-        elements = self.soup.findAll("p", {"class": "Recipe__time-label"})
+        elements = self.soup.findAll("p", {"class": "Recipe__timeEntry"})
         return sum([get_minutes(element) for element in elements])
 
     def yields(self):
-        return get_yields(self.soup.find("span", {"class": "Recipe__yield-entry"}))
+        return get_yields(self.soup.find("p", {"class": "jsx-1778438071 Recipe__yield"}))
 
     def ingredients(self):
         ingredients = self.soup.findAll("li", {"class": "Recipe__ingredient"})
