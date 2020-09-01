38292299e1cef3cc6ac264eada4692927e097d6c
lucyColeclough <61566739+lucyColeclough@users.noreply.github.com>
2020-09-01 12:08:39 +0100

    Merge fixes to bbcgoodfood.com (#218)
    * Fix changes to the bbcgoodfood website by using their schema
    
    * Fix changes to the bbcgoodfood website by using their schema
    
    * bbcgoodfood: Get text from schema html and update test and test html

--- recipe_scrapers/bbcgoodfood.py
+++ recipe_scrapers/bbcgoodfood.py
@@ -1,5 +1,5 @@
 from ._abstract import AbstractScraper
-from ._utils import get_minutes, normalize_string, get_yields
+from bs4 import BeautifulSoup
 
 
 class BBCGoodFood(AbstractScraper):
@@ -8,53 +8,21 @@ class BBCGoodFood(AbstractScraper):
         return "bbcgoodfood.com"
 
     def title(self):
-        return self.soup.find("h1", {"itemprop": "name"}).get_text()
+        return self.schema.title()
 
     def total_time(self):
-        return sum(
-            [
-                get_minutes(
-                    self.soup.find(
-                        "span", {"class": "recipe-details__cooking-time-prep"}
-                    ).find("span")
-                ),
-                get_minutes(
-                    self.soup.find(
-                        "span", {"class": "recipe-details__cooking-time-cook"}
-                    ).find("span")
-                ),
-            ]
-        )
+        return self.schema.total_time()
 
     def yields(self):
-        return get_yields(
-            self.soup.find(
-                "span", {"class": "recipe-details__text", "itemprop": "recipeYield"}
-            )
-        )
+        return self.schema.yields()
 
     def image(self):
-        image = self.soup.find("img", {"itemprop": "image", "src": True})
-        return image["src"] if image else None
+        return self.schema.image()
 
     def ingredients(self):
-        ingredients = self.soup.findAll("li", {"itemprop": "ingredients"})
-
-        return [
-            normalize_string(
-                "{normal_text}{tooltip_text}".format(
-                    normal_text=ingredient.find(text=True),
-                    tooltip_text=ingredient.find("a").get_text()
-                    if ingredient.find("a") is not None
-                    else "",
-                )
-            )
-            for ingredient in ingredients
-        ]
+        return self.schema.ingredients()
 
     def instructions(self):
-        instructions = self.soup.findAll("li", {"itemprop": "recipeInstructions"})
-
-        return "\n".join(
-            [normalize_string(instruction.get_text()) for instruction in instructions]
-        )
+        inst = str(self.schema.instructions())
+        instSoup = BeautifulSoup(inst, features="html.parser")
+        return instSoup.text
