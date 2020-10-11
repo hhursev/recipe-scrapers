aa359661c31df53885e19f5acb2e0171b6f87398
Paul Tindall <ptindall@gotransverse.com>
2020-10-11 18:38:00 -0500

    Add wrapper methods for clarity.

--- recipe_scrapers/innit.py
+++ recipe_scrapers/innit.py
@@ -9,3 +9,24 @@ class Innit(AbstractScraper):
     @classmethod
     def host(self, domain="com"):
         return f"innit.{domain}"
+
+    def title(self):
+        return self.schema.title()
+
+    def total_time(self):
+        return self.schema.total_time()
+
+    def yields(self):
+        return self.schema.yields()
+
+    def image(self):
+        return self.schema.image()
+
+    def ingredients(self):
+        return self.schema.ingredients()
+
+    def instructions(self):
+        return self.schema.instructions()
+
+    def ratings(self):
+        return self.schema.ratings()
