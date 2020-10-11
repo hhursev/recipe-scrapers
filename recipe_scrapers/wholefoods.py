5856e4daaf141e5bf9cdef438378a3757297f9c0
Paul Tindall <ptindall@gotransverse.com>
2020-10-11 18:39:36 -0500

    Add wrapper methods for clarity.

--- recipe_scrapers/wholefoods.py
+++ recipe_scrapers/wholefoods.py
@@ -5,3 +5,24 @@ class WholeFoods(AbstractScraper):
     @classmethod
     def host(self, domain="com"):
         return f"www.wholefoodsmarket.{domain}"
+
+    def title(self):
+        return self.schema.title()
+
+    def total_time(self):
+        return self.schema.total_time()
+
+    def yields(self):
+        return self.schema.yields()
+
+    def image(self):
+        return self.schema.image()
+
+    def ingredients(self):
+        return self.schema.ingredients()
+
+    def instructions(self):
+        return self.schema.instructions()
+
+    def ratings(self):
+        return self.schema.ratings()
