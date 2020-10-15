07aa888f3017ebddcbdf22ea3425d110af39867b
EwenQuim <ewen.quimerch@gmail.com>
2020-10-15 14:46:33 +0200

    CuisineAZ tests

--- tests/test_cuisineaz.py
+++ tests/test_cuisineaz.py
@@ -11,35 +11,32 @@ class CuisineAZScraper(ScraperTest):
         self.assertEqual("cuisineaz.com", self.harvester_class.host())
 
     def test_title(self):
-        self.assertEqual(self.harvester_class.title(), "Filer")
+        self.assertEqual(self.harvester_class.title(), "Filet de saumon au four")
 
     def test_yields(self):
-        self.assertEqual("2 serving(s)", self.harvester_class.yields())
+        self.assertEqual("4 Pers.", self.harvester_class.yields())
 
     def test_image(self):
         self.assertEqual(
-            "https://lovingitvegan.com/wp-content/uploads/2018/08/Kale-Smoothie-8-225x225.jpg",
+            "https://img.cuisineaz.com/610x610/2014-02-24/i78436-filet-de-saumon-au-four.jpeg",
             self.harvester_class.image(),
         )
 
     def test_ingredients(self):
         self.assertCountEqual(
             [
-                "1 and 1/4 cups (300ml) Soy Milk (or sub almond milk)",
-                "2 Frozen Bananas (~200g, previously peeled, broken into quarters and frozen for at least 12 hours)",
-                "1/2 cup (75g) Raw Cashews",
-                "2 cups (56g) Kale (torn up, packed)",
-                "4 Medjool Dates (pitted)*",
-                "1 tsp Minced Ginger",
-                "1/8 tsp Cinnamon",
-                "1 Tbsp Lime Juice (freshly squeezed)",
-                "1 cup Ice Cubes",
+                "600 g de filet de saumon",
+                "30 ml de huile d'olive",
+                "4 g de origan séché",
+                "sel, poivre du moulin",
             ],
             self.harvester_class.ingredients(),
         )
 
     def test_instructions(self):
         return self.assertEqual(
-            "Add the soy milk to your blender jug and then the frozen bananas, cashews, kale, dates, minced ginger, cinnamon and fresh lime juice. Top with ice cubes.\nBlend until very smooth.\nPour out into glasses and serve.",
+            "Préchauffez le four à th.7 (210°C). Coupez le filet en pavés de saumon de même taille, correspondant au nombre de parts désirées, et déposez-les sur une plaque huilée ou dans un plat à four. Arrosez-les d'huile d'olive à leur tour. Salez et poivrez à votre convenance et parsemez d'origan.",
+            "Disposez le plat ou la plaque au centre du four. Comptez environ 10 min pour un filet de 2 à 2,5 cm d'épaisseur. Le temps nécessaire à la bonne cuisson du saumon dépend non seulement de l'épaisseur des pavés mais aussi de la température réelle de votre four, c'est pourquoi il est important de vérifier régulièrement la cuisson du saumon à l'aide d'une fourchette.",
+            "Lorsque vos pavés de saumon sont cuits, servez-les immédiatement, accompagnés d'une bonne salade bien assaisonnée, et éventuellement d'une belle timbale de riz basmati.",
             self.harvester_class.instructions(),
         )
