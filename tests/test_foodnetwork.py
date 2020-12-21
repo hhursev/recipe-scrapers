ec4a13d0e097e9abc9f5f2a8d779fecf487c76d5
James Addison <james@reciperadar.com>
2020-12-21 19:53:03 +0000

    Add TODO note regarding relative canonical URL extraction

--- tests/test_foodnetwork.py
+++ tests/test_foodnetwork.py
@@ -11,6 +11,7 @@ class TestFoodNetworkScraper(ScraperTest):
         self.assertEqual("foodnetwork.com", self.harvester_class.host())
 
     def test_canonical_url(self):
+        # TODO: Find a way to supply original content base URL at test-time (via WARC-file?)
         self.assertEqual("foodnetwork.testhtml_files/chicken-marsala-recipe-1951778.html", self.harvester_class.canonical_url())
 
     def test_title(self):
