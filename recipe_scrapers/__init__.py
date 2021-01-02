276330571c99364e590ac7a5eeb5499300ceb37c
Adam Keys <adam@key-solutions.ca>
2021-01-01 19:03:52 -0500

    Add support for eatingbirdfood.com
    Tests: https://www.eatingbirdfood.com/curried-shakshuka/

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -32,6 +32,7 @@ from .cuisineaz import CuisineAZ
 from .cybercook import Cybercook
 from .delish import Delish
 from .domesticateme import DomesticateMe
+from .eatingbirdfood import EatingBirdFood
 from .eatsmarter import Eatsmarter
 from .eatwhattonight import EatWhatTonight
 from .epicurious import Epicurious
@@ -154,6 +155,7 @@ SCRAPERS = {
     Cybercook.host(): Cybercook,
     Delish.host(): Delish,
     DomesticateMe.host(): DomesticateMe,
+    EatingBirdFood.host(): EatingBirdFood,
     Eatsmarter.host(): Eatsmarter,
     Eatsmarter.host(domain="de"): Eatsmarter,
     EatWhatTonight.host(): EatWhatTonight,
