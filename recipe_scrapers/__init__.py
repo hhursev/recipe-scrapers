5315fa76cf419934da9fc3e5cbc4e427e79dc6e4
Brendan Carpio <bfcarpio.git@mailnull.com>
2021-01-21 20:26:45 -0800

    add OneHundredOneCookbooks

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -88,6 +88,7 @@ from .nourishedbynutrition import NourishedByNutrition
 from .nutritionbynathalie import NutritionByNathalie
 from .nytimes import NYTimes
 from .ohsheglows import OhSheGlows
+from .onehundredonecookbooks import OneHundredOneCookBooks
 from .paleorunningmomma import PaleoRunningMomma
 from .panelinha import Panelinha
 from .paninihappy import PaniniHappy
@@ -208,6 +209,7 @@ SCRAPERS = {
     LivelyTable.host(): LivelyTable,
     LeCremeDeLaCrumb.host(): LeCremeDeLaCrumb,
     Lovingitvegan.host(): Lovingitvegan,
+    OneHundredOneCookBooks.host(): OneHundredOneCookBooks,
     PaleoRunningMomma.host(): PaleoRunningMomma,
     RachlMansfield.host(): RachlMansfield,
     SpruceEats.host(): SpruceEats,
