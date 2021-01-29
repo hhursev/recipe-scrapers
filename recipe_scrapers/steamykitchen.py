982fa84b892351af6bf5b39a68ccfe281b9cc32d
Brendan Carpio <bfcarpio.git@mailnull.com>
2021-01-28 18:08:28 -0800

    SteamyKitchen to schema

--- recipe_scrapers/steamykitchen.py
+++ recipe_scrapers/steamykitchen.py
@@ -1,5 +1,4 @@
 from ._abstract import AbstractScraper
-from ._utils import get_minutes, normalize_string, get_yields
 
 
 class SteamyKitchen(AbstractScraper):
@@ -7,36 +6,28 @@ class SteamyKitchen(AbstractScraper):
     def host(cls):
         return "steamykitchen.com"
 
+    def author(self):
+        return self.schema.author()
+
     def title(self):
-        return self.soup.find("h2", {"itemprop": "name"}).get_text()
+        return self.schema.title()
 
     def total_time(self):
-        return sum(
-            [
-                get_minutes(self.soup.find(itemprop="prepTime").parent),
-                get_minutes(self.soup.find(itemprop="cookTime").parent),
-            ]
-        )
+        return self.schema.total_time()
 
     def yields(self):
-        return get_yields(self.soup.find("span", itemprop="recipeYield"))
+        return self.schema.yields()
 
     def image(self):
-        image = self.soup.find("img", {"itemprop": "image", "src": True})
-        return image["src"] if image else None
+        # Recipe section and schema have no image so stealing from the page
+        return self.soup.find("img")["src"]
 
     def ingredients(self):
-        ingredients = self.soup.findAll("span", {"itemprop": "ingredients"})
-
-        return [
-            normalize_string(ingredient.get_text())
-            for ingredient in ingredients
-            if len(normalize_string(ingredient.get_text())) > 0
-        ]
+        return self.schema.ingredients()
 
     def instructions(self):
-        instructions = self.soup.find("div", {"class": "instructions"}).findAll("p")
+        return self.schema.instructions()
 
-        return "\n".join(
-            [normalize_string(instruction.get_text()) for instruction in instructions]
-        )
+    def ratings(self):
+        # Schema has no ratings and I can't see any near the recipe
+        return None
