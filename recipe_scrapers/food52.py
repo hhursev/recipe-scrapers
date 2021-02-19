5f56184465b303fe1f1d7b2e8f9273ee2fbaca44
carl anderson <39501603+weightwatchers-carlanderson@users.noreply.github.com>
2021-02-18 23:45:53 -0500

    New Site: Food52.com (#325)
    * initial commit
    
    * adding marthastewart
    
    * running black on files
    
    * removing dupe import
    
    * black on test file
    
    * CSS changed. updating code to accomodate
    
    * initial commit
    
    Co-authored-by: Carl Anderson <carl.anderson@weightwatchers.com>

---
+++ recipe_scrapers/food52.py
@@ -0,0 +1,42 @@
+from recipe_scrapers._utils import normalize_string, get_minutes
+from recipe_scrapers._abstract import AbstractScraper
+
+
+class Food52(AbstractScraper):
+    @classmethod
+    def host(cls):
+        return "food52.com"
+
+    def title(self):
+        print("title was called")
+        return self.schema.title()
+
+    def total_time(self):
+        ul = self.soup.find("ul", {"class": "recipe__details"})
+        total = 0
+        for li in ul.find_all("li"):
+            if li.span.get_text().lower() in ["prep time", "cook time"]:
+                total += get_minutes(list(li.children)[2].strip())
+        return total
+
+    def yields(self):
+        return self.schema.yields()
+
+    def image(self):
+        return self.schema.image()
+
+    def ingredients(self):
+        return self.schema.ingredients()
+
+    def instructions(self):
+        instructions = self.soup.findAll("li", {"class": "recipe__list-step"})
+
+        return "\n".join(
+            [
+                normalize_string(instruction.span.get_text())
+                for instruction in instructions
+            ]
+        )
+
+    def ratings(self):
+        return self.schema.ratings()
---
+++ tests/test_food52.py
@@ -0,0 +1,69 @@
+from tests import ScraperTest
+
+from recipe_scrapers.food52 import Food52
+
+
+class TestFood52(ScraperTest):
+
+    scraper_class = Food52
+
+    def test_host(self):
+        self.assertEqual("food52.com", self.harvester_class.host())
+
+    def test_canonical_url(self):
+        self.assertEqual(
+            "https://food52.com/recipes/84961-pomegranate-chicken-wings-recipe",
+            self.harvester_class.canonical_url(),
+        )
+
+    def test_title(self):
+        self.assertEqual(
+            self.harvester_class.title(),
+            "Sticky Pomegranate &amp; Black Pepper Chicken Wings",
+        )
+
+    def test_total_time(self):
+        self.assertEqual(1520, self.harvester_class.total_time())
+
+    def test_yields(self):
+        self.assertEqual("Serves 2 to 4", self.harvester_class.yields())
+
+    def test_ingredients(self):
+        self.assertCountEqual(
+            [
+                "2 to 2 1/4 pounds chicken wings",
+                "1 tablespoon kosher salt",
+                "1 tablespoon granulated sugar",
+                "2 teaspoons baking powder",
+                "1/2 teaspoon MSG (optional)",
+                "2 tablespoons neutral oil, such as grapeseed or canola",
+                "1/4 cup granulated sugar",
+                "1/2 cup pomegranate molasses",
+                "2 tablespoons freshly ground black pepper",
+                "2 teaspoons kosher salt",
+                "2 teaspoons ground cinnamon",
+                "1 teaspoon freshly ground nutmeg",
+                "1/2 teaspoon toasted walnut halves, roughly chopped",
+                "Flaky salt (optional)",
+            ],
+            self.harvester_class.ingredients(),
+        )
+
+    def test_instructions(self):
+        return self.assertEqual(
+            "\n".join(
+                [
+                    "Line a sheet pan with foil (for easy cleanup) and set an ovenproof wire rack inside.",
+                    "Brine the wings: Pat the wings dry with a paper towel and place in a large bowl. In a small bowl, stir together the kosher salt, sugar, baking powder, and MSG (if using). Sprinkle this mixture over the wings and toss until evenly coated. Arrange the wings on the wire rack and refrigerate uncovered at least 6 hours or preferably overnight, flipping the wings halfway through. (Wash the bowl and keep it handy—we’ll be using it again.)",
+                    "When you’re ready to roast, heat the oven to 350°F.",
+                    "The first roast: Transfer the wings to the big bowl, toss with the oil until evenly coated, then return them to the wire rack. Roast the wings, flipping them once halfway through, until the skin is lightly browned, the flesh of the drumettes is starting to pull away from the bones, and the fat from the skin has mostly rendered, about 1 hour.",
+                    "Meanwhile, prepare the glaze: Wash the large bowl. Add the sugar and pour 2 tablespoons of boiling water on top, whisking until the sugar dissolves. Add the pomegranate molasses, black pepper, salt, cinnamon, and nutmeg, whisking until combined.",
+                    "The second roast: Remove the wings from the oven and increase the temperature to 375°F. Transfer the wings to the bowl with the glaze and toss to coat. Use tongs to return the wings to the rack, reserving any remaining glaze in the bowl, and roast until lightly charred and glossy, about 20 minutes.",
+                    "Once the wings are out of the oven, transfer them to the bowl and toss again in the remaining glaze. Transfer the wings to a platter and garnish with the walnuts. Sprinkle with flaky salt if desired.",
+                ]
+            ),
+            self.harvester_class.instructions(),
+        )
+
+    def test_ratings(self):
+        self.assertEqual(5.0, self.harvester_class.ratings())
