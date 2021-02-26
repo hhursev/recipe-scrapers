0c271a451be757089db6d71a6d3e39233e0aa7a2
carl anderson <39501603+weightwatchers-carlanderson@users.noreply.github.com>
2021-02-26 05:23:11 -0500

    Enhancing Site: bettycrocker.com (#341)
    * simplifying code, ehancing 3 methods
    
    * cleaner code
    
    * removing unused imports
    
    Co-authored-by: Carl Anderson <carl.anderson@weightwatchers.com>

--- recipe_scrapers/bettycrocker.py
+++ recipe_scrapers/bettycrocker.py
@@ -7,7 +7,7 @@
 from typing import List, Optional
 
 from ._abstract import AbstractScraper
-from ._utils import get_minutes, get_yields, normalize_string
+from ._utils import normalize_string
 
 
 class BettyCrocker(AbstractScraper):
@@ -16,61 +16,43 @@ class BettyCrocker(AbstractScraper):
         return "bettycrocker.com"
 
     def title(self) -> Optional[str]:
-        return self.soup.find("h1").get_text()
+        return self.schema.title()
 
     def total_time(self) -> Optional[int]:
-        total_time = 0
-        tt = self.soup.find("li", {"id": "gmi_rp_primaryAttributes_total"})
-        if tt:
-            tt1 = normalize_string(tt.get_text())
-            tt2 = get_minutes(tt1)
-        total_time = tt2
-        return total_time
+        return self.schema.total_time()
 
     def yields(self) -> Optional[str]:
-        recipe_yield = self.soup.find("li", {"id": "gmi_rp_primaryAttributes_servings"})
-        if recipe_yield:
-            y = recipe_yield.find(attrs={"class": "attributeValue"})
-            valu = y.text + " serving(s)"
-            return valu
-        else:
-            return get_yields(
-                self.soup.find(
-                    "div", {"class": "recipe-adjust-servings__original-serving"}
-                ).get_text()
-            )
+        return self.schema.yields()
 
     def image(self) -> Optional[str]:
-        image = self.soup.find("div", {"class": "recipeImage"})
-        if image:
-            tag = image.find("meta")
-            src = tag.get("content", None)
-
-        return src if image else None
+        return self.schema.image()
 
     def ingredients(self) -> Optional[List[str]]:
-        ingredients = self.soup.findAll("div", {"class": "recipePartIngredient"})
-
-        if not ingredients:
-            ingredients = self.soup.findAll("span", {"class": "ingredients-item-name"})
+        ingredients = self.soup.find(
+            "div", {"class": "recipePartIngredientGroup"}
+        ).ul.findAll("li")
 
         return [
-            normalize_string(ingredient.get_text())
+            normalize_string(
+                ingredient.find("div", {"class": "quantity"}).text
+                + " "
+                + ingredient.find("div", {"class": "description"}).span.text
+            )
             for ingredient in ingredients
-            if ingredient.get_text(strip=True)
-            not in ("Add all ingredients to list", "", "ADVERTISEMENT")
         ]
 
     def instructions(self) -> Optional[str]:
         instructions = self.soup.findAll("li", {"class": "recipePartStep"})
-        retstr = ""
-        for instruction in instructions:
-            instemp = instruction.find("div", {"class": "recipePartStepDescription"})
-            s = normalize_string(instemp.get_text())
-
-            retstr = retstr + s.strip() + " "
-        instructions = retstr.strip()
-        return instructions
+        return "\n".join(
+            [
+                normalize_string(
+                    instruction.find(
+                        "div", {"class": "recipePartStepDescription"}
+                    ).get_text()
+                )
+                for instruction in instructions
+            ]
+        )
 
     def ratings(self) -> Optional[float]:
         r = self.soup.find("span", {"class": "ratingCount"}).get_text()
