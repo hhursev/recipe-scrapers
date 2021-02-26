6b6519e35d86833c707a2201ac2f2c4c7bd25435
carl anderson <39501603+weightwatchers-carlanderson@users.noreply.github.com>
2021-02-26 05:26:58 -0500

    instructions now \n delimited string, adding test_canonical_url (#336)
    Co-authored-by: Carl Anderson <carl.anderson@weightwatchers.com>

--- tests/test_jamieoliver.py
+++ tests/test_jamieoliver.py
@@ -7,6 +7,12 @@ class TestJamieOliverScraper(ScraperTest):
 
     scraper_class = JamieOliver
 
+    def test_canonical_url(self):
+        self.assertEqual(
+            "https://www.jamieoliver.com/recipes/chocolate-recipes/bloomin-brilliant-brownies/",
+            self.harvester_class.canonical_url(),
+        )
+
     def test_host(self):
         self.assertEqual("jamieoliver.com", self.harvester_class.host())
 
@@ -21,29 +27,37 @@ class TestJamieOliverScraper(ScraperTest):
 
     def test_image(self):
         self.assertEqual(
-            "jamieoliver_files/88_1_1441269331.jpg", self.harvester_class.image()
+            "https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/88_1_1441269331.jpg?tr=w-800,h-1066",
+            self.harvester_class.image(),
         )
 
     def test_ingredients(self):
         self.assertCountEqual(
             [
+                "200 g quality dark chocolate (70%)",
                 "250 g unsalted butter",
-                "200 g good-quality dark chocolate (70% cocoa solids) , broken up",
                 "75 g dried sour cherries , optional",
                 "50 g chopped nuts , optional",
-                "80 g cocoa powder , sifted",
-                "65 g plain flour , sifted",
+                "80 g quality cocoa powder",
+                "65 g plain flour",
                 "1 teaspoon baking powder",
                 "360 g caster sugar",
                 "4 large free-range eggs",
-                "zest of 1 orange , optional",
-                "250 ml crème fraîche , optional",
             ],
             self.harvester_class.ingredients(),
         )
 
     def test_instructions(self):
-        self.assertEqual(
-            "Preheat your oven to 180°C/350°F/gas 4. Line a 24cm square baking tin with greaseproof paper. In a large bowl over some simmering water, melt the butter and the chocolate and mix until smooth. Add the cherries and nuts, if you’re using them, and stir together. In a separate bowl, mix together the cocoa powder, flour, baking powder and sugar, then add this to the chocolate, cherry and nut mixture. Stir together well. Beat the eggs and mix in until you have a silky consistency. Pour your brownie mix into the baking tray, and place in the oven for around 25 minutes. You don’t want to overcook them so, unlike cakes, you don’t want a skewer to come out all clean. The brownies should be slightly springy on the outside but still gooey in the middle. Allow to cool in the tray, then carefully transfer to a large chopping board and cut into chunky squares. These make a fantastic dessert served with a dollop of crème fraîche mixed with some orange zest.",
+        return self.assertEqual(
+            "\n".join(
+                [
+                    "Preheat the oven to 180°C/350°F/gas 4. Line a 24cm square baking tin with greaseproof paper.",
+                    "Snap the chocolate into a large bowl, add the butter and place over a pan of simmering water, until melted, stirring regularly. Stir through the cherries and nuts (if using).",
+                    "Sift the cocoa powder and flour into a separate bowl, add the baking powder and sugar, then mix together.",
+                    "Add the dry ingredients to the chocolate, cherry and nut mixture and stir together well. Beat the eggs, then mix in until you have a silky consistency.",
+                    "Pour the brownie mix into the baking tin, and place in the oven for around 25 minutes. You don’t want to overcook them so, unlike cakes, you don’t want a skewer to come out clean – the brownies should be slightly springy on the outside but still gooey in the middle.",
+                    "Allow to cool in the tray, then carefully transfer to a large chopping board and cut into chunky squares. Delicious served with a dollop of crème fraîche or yoghurt mixed with some orange zest.",
+                ]
+            ),
             self.harvester_class.instructions(),
         )
