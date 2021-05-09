74d2e6030cd036b442bf98d00fc34c5775d9e9c2
James Addison <james@reciperadar.com>
2021-05-09 14:42:36 +0100

    afghankitchenrecipes: add scraper implementation

--- recipe_scrapers/afghankitchenrecipes.py
+++ recipe_scrapers/afghankitchenrecipes.py
@@ -1,4 +1,5 @@
 from ._abstract import AbstractScraper
+from ._utils import get_yields
 
 
 class afghankitchenrecipes(AbstractScraper):
@@ -7,25 +8,39 @@ class afghankitchenrecipes(AbstractScraper):
         return "afghankitchenrecipes.com"
 
     def author(self):
-        return self.schema.author()
+        given_name = self.soup.find("h5", {"class": "given-name"})
+        return given_name and given_name.find("a", {"rel": "author"}).get_text()
 
     def title(self):
         return self.schema.title()
 
     def total_time(self):
-        return self.schema.total_time()
+        ready_in = self.soup.find("li", {"class": "ready-in"})
+        if not ready_in:
+            return
+        ready_text = ready_in.find("span", {"class": "value"}).get_text()
+        if not ready_text or not ready_text.endswith("h"):
+            return
+        hours, minutes = ready_text[:-2].split(":")
+        return int(hours) * 60 + int(minutes)
 
     def yields(self):
-        return self.schema.yields()
+        servings = self.soup.find("li", {"class": "servings"})
+        if not servings:
+            return
+        return get_yields(servings)
 
     def image(self):
         return self.schema.image()
 
     def ingredients(self):
-        return self.schema.ingredients()
+        ingredient_elements = self.soup.findAll("li", {"class": "ingredient"})
+        return [element.get_text() for element in ingredient_elements if element.get_text()]
 
     def instructions(self):
-        return self.schema.instructions()
+        instruction_elements = self.soup.findAll("p", {"class": "instructions"})
+        return "\n".join([element.get_text().strip() for element in instruction_elements if element.get_text()])
 
     def ratings(self):
-        return self.schema.ratings()
+        rating = self.soup.find("meta", {"itemprop": "ratingValue"})
+        return rating and rating.get("content") and round(float(rating["content"]), 2)
