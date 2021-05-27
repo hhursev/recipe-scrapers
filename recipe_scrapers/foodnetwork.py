e78515eaf6c481f8c46dfdb786e8e18b71561121
Michael Chiasson <michaeljchiasson@gmail.com>
2021-05-27 09:46:48 -0600

    use ld+json instead to parse recipe

--- recipe_scrapers/foodnetwork.py
+++ recipe_scrapers/foodnetwork.py
@@ -8,33 +8,16 @@ class FoodNetwork(AbstractScraper):
         return "foodnetwork.com"
 
     def title(self):
-        return self.soup.find("h1").get_text().strip()
+        return self.schema.title()
 
     def total_time(self):
-        return get_minutes(
-            self.soup.find("span", {"class": "m-RecipeInfo__a-Description--Total"})
-        )
+        return self.schema.total_time()
 
     def yields(self):
-        return get_yields(
-            self.soup.find("ul", {"class": "o-RecipeInfo__m-Yield"}).find(
-                "span", {"class": "o-RecipeInfo__a-Description"}
-            )
-        )
+        return self.schema.yields()
 
     def ingredients(self):
-        ingredients = self.soup.findAll(lambda tag: tag.name == 'p' and tag.get('class') == ['o-Ingredients__a-Ingredient'])
-
-        return [normalize_string(ingredient.get_text()) for ingredient in ingredients]
+        return self.schema.ingredients()
 
     def instructions(self):
-        instructions = self.soup.findAll("li", {"class": "o-Method__m-Step"})
-
-        return "\n".join(
-            [
-                normalize_string(instruction.get_text())
-                for instruction in instructions
-                if instruction.get_text(strip=True)
-                not in ("Watch how to make this recipe.",)
-            ]
-        )
+        return self.schema.instructions()
