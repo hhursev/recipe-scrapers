513d0bea080def8ec2e4954dc5f54b457eb33b49
Ryan Brennan <rbren90@gmail.com>
2021-06-13 14:35:53 +0100

    Use schema for recipe & update test

--- recipe_scrapers/gousto.py
+++ recipe_scrapers/gousto.py
@@ -1,5 +1,4 @@
 from ._abstract import AbstractScraper
-from ._utils import get_minutes, get_yields, normalize_string
 
 
 class Gousto(AbstractScraper):
@@ -7,66 +6,23 @@ class Gousto(AbstractScraper):
     def host(cls):
         return "gousto.co.uk"
 
-    def title(self):  # div[class*="RecipeHero_title"]>h1
-        return self.soup.select_one('div[class*="RecipeHero_title"]>h1').text
+    def title(self):
+        return self.schema.title()
 
     def total_time(self):
-        return get_minutes(
-            self.soup.find("p", {"class": "recipehighlight-box-value"}).parent
-        )
+        return self.schema.total_time()
 
     def yields(self):
-        yields = (
-            self.soup.find("div", {"id": "ingredients"})
-            .find("div", {"class": "panel-subheading"})
-            .get_text()
-        )
+        return self.schema.yields()
 
-        return get_yields("{} servings".format(yields))
+    def image(self):
+        return self.schema.image()
 
     def ingredients(self):
-        # TODO: Fix append
-        container = self.soup.find("div", {"id": "ingredients"})
-
-        ingredients = container.find_all(
-            "figcaption", {"class": "indivrecipe-ingredients-text"}
-        )
-
-        extras = (
-            container.find("div", {"class": "panel indivrecipe-panel"})
-            .find("div", {"class": "panel-subheading"})
-            .get_text()
-        )
-
-        extras = extras.strip().split(", ")
-
-        ingredients = [
-            normalize_string(ingredient.get_text()) for ingredient in ingredients
-        ]
-
-        for extra in extras:
-            ingredients.append(extra)
-
-        return ingredients
+        return self.schema.ingredients()
 
     def instructions(self):
-        instructions = self.soup.find("div", {"id": "instructions"}).find_all(
-            "div", {"class": "indivrecipe-cooking-text-wrapper"}
-        )
-
-        instr_container = [
-            p.get_text() for subdiv in instructions for p in subdiv.findAll("p")
-        ]
-
-        return "\n".join(
-            normalize_string(instruction) for instruction in instr_container
-        )
+        return self.schema.instructions()
 
     def ratings(self):
-        ratings = self.soup.find("div", {"class": "star-overflow"}).find(
-            "img", alt=True
-        )
-
-        rating = int(ratings["alt"][0])
-
-        return rating
+        return self.schema.ratings()
--- tests/test_gousto.py
+++ tests/test_gousto.py
@@ -10,9 +10,9 @@ class TestGoustoScraper(ScraperTest):
         self.assertEqual("gousto.co.uk", self.harvester_class.host())
 
     def test_canonical_url(self):
+        print(self.harvester_class.canonical_url())
         self.assertEqual(
-            "https://www.gousto.co.uk/cookbook/pork-recipes/creamy-pork-tagliatelle",
-            self.harvester_class.canonical_url(),
+            "https://test.example.com/", self.harvester_class.canonical_url()
         )
 
     def test_title(self):
@@ -25,27 +25,28 @@ class TestGoustoScraper(ScraperTest):
         self.assertEqual("2 serving(s)", self.harvester_class.yields())
 
     def test_ingredients(self):
+        print("ingredients list", self.harvester_class.ingredients())
         self.assertCountEqual(
             [
                 "1 onion",
                 "150g chestnut mushrooms",
                 "1 garlic clove",
                 "10g fresh parsley",
-                "30g rennet-free parmesan †",
+                "30g rennet-free parmesan",
                 "2 British pork loin steaks",
-                "1 pot of double cream (227ml) †",
-                "1/2 beef stock cube †",
-                "200g linguine †",
-                "Olive oil",
-                "pepper",
-                "salt",
+                "1 pot of double cream (227ml)",
+                "1/2 beef stock cube",
+                "200g linguine"
+                # "Olive oil",
+                # "pepper",
+                # "salt",
             ],
             self.harvester_class.ingredients(),
         )
 
     def test_instructions(self):
         return self.assertEqual(
-            "Preheat the oven to 200°C/ 180°C (fan)/ 400°F/ Gas 6\nBoil a kettle (used in steps 3 & 5)\nAdd 1-2 tbsp of olive oil to a pan on a high heat\nOnce the oil is hot, sear the pork tenderloin for 2 min on each side, or until browned all over (keep the pan for step 6)\nSeason to your taste with salt and pepper\nPlace the pork on an oven-proof tray and put in the oven for 8-10 min, or until cooked through\nOnce done, remove from the oven, cover well and allow to rest until step 7\nDissolve half the (whole) beef stock cube in 100ml (200ml) of boiling water\nCut the mushrooms into quarters\nPeel the onion(s) and chop finely\nCrush the garlic with the side of a knife, peel it and chop finely\nChop the parsley finely\nCook the pasta in a pot of boiling water and salt (optional) for 8-10 min or until the pasta is cooked to your taste, stirring occasionally, then drain\nReturn the pork pan to a medium-high heat, add the garlic and onion and cook for 1 min\nAdd the mushrooms and cook for 1 min\nReduce the heat, add the stock and 50ml (100ml) of cream and simmer for 5 min\nAdd the parsley\nGrate the parmesan\nCut the pork into slices (approx. 2cm)\nSeason the mushroom sauce to your taste with salt and pepper\nAdd the pasta to the sauce and mix well\nServe the pasta with the pork on top and sprinkle with parmesan\nEnjoy!",
+            "Preheat the oven to 200°C/ 180°C (fan)/ 400°F/ Gas 6 Boil a kettle (used in steps 3 & 5)\nAdd 1-2 tbsp of olive oil to a pan on a high heat Once the oil is hot, sear the pork tenderloin for 2 min on each side, or until browned all over (keep the pan for step 6) Season to your taste with salt and pepper\nPlace the pork on an oven-proof tray and put in the oven for 8-10 min, or until cooked through Once done, remove from the oven, cover well and allow to rest until step 7 Dissolve half the (whole) beef stock cube in 100ml (200ml) of boiling water\nCut the mushrooms into quarters Peel the onion(s) and chop finely Crush the garlic with the side of a knife, peel it and chop finely Chop the parsley finely\nCook the pasta in a pot of boiling water and salt (optional) for 8-10 min or until the pasta is cooked to your taste, stirring occasionally, then drain\nReturn the pork pan to a medium-high heat, add the garlic and onion and cook for 1 min Add the mushrooms and cook for 1 min Reduce the heat, add the stock and 50ml (100ml) of cream and simmer for 5 min Add the parsley\nGrate the parmesan Cut the pork into slices (approx. 2cm) Season the mushroom sauce to your taste with salt and pepper\nAdd the pasta to the sauce and mix well Serve the pasta with the pork on top and sprinkle with parmesan Enjoy!",
             self.harvester_class.instructions(),
         )
 
