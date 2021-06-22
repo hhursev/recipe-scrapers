baa6d3a49485782aef1f302e4911e00119afe1cf
Micah Cochran <micah_coch@yahoo.com>
2021-06-21 19:42:34 -0500

    add image(), return no content when there is none

--- recipe_scrapers/nihhealthyeating.py
+++ recipe_scrapers/nihhealthyeating.py
@@ -8,15 +8,22 @@ class NIHHealthyEating(AbstractScraper):
         return "healthyeating.nhlbi.nih.gov"
 
     def title(self):
+        # This content must be present for all recipes on this website.
         return self.soup.h1.get_text().strip()
 
     def total_time(self):
+        # This content must be present for all recipes on this website.
         time_table = self.soup.find("table", {"class": "recipe_time_table"})
 
-        return sum([get_minutes(td) for td in time_table.find_all("td")])
+        if time_table is not None:
+            return sum([get_minutes(td) for td in time_table.find_all("td")])
 
     def yields(self):
+        # This content must be present for all recipes on this website.
         time_table = self.soup.find("table", {"class": "recipe_time_table"})
+
+        if time_table is None:
+            return
 
         i = 0
         for t in time_table.findAll("th"):
@@ -28,15 +35,44 @@ class NIHHealthyEating(AbstractScraper):
             return
         return get_yields(time_table.find_all("td")[i])
 
-    def ingredients(self):
-        ingredients = self.soup.find("div", {"id": "ingredients"}).findAll("p")
+    def image(self):
+        # Optional content recipes on this website.
+        img = self.soup.find("img", {"class": "recipe_image", "src": True})
 
-        return [normalize_string(paragraph.get_text()) for paragraph in ingredients]
+        if img is None:
+            return " "
+
+        image_relative_url = img.get("src")
+
+        if image_relative_url is not None:
+            image_relative_url = f"https://{self.host()}{image_relative_url}"
+        else:
+            image_relative_url = " "
+
+        return image_relative_url
+
+    def ingredients(self):
+        # This content must be present for recipes on this website.
+        ingredients_div = self.soup.find("div", {"id": "ingredients"})
+
+        if ingredients_div is None:
+            return []
+
+        ingredients_p = ingredients_div.findAll("p")
+        ingredients = [normalize_string(para.get_text()) for para in ingredients_p]
+
+        return [
+            ing for ing in ingredients if not ing.lower().startswith("recipe cards")
+        ]
 
     def instructions(self):
-        instructions = self.soup.find("div", {"id": "recipe_directions"}).findAll(
-            "div", {"class": "steptext"}
-        )
+        # This content must be present for recipes on this website.
+        directions_div = self.soup.find("div", {"id": "recipe_directions"})
+
+        if directions_div is None:
+            return ""
+
+        instructions = directions_div.findAll("div", {"class": "steptext"})
 
         return "\n".join(
             [normalize_string(instruction.get_text()) for instruction in instructions]
--- tests/test_nih.py
+++ tests/test_nih.py
@@ -2,7 +2,7 @@ from recipe_scrapers.nihhealthyeating import NIHHealthyEating
 from tests import ScraperTest
 
 # test recipe's URL
-# https://healthyeating.nhlbi.nih.gov/recipedetail.aspx?linkId=17&cId=10&rId=247
+# https://healthyeating.nhlbi.nih.gov/recipedetail.aspx?cId=0&rId=188
 
 
 class TestNIHHealthyEatingRecipesScraper(ScraperTest):
@@ -13,32 +13,40 @@ class TestNIHHealthyEatingRecipesScraper(ScraperTest):
         self.assertEqual("healthyeating.nhlbi.nih.gov", self.harvester_class.host())
 
     def test_title(self):
-        self.assertEqual(
-            self.harvester_class.title(), "Vietnamese Fresh Spring Rolls (Gỏi Cuốn)"
-        )
+        self.assertEqual(self.harvester_class.title(), "Baked Tilapia With Tomatoes")
 
     def test_total_time(self):
-        self.assertEqual(15, self.harvester_class.total_time())
+        self.assertEqual(35, self.harvester_class.total_time())
 
     def test_yields(self):
-        self.assertEqual("8 serving(s)", self.harvester_class.yields())
+        self.assertEqual("4 serving(s)", self.harvester_class.yields())
+
+    def test_image(self):
+        self.assertEqual(
+            "https://healthyeating.nhlbi.nih.gov/images/food/Baked_Tilapia_With_Tomatoes.jpg",
+            self.harvester_class.image(),
+        )
 
     def test_ingredients(self):
         self.assertCountEqual(
             [
-                "1 C carrots, cut into long, thin strips",
-                "2 C bean sprouts",
-                "2 C cucumber, seeded and cut into long, thin strips",
-                "1 C minced scallions",
-                "½ C chopped fresh cilantro",
-                "¼ C chopped fresh mint",
-                "8 rice paper wrappers",
+                "Cooking spray",
+                "4 tilapia fillets",
+                "4 medium tomatoes, peeled and chopped",
+                "2 Tbsp olive oil",
+                "1½ tsp thyme",
+                "¼ C pitted black olives, diced",
+                "¼ tsp red pepper flakes",
+                "2 cloves garlic, minced",
+                "½ C red onion, diced",
+                "1 Tbsp lime juice",
+                "Parsley and lemon wedges for garnish",
             ],
             self.harvester_class.ingredients(),
         )
 
     def test_instructions(self):
         return self.assertEqual(
-            "Toss first six ingredients in a large bowl.\nSoak one rice paper wrapper in warm water until soft (1 to 2 minutes). Shake off excess water.\nPlace vegetable filling off-center on rice paper, and fold like an egg roll (tuck in the sides to keep the filling inside).\nRepeat with remaining vegetable filling and rice paper wrappers.\nOnce you have assembled all of the spring rolls, serve immediately.",
+            "Preheat oven to 400 °F.\nSpray baking dish with cooking spray.\nArrange fillets in the baking dish. The dish should be large enough so the fillets do not overlap.\nMix remaining ingredients, except parsley and lemon wedges, in a bowl.\nSpoon the tomato mixture evenly over the fillets.\nBake uncovered 15 to 20 minutes or until the fish flakes easily with a fork.\nGarnish with parsley and a lemon wedge, and serve.",
             self.harvester_class.instructions(),
         )
