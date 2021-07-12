46372baa09e0c53ceae7561449685570cbbedaef
James Addison <james@reciperadar.com>
2021-07-12 22:28:06 +0100

    Add scraper for jimcooksfoodgood.com

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -73,6 +73,7 @@ from .indianhealthyrecipes import IndianHealthyRecipes
 from .innit import Innit
 from .inspiralized import Inspiralized
 from .jamieoliver import JamieOliver
+from .jimcooksfoodgood import JimCooksFoodGood
 from .joyfoodsunshine import Joyfoodsunshine
 from .justbento import JustBento
 from .kennymcgovern import KennyMcGovern
@@ -235,6 +236,7 @@ SCRAPERS = {
     Innit.host(): Innit,
     Inspiralized.host(): Inspiralized,
     JamieOliver.host(): JamieOliver,
+    JimCooksFoodGood.host(): JimCooksFoodGood,
     Joyfoodsunshine.host(): Joyfoodsunshine,
     JustBento.host(): JustBento,
     KennyMcGovern.host(): KennyMcGovern,
