728845e250270383e9f4f8fa9302d501fa4b84f3
kev <kprichard@gmail.com>
2021-07-18 16:06:20 -0700

    Adds Allrecipes extended nutrient data, linked as 'Full Nutrition' on allrecipes.com pages
    ### Details
    - adds the extended nutrient data, available on some AllRecipes pages (linked as 'Full Nutrition'), to AllRecipes.nutrients()
    - adds AllRecipes.nutrients_unitized(), which breaks out measure values and measure units into a tuple, e.g. {.. "vitamin a iu": [882.5, "IU"], "saturated fat%": [55.0, "RDA"], ..}
    - adds AllRecipes.to_dict() so that instances can be more easily serialized
    
    #### Unitizing
    - most extended nutrient rows are parsed into (float, str)
    - some extended nutrient rows contain a third value, typically with a trailing percent symbol (r"\d+%"), which this patch removes and reattaches as an additional nutrient entry, like: "<nutrient_name>%": [<nutrient_value>, "RDA"] (because "%" is believed to mean "percent of recommended daily allowance"; see USDA RDA: https://www.nal.usda.gov/fnic/what-are-recommended-dietary-allowances-rdas-and-dietary-reference-intakes-dris))
    - rows with "calories" in the name get their unit set to "calories"
    
    ### Testing
    - adds TestAllRecipesScraper.nutrients()
    - adds TestAllRecipesScraper.nutrients_unitized()

--- recipe_scrapers/allrecipes.py
+++ recipe_scrapers/allrecipes.py
@@ -1,4 +1,21 @@
 from ._abstract import AbstractScraper
+import inspect
+import logging
+import re
+
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+
+
+UNITIZERX = re.compile(r"^([0-9.]+)\s*([^0-9.]*)$")
+EXT_NUTRS_SEL = "section.recipe-nutrition.nutrition-section div.nutrition-row"
+EXT_NUTRS_CLASH = "Extended nutrient name clashes with basic nutrient:" \
+                  "%s = %s vs %s"
+EXT_NUTR_PCT = "Got unit type from a percentage value: %s = %s (%s)"
+DICT_FIELDS = ['author', 'canonical_url', 'image', 'ingredients',
+               'instructions', 'language', 'links', 'ratings',
+               'site_name', 'title', 'total_time', 'url', 'yields']
 
 
 class AllRecipes(AbstractScraper):
@@ -40,3 +57,74 @@ class AllRecipes(AbstractScraper):
 
     def ratings(self):
         return self.schema.ratings()
+
+    def nutrients(self):
+        def unhandled(n):
+            logger.warn(f"Unhandled AllRecipes extended nutrient format: {n}")
+
+        # Find extended nutrients
+        ext_nutrs = {}
+        for node in self.soup.select(EXT_NUTRS_SEL):
+            tupl = list(node.stripped_strings)
+            if len(tupl) >= 2:
+                name = tupl[0].strip(':')
+                ext_nutrs[name] = tupl[1]
+                if len(tupl) == 3:
+                    if '%' in tupl[2]:
+                        ext_nutrs[name + '%'] = tupl[2].strip(' %')
+                    else:
+                        unhandled(node)
+            else:
+                unhandled(node)
+
+        # Marry basic and extended nutrients, reporting name clashes
+        nutr = self.schema.nutrients()
+        for name, value in ext_nutrs.items():
+            if name in nutr:
+                logger.warn(EXT_NUTRS_CLASH, name, value, ext_nutrs[name])
+                nutr[f"Ext {name}"] = value
+            else:
+                nutr[name] = value
+
+        return nutr
+
+    def nutrients_unitized(self):
+        unitized = {}
+        for name, value in self.nutrients().items():
+            try:
+                new_value = UNITIZERX.match(value).groups()
+            except AttributeError as excep:
+                new_value = (value, None)
+            if name.endswith('%'):
+                if new_value[1]:
+                    logger.warn(EXT_NUTR_PCT, name, value, str(new_value[1]))
+                else:
+                    new_value = (value, "RDA")
+
+            # Special cases: transfer unit types found in name
+            if not new_value[1]:
+                if 'calories' in name:
+                    new_value = (value, "calories")
+            unitized[name] = (float(new_value[0]), new_value[1])
+        return unitized
+
+    def to_dict(self, html=False, unitized=False, skip_attribs=None):
+        obj = {}
+        for attrib_name in DICT_FIELDS:
+            if skip_attribs and attrib_name in skip_attribs:
+                continue
+            attrib = getattr(self.__class__, attrib_name, None)
+            if attrib:
+                if inspect.isfunction(attrib) or inspect.ismethod(attrib):
+                    obj[attrib_name] = attrib(self)
+                else:
+                    obj[attrib_name] = attrib
+            else:
+                logger.warn("Expected attrib not found: %s", attrib_name)
+        if html:
+            obj['html'] = str(self.soup)
+        if unitized:
+            obj['nutrients'] = self.nutrients_unitized()
+        else:
+            obj['nutrients'] = self.nutrients()
+        return obj
