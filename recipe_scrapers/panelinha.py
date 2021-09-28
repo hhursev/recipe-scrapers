c243acaf1345cd5946a9b07145d6aefed8718ec7
Leonardo Fedalto <lfedalto@gmail.com>
2021-09-28 16:24:23 -0300

    Handle edge case for panelinha.com.br
    This fixes the scrapper for some recipes that follows a different html structure.

--- recipe_scrapers/panelinha.py
+++ recipe_scrapers/panelinha.py
@@ -1,5 +1,9 @@
+import re
+
 from ._abstract import AbstractScraper
 from ._utils import get_minutes, normalize_string
+
+INSTRUCTIONS_NUMBERING_REGEX = re.compile(r"^\d{1,2}\.\s*")
 
 
 class Panelinha(AbstractScraper):
@@ -26,10 +30,24 @@ class Panelinha(AbstractScraper):
         instructions = self.soup.find(
             "h4", string="Modo de preparo"
         ).nextSibling.findAll("li")
+        instructions = [
+            normalize_string(instruction.get_text()) for instruction in instructions
+        ]
 
-        return "\n".join(
-            [normalize_string(instruction.get_text()) for instruction in instructions]
-        )
+        # Some recipes pages have a different html structure.
+        if not instructions:
+            instructions = self.soup.find(
+                "h4", string="Modo de preparo"
+            ).nextSibling.p.contents
+            instructions = filter(lambda x: isinstance(x, str), instructions)
+            instructions = [
+                normalize_string(instruction.get_text()) for instruction in instructions
+            ]
+            instructions = map(
+                lambda step: INSTRUCTIONS_NUMBERING_REGEX.sub("", step), instructions
+            )
+
+        return "\n".join(instructions)
 
     def yields(self):
         return normalize_string(
