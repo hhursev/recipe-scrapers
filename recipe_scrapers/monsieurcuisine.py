3a920cbc254e503cb8aae3b9439eb08860c9dd7f
oncleben31 <oncleben31@users.noreply.github.com>
2021-10-04 12:28:24 +0200

    Remove code protection
    As discussed in PR review. Objective is to have high code coverage and
    let the error raised if somemthing change on website side.

--- recipe_scrapers/monsieurcuisine.py
+++ recipe_scrapers/monsieurcuisine.py
@@ -9,8 +9,6 @@ class MonsieurCuisine(AbstractScraper):
 
     def author(self):
         container = self.soup.find("p", {"class": "recipe_author"})
-        if not container:
-            return None
 
         return container.span.get_text().strip()
 
@@ -21,8 +19,6 @@ class MonsieurCuisine(AbstractScraper):
         container = self.soup.find("div", {"class": "duration-information"}).find(
             "div", {"class": "recipe-duration-total"}
         )
-        if not container:
-            return None
 
         minutes = get_minutes(container.b.get_text().replace(":", "h"))
         return minutes
@@ -31,15 +27,11 @@ class MonsieurCuisine(AbstractScraper):
         container = self.soup.find("div", {"class": "recipe--info"}).find(
             "div", {"class": "recipe-portions"}
         )
-        if not container:
-            return None
 
         return get_yields(container.get_text())
 
     def image(self):
         container = self.soup.find("meta", {"property": "og:image"})
-        if not container:
-            return None
 
         return container.get("content")
 
@@ -47,8 +39,6 @@ class MonsieurCuisine(AbstractScraper):
         container = self.soup.find(
             "div", {"class": "recipe--ingredients-html-item col-md-8"}
         )
-        if not container:
-            return None
 
         ingredients = container.findAll("li")
 
@@ -56,8 +46,6 @@ class MonsieurCuisine(AbstractScraper):
 
     def instructions(self):
         container = self.soup.find("div", {"class": "recipe--instructions"})
-        if not container:
-            return None
 
         instructions = container.find_all("li")
 
@@ -67,8 +55,6 @@ class MonsieurCuisine(AbstractScraper):
 
     def ratings(self):
         container = self.soup.find("div", {"class": "recipe--rating"})
-        if not container:
-            return None
 
         full_stars = container.find_all("span", {"class": "mc-star_filled"})
         half_stars = container.find_all("span", {"class": "mc-half_star_thick"})
