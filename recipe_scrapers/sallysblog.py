02ed4b164346c17e54f53786e05ee042cba3d096
Jan Philipp Ecker <info@jan-ecker.eu>
2021-10-19 20:45:30 +0200

    Removed unused dependency from sallys-blog.de scraper

--- recipe_scrapers/sallysblog.py
+++ recipe_scrapers/sallysblog.py
@@ -1,5 +1,5 @@
 from ._abstract import AbstractScraper
-from ._utils import get_minutes, get_yields, normalize_string
+from ._utils import get_minutes, normalize_string
 
 
 class SallysBlog(AbstractScraper):
