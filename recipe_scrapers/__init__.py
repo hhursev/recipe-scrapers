516e7f47d42f5a6658db99b410e745f91db23ad3
Jan Philipp Ecker <info@jan-ecker.eu>
2021-10-20 05:04:46 +0200

    Added support for sallys-blog.de (#442)
    * Added support for sallys-blog.de
    
    * Added missing test for sallys-blog.de total time
    
    * Fixed formatting and string handling of sallys-blog.de scraper
    
    * Removed unused dependency from sallys-blog.de scraper

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -123,6 +123,7 @@ from .recipietineats import RecipieTinEats
 from .redhousespice import RedHouseSpice
 from .reishunger import Reishunger
 from .sallysbakingaddiction import SallysBakingAddiction
+from .sallysblog import SallysBlog
 from .saveur import Saveur
 from .seriouseats import SeriousEats
 from .simplyquinoa import SimplyQuinoa
@@ -295,6 +296,7 @@ SCRAPERS = {
     RedHouseSpice.host(): RedHouseSpice,
     Reishunger.host(): Reishunger,
     SallysBakingAddiction.host(): SallysBakingAddiction,
+    SallysBlog.host(): SallysBlog,
     Saveur.host(): Saveur,
     SeriousEats.host(): SeriousEats,
     SimplyQuinoa.host(): SimplyQuinoa,
