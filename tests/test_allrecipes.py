21dbba762381264a5d2c918efb13a1759d2673a6
Hristo Harsev <hhursev@gmail.com>
2021-11-27 18:11:34 +0200

    Fix black --check complaints

--- tests/test_allrecipes.py
+++ tests/test_allrecipes.py
@@ -15,7 +15,7 @@ class TestAllRecipesScraper(ScraperTest):
     def test_canonical_url(self):
         self.assertEqual(
             "https://www.allrecipes.com/recipe/133948/four-cheese-margherita-pizza/",
-            self.harvester_class.canonical_url()
+            self.harvester_class.canonical_url(),
         )
 
     def test_title(self):
@@ -36,7 +36,7 @@ class TestAllRecipesScraper(ScraperTest):
     def test_image(self):
         self.assertEqual(
             "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F694708.jpg",
-            self.harvester_class.image()
+            self.harvester_class.image(),
         )
 
     def test_ingredients(self):
@@ -53,13 +53,13 @@ class TestAllRecipesScraper(ScraperTest):
                 "½ cup freshly grated Parmesan cheese",
                 "½ cup crumbled feta cheese",
             ],
-            self.harvester_class.ingredients()
+            self.harvester_class.ingredients(),
         )
 
     def test_instructions(self):
         return self.assertEqual(
             "Stir together olive oil, garlic, and salt; toss with tomatoes, and allow to stand for 15 minutes. Preheat oven to 400 degrees F (200 degrees C).\nBrush each pizza crust with some of the tomato marinade. Sprinkle the pizzas evenly with Mozzarella and Fontina cheeses. Arrange tomatoes overtop, then sprinkle with shredded basil, Parmesan, and feta cheese.\nBake in preheated oven until the cheese is bubbly and golden brown, about 10 minutes.",
-            self.harvester_class.instructions()
+            self.harvester_class.instructions(),
         )
 
     def test_ratings(self):
@@ -69,6 +69,6 @@ class TestAllRecipesScraper(ScraperTest):
         self.assertEqual("", self.harvester_class.cuisine())
 
     def test_category(self):
-        self.assertEqual("World Cuisine Recipes,European,Italian",
-            self.harvester_class.category()
+        self.assertEqual(
+            "World Cuisine Recipes,European,Italian", self.harvester_class.category()
         )
