f8c752cc2a630a1d02f2b67f65b12914b3569034
Hristo Harsev <hhursev@gmail.com>
2022-02-19 19:02:39 +0200

    Update koket.se to use the style of the project. Closes #476

--- recipe_scrapers/koket.py
+++ recipe_scrapers/koket.py
@@ -1,6 +1,3 @@
-import json
-import re
-
 from ._abstract import AbstractScraper
 
 
@@ -25,38 +22,10 @@ class Koket(AbstractScraper):
         return self.schema.image()
 
     def ingredients(self):
-        jsData = re.search(
-            r'(?<=<script id="__NEXT_DATA__" type="application\/json">)(.*?)(?=<\/script>)',
-            str(self.soup),
-        )
-        data = json.loads(jsData.group(0))
-        return data["props"]["pageProps"]["item"]["ingredients"]
+        return self.schema.ingredients()
 
     def instructions(self):
-        instructions = []
-        for instruction in self.soup.find("section", {"id": "step-by-step"}).find_all(
-            "span"
-        ):
-            try:
-                header = instruction.find("b")
-                instructions.append({"type": "header", "name": header.get_text()})
-                instructions.append(
-                    {"type": "instruction", "name": header.next_sibling.next_sibling}
-                )
-            except AttributeError:
-                instructions.append(
-                    {"type": "instruction", "name": instruction.get_text()}
-                )
-        return instructions
+        return self.schema.instructions()
 
     def ratings(self):
-        jsData = re.search(
-            r'(?<=<script id="__NEXT_DATA__" type="application\/json">)(.*?)(?=<\/script>)',
-            str(self.soup),
-        )
-        data = json.loads(jsData.group(0))
-        data = data["props"]["pageProps"]["item"]
-        return {
-            "rating_value": data["rating_value"],
-            "rating_count": data["rating_count"],
-        }
+        return self.schema.ratings()
--- tests/test_koket.py
+++ tests/test_koket.py
@@ -10,311 +10,46 @@ class TestKoketScraper(ScraperTest):
         self.assertEqual("koket.se", self.harvester_class.host())
 
     def test_author(self):
-        self.assertEqual("Agnes Fredriksson", self.harvester_class.author())
+        self.assertEqual("Tommy Myllymäki", self.harvester_class.author())
 
     def test_title(self):
-        self.assertEqual(
-            "Blommande äpple med salt kolasås", self.harvester_class.title()
-        )
+        self.assertEqual("Myllymäkis toast skagen", self.harvester_class.title())
 
     def test_total_time(self):
-        self.assertEqual(30, self.harvester_class.total_time())
+        self.assertEqual(25, self.harvester_class.total_time())
 
     def test_yields(self):
         self.assertEqual("4 serving(s)", self.harvester_class.yields())
 
     def test_image(self):
         self.assertEqual(
-            "https://img.koket.se/standard-mega/blommande-apple-med-salt-kolasas.jpg",
+            "https://img.koket.se/standard-mega/myllymakis-toast-skagen-2.jpg",
             self.harvester_class.image(),
         )
 
     def test_ingredients(self):
-
-        json_data = [
-            {
-                "id": None,
-                "name": "Blommande äpple",
-                "plural": None,
-                "type": "header",
-                "preparation": None,
-                "comment": None,
-                "alternative_amount": None,
-                "unit": None,
-                "amount_info": None,
-            },
-            {
-                "id": 9515,
-                "name": "äpple",
-                "plural": "äpplen",
-                "type": "ingredient",
-                "preparation": None,
-                "comment": "fast sort",
-                "alternative_amount": None,
-                "unit": None,
-                "amount_info": {
-                    "to": None,
-                    "from": 4,
-                    "approximate": None,
-                    "optional": None,
-                },
-            },
-            {
-                "id": 9873,
-                "name": "gräddkola",
-                "plural": "gräddkolor",
-                "type": "ingredient",
-                "preparation": None,
-                "comment": "Werthers",
-                "alternative_amount": None,
-                "unit": None,
-                "amount_info": {
-                    "to": None,
-                    "from": 4,
-                    "approximate": None,
-                    "optional": None,
-                },
-            },
-            {
-                "id": 10709,
-                "name": "smör",
-                "plural": None,
-                "type": "ingredient",
-                "preparation": None,
-                "comment": None,
-                "alternative_amount": None,
-                "unit": "g",
-                "amount_info": {
-                    "to": None,
-                    "from": 50,
-                    "approximate": None,
-                    "optional": None,
-                },
-            },
-            {
-                "id": 10658,
-                "name": "salt",
-                "plural": None,
-                "type": "ingredient",
-                "preparation": None,
-                "comment": None,
-                "alternative_amount": None,
-                "unit": "krm",
-                "amount_info": {
-                    "to": None,
-                    "from": 1,
-                    "approximate": True,
-                    "optional": None,
-                },
-            },
-            {
-                "id": 10290,
-                "name": "malen kanel",
-                "plural": None,
-                "type": "ingredient",
-                "preparation": None,
-                "comment": None,
-                "alternative_amount": None,
-                "unit": "tsk",
-                "amount_info": {
-                    "to": None,
-                    "from": 2,
-                    "approximate": None,
-                    "optional": None,
-                },
-            },
-            {
-                "id": 10620,
-                "name": "rörsocker",
-                "plural": None,
-                "type": "ingredient",
-                "preparation": None,
-                "comment": None,
-                "alternative_amount": None,
-                "unit": "msk",
-                "amount_info": {
-                    "to": None,
-                    "from": 4,
-                    "approximate": None,
-                    "optional": None,
-                },
-            },
-            {
-                "id": 10878,
-                "name": "valnötter",
-                "plural": None,
-                "type": "ingredient",
-                "preparation": None,
-                "comment": None,
-                "alternative_amount": None,
-                "unit": None,
-                "amount_info": {
-                    "to": None,
-                    "from": 10,
-                    "approximate": True,
-                    "optional": None,
-                },
-            },
-            {
-                "id": 10882,
-                "name": "vaniljglass",
-                "plural": None,
-                "type": "ingredient",
-                "preparation": None,
-                "comment": None,
-                "alternative_amount": None,
-                "unit": None,
-                "amount_info": None,
-            },
-            {
-                "id": None,
-                "name": "Kolasås",
-                "plural": None,
-                "type": "header",
-                "preparation": None,
-                "comment": None,
-                "alternative_amount": None,
-                "unit": None,
-                "amount_info": None,
-            },
-            {
-                "id": 10919,
-                "name": "vispgrädde",
-                "plural": None,
-                "type": "ingredient",
-                "preparation": None,
-                "comment": None,
-                "alternative_amount": None,
-                "unit": "dl",
-                "amount_info": {
-                    "to": None,
-                    "from": 2.5,
-                    "approximate": None,
-                    "optional": None,
-                },
-            },
-            {
-                "id": 10767,
-                "name": "strösocker",
-                "plural": None,
-                "type": "ingredient",
-                "preparation": None,
-                "comment": None,
-                "alternative_amount": None,
-                "unit": "msk",
-                "amount_info": {
-                    "to": None,
-                    "from": 3,
-                    "approximate": None,
-                    "optional": None,
-                },
-            },
-            {
-                "id": 10692,
-                "name": "sirap",
-                "plural": None,
-                "type": "ingredient",
-                "preparation": None,
-                "comment": None,
-                "alternative_amount": None,
-                "unit": "dl",
-                "amount_info": {
-                    "to": None,
-                    "from": 0.5,
-                    "approximate": None,
-                    "optional": None,
-                },
-            },
-            {
-                "id": 10658,
-                "name": "salt",
-                "plural": None,
-                "type": "ingredient",
-                "preparation": None,
-                "comment": None,
-                "alternative_amount": None,
-                "unit": "tsk",
-                "amount_info": {
-                    "to": None,
-                    "from": 1,
-                    "approximate": None,
-                    "optional": None,
-                },
-            },
-            {
-                "id": 10709,
-                "name": "smör",
-                "plural": None,
-                "type": "ingredient",
-                "preparation": None,
-                "comment": None,
-                "alternative_amount": None,
-                "unit": "msk",
-                "amount_info": {
-                    "to": None,
-                    "from": 1,
-                    "approximate": None,
-                    "optional": None,
-                },
-            },
-        ]
         self.assertEqual(
-            [str(json_element) for json_element in json_data],
+            [
+                "1 kg räkor med skal (gärna färska av fin kvalitet)",
+                "2 äggulor",
+                "2 tsk senap",
+                "1 msk vitvinsvinäger",
+                "6 dl matolja",
+                "1 kruka dill",
+                "10 cm färsk pepparrot, skalad",
+                "4 skiva vitt bröd (ej levain)",
+                "smör, till stekning",
+                "50 g löjrom",
+                "1 citron",
+            ],
             self.harvester_class.ingredients(),
         )
 
     def test_instructions(self):
-        json_data = [
-            {"type": "header", "name": "Blommande äpplen"},
-            {"type": "instruction", "name": "\nSätt ugnen på 220 grader."},
-            {
-                "type": "instruction",
-                "name": "Skär av toppen på äpplena och gröp ur kärnhuset uppifrån, utan att göra hål i botten. Skär två cirklar runt hålet.",
-            },
-            {
-                "type": "instruction",
-                "name": "Lägg äpplet upp och ner och skär snitt uppifrån och ner. ",
-            },
-            {
-                "type": "instruction",
-                "name": "Lägg äpplena i en ugnsfast form med botten neråt.",
-            },
-            {"type": "instruction", "name": "Lägg i en kola i varje äpple."},
-            {
-                "type": "instruction",
-                "name": "Smält smöret och blanda ihop med salt, kanel och rörsocker.",
-            },
-            {
-                "type": "instruction",
-                "name": "Pensla äpplena med smörblandningen och sätt in i ugnen. Efter ca 20 minuter är de klara och har slagit ut som en blomma.",
-            },
-            {
-                "type": "instruction",
-                "name": "Under tiden så grovhacka nötterna och rosta i en torr panna tillsammans med salt. Det går fort så ha koll! ",
-            },
-            {
-                "type": "instruction",
-                "name": "Servera de varma äpplena med en kula vaniljglass, kolasås (se recept nedan) och strö över de rostade nötterna.",
-            },
-            {"type": "header", "name": "Kolsås"},
-            {"type": "instruction", "name": "Blanda alla ingredienser i en kastrull."},
-            {
-                "type": "instruction",
-                "name": "Låt koka under omrörning tills den har en gyllene färg.",
-            },
-            {
-                "type": "instruction",
-                "name": "Låt den svalna lite och servera!\xa0Förvara inte i kylen. Då stelnar den.",
-            },
-        ]
-
         self.assertEqual(
-            [str(json_element) for json_element in json_data],
+            """Skala alla räkor och ställ åt sidan.\nGör en majonnäs genom att lägga ner äggulor, senapen och vinägern i en bunke. Tillsätt matoljan i en tunn stråle medan du vispar hela tiden. Använd elvisp eller handvisp. När majonnäsen är tjock och du ser dragen/spåren av vispen i majonnäsen är den klar.\nLägg alla räkor i en bunke, tillsätt fint plockad dill och blanda ner lite majonnäs i taget.\nTillsätt lite riven pepparrot och smaka av. Slå på mer majonnäs för en rinnigare röra eller mer pepparrot för mer sting.\nTa fram brödet och skär ut önskad form utan att ta med kanterna, använd en skål eller ett glas som mall om ni vill ha runda bröd. Stek sedan gyllene i smör.\nLägg upp bröden på tallrik, toppa med skagenröra och en rejäl klick löjrom. Avsluta med en dillkvist och en citronskiva.""",
             self.harvester_class.instructions(),
         )
 
-        def test_ratings(self):
-            rating_dict = self.harvester_class.ratings()
-            self.assertEqual(
-                [4.7, 14], [rating_dict["rating_value"], rating_dict["rating_count"]]
-            )
+    def test_ratings(self):
+        self.assertEqual(3.6, self.harvester_class.ratings())
