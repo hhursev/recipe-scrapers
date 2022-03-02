badb54ca5ef682031fe2b67a8de97cc3d5fe5b3e
Daniel <daschu13@yahoo.de>
2022-03-02 23:56:10 +0100

    Add support for kptncook recipes (#503)
    * generate kptncook class
    
    * Implement kptncook scraper

---
+++ recipe_scrapers/kptncook.py
@@ -0,0 +1,121 @@
+import json
+from urllib.parse import parse_qs, urlparse
+
+import requests
+
+from recipe_scrapers.settings import settings
+
+from ._abstract import AbstractScraper
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0"
+}
+# These languages use measurements like cups and spoons rather than grams and liters
+IMPERIAL_LANGUAGES = ["en"]
+KPTN_DEFAULT_LANGUAGE = "en"
+
+
+class KptnCook(AbstractScraper):
+    def __init__(self, url, *args, **kwargs):
+        if settings.TEST_MODE:
+            self.recipe_json = json.loads(url.read())[0]
+            self.lang = KPTN_DEFAULT_LANGUAGE
+            self.final_url = "https://mobile.kptncook.com/recipe/pinterest/Low-Carb-Tarte-Flamb%C3%A9e-with-Serrano-Ham-%26-Cream-Cheese/315c3c32?lang=en"
+        else:
+            if urlparse(url).hostname == "mobile.kptncook.com":
+                parsed_url = urlparse(url)
+            else:
+                # If it's a sharing.kptncook.com/* link we first need to follow the http forwards to get the recipe ID
+                recipe_request = requests.get(url, headers=HEADERS)
+                parsed_url = urlparse(recipe_request.url)
+            # Extract language from URL
+            query = parse_qs(parsed_url.query)
+            self.lang = query["lang"][0] if "lang" in query else KPTN_DEFAULT_LANGUAGE
+
+            # Build final recipe url (of type mobile.kptncook.com/*)
+            self.final_url = "".join(
+                ["https://", parsed_url.hostname, parsed_url.path, f"?lang={self.lang}"]
+            )
+            # Extract recipe id from the url path
+            recipe_uid = parsed_url.path.split("/")[-1]
+
+            # Request the final recipe json from the kptncook api
+            api_url = f"https://mobile.kptncook.com/recipes/search?kptnkey=6q7QNKy-oIgk-IMuWisJ-jfN7s6&lang={self.lang}"
+            json_request_body = [{"uid": recipe_uid}]
+            self.recipe_json = json.loads(
+                requests.post(api_url, headers=HEADERS, json=json_request_body).content
+            )[0]
+
+    @classmethod
+    def host(self, subdomain="mobile"):
+        return f"{subdomain}.kptncook.com"
+
+    def author(self):
+        author = self.recipe_json["authors"][0]
+        return f"{author['name']} ({author['link']})"
+
+    def title(self):
+        return self.recipe_json["title"]
+
+    def category(self):
+        return self.recipe_json["rtype"]
+
+    def total_time(self):
+        return self.recipe_json["cookingTime"] + self.recipe_json["preparationTime"]
+
+    def cook_time(self):
+        return self.recipe_json["cookingTime"]
+
+    def prep_time(self):
+        return self.recipe_json["preparationTime"]
+
+    def yields(self):
+        return 1
+
+    def nutrients(self):
+        return self.recipe_json["recipeNutrition"]
+
+    def canonical_url(self):
+        return self.final_url
+
+    def image(self):
+        return f"{self.recipe_json['imageList'][0]['url']}?kptnkey=6q7QNKy-oIgk-IMuWisJ-jfN7s6"
+
+    def ingredients(self):
+        return [
+            " ".join(
+                str(x)
+                for x in
+                # The filter is needed because "measure" and "quantity" fields are not always provided
+                filter(
+                    None,
+                    [
+                        ingredient.get("quantity"),
+                        ingredient.get("measure"),
+                        ingredient["ingredient"]["title"],
+                    ]
+                    if self.lang not in IMPERIAL_LANGUAGES
+                    else [
+                        ingredient.get("quantityUS"),
+                        ingredient.get("measureUS"),
+                        ingredient["ingredient"]["title"],
+                    ],
+                )
+            )
+            for ingredient in self.recipe_json["ingredients"]
+        ]
+
+    def instructions(self):
+        return "\n".join(step["title"] for step in self.recipe_json["steps"])
+
+    def ratings(self):
+        return ""
+
+    def cuisine(self):
+        return ""
+
+    def description(self):
+        return self.recipe_json["authorComment"]
+
+    def language(self):
+        return self.lang
---
+++ tests/test_kptncook.py
@@ -0,0 +1,87 @@
+from recipe_scrapers.kptncook import KptnCook
+from tests import ScraperTest
+
+
+class TestKptnCookScraper(ScraperTest):
+
+    scraper_class = KptnCook
+
+    def test_host(self):
+        self.assertEqual("mobile.kptncook.com", self.harvester_class.host())
+
+    def test_author(self):
+        self.assertEqual(
+            "Laura (http://lauraleanskitchen.com/de/)", self.harvester_class.author()
+        )
+
+    def test_title(self):
+        self.assertEqual(
+            "Low Carb Tarte Flambée with Serrano Ham & Cream Cheese",
+            self.harvester_class.title(),
+        )
+
+    def test_category(self):
+        self.assertEqual("Pork", self.harvester_class.category())
+
+    def test_total_time(self):
+        self.assertEqual(25, self.harvester_class.total_time())
+
+    def test_prep_time(self):
+        self.assertEqual(20, self.harvester_class.prep_time())
+
+    def test_cook_time(self):
+        self.assertEqual(5, self.harvester_class.cook_time())
+
+    def test_yields(self):
+        self.assertEqual(1, self.harvester_class.yields())
+
+    def test_image(self):
+        self.assertEqual(
+            "https://d2am1qai33sroc.cloudfront.net/image/59b8e8a6950000a90949a083?kptnkey=6q7QNKy-oIgk-IMuWisJ-jfN7s6",
+            self.harvester_class.image(),
+        )
+
+    def test_ingredients(self):
+        ingredients = [
+            "1.0 egg",
+            "0.5 cup(s) cream cheese / curd cheese",
+            "salt",
+            "pepper",
+            "2.0 tbsp cream cheese",
+            "2.0 slice(s) serrano ham",
+            "0.5 cup(s) arugula",
+            "0.5 cup(s) cherry tomato",
+            "0.5 tsp herbes de Provence, dried",
+            "0.25 tsp chili flake",
+        ]
+        self.assertEqual(ingredients, self.harvester_class.ingredients())
+
+    def test_instructions(self):
+        instructions = "All set?\nPreheat oven to 360°F (conventional, recommended) or 325°F (fan).\nMix curd, eggs, chili flakes, herbes de Provence, salt, and pepper to a bowl and mix.\nPlace the mass on a lined baking sheet, spread thinly (make sure the layer is very thin) and bake for 10-15 min.\nWash and slice cherry tomatoes.\nWash and drain arugula.\nRemove the base from the oven and spread with cream cheese. Top with tomatoes and Serrano ham, and bake for another 5 min.\nRemove the tarte flambée from the oven, top with arugula, and enjoy."
+        self.assertEqual(instructions, self.harvester_class.instructions())
+
+    def test_ratings(self):
+        self.assertEqual("", self.harvester_class.ratings())
+
+    def test_cuisine(self):
+        self.assertEqual("", self.harvester_class.cuisine())
+
+    def test_description(self):
+        self.assertEqual(
+            "Flourless but so delicious!", self.harvester_class.description()
+        )
+
+    def test_language(self):
+        self.assertEqual("en", self.harvester_class.language())
+
+    def test_nutrients(self):
+        self.assertEqual(
+            {"calories": 368, "protein": 34, "fat": 22, "carbohydrate": 9},
+            self.harvester_class.nutrients(),
+        )
+
+    def test_canonical_url(self):
+        self.assertEqual(
+            "https://mobile.kptncook.com/recipe/pinterest/Low-Carb-Tarte-Flamb%C3%A9e-with-Serrano-Ham-%26-Cream-Cheese/315c3c32?lang=en",
+            self.harvester_class.canonical_url(),
+        )
