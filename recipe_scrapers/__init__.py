d76a2e23227f758f750c72cda6d80018156be70c
Brett Eisenberg <brett@librum.org>
2022-03-13 10:01:37 -0700

    add ditchthecarbs.com

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -37,6 +37,7 @@ from .cucchiaio import Cucchiaio
 from .cuisineaz import CuisineAZ
 from .cybercook import Cybercook
 from .delish import Delish
+from .ditchthecarbs import DitchTheCarbs
 from .domesticateme import DomesticateMe
 from .downshiftology import Downshiftology
 from .dr import Dr
@@ -220,6 +221,7 @@ SCRAPERS = {
     CuisineAZ.host(): CuisineAZ,
     Cybercook.host(): Cybercook,
     Delish.host(): Delish,
+    DitchTheCarbs.host(): DitchTheCarbs,
     DomesticateMe.host(): DomesticateMe,
     Downshiftology.host(): Downshiftology,
     Dr.host(): Dr,
