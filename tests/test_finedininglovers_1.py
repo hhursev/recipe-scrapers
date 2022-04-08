da5b63f73f0de9aab41e4bc9dc57b154ba019c92
adrian <adrianpasternak@gmail.com>
2022-04-08 21:50:29 +0200

    fix finedininglovers.com instruction parsing

--- tests/test_finedininglovers_1.py
+++ tests/test_finedininglovers_1.py
@@ -6,6 +6,10 @@ class TestFineDiningLoversScraper(ScraperTest):
 
     scraper_class = FineDiningLovers
 
+    @property
+    def test_file_name(self):
+        return "{}_1".format(self.scraper_class.__name__.lower())
+
     def test_host(self):
         self.assertEqual("finedininglovers.com", self.harvester_class.host())
 
