8391eb2671cf6bb7c0733948dd1f8a6213144a19
Hristo Harsev <hhursev@gmail.com>
2022-04-10 10:06:03 +0300

    Update epicurious scraper. Closes #504

--- recipe_scrapers/epicurious.py
+++ recipe_scrapers/epicurious.py
@@ -1,5 +1,4 @@
 from ._abstract import AbstractScraper
-from ._utils import get_minutes, get_yields, normalize_string
 
 
 class Epicurious(AbstractScraper):
@@ -8,54 +7,22 @@ class Epicurious(AbstractScraper):
         return "epicurious.com"
 
     def title(self):
-        return normalize_string(self.soup.find("h1", {"itemprop": "name"}).get_text())
+        return self.schema.title()
 
     def total_time(self):
-        total_time = self.soup.find("dd", {"class": "total-time"})
-
-        return get_minutes(total_time)
+        return self.schema.total_time()
 
     def yields(self):
-        return get_yields(self.soup.find("dd", {"itemprop": "recipeYield"}))
+        return self.schema.yields()
 
     def image(self):
-        image = self.soup.find("img", {"class": "photo", "srcset": True})
-        return image["srcset"] if image else None
+        return self.schema.image()
 
     def ingredients(self):
-        ingredients = self.soup.findAll("li", {"itemprop": "ingredients"})
-
-        return [normalize_string(ingredient.get_text()) for ingredient in ingredients]
+        return self.schema.ingredients()
 
     def instructions(self):
-        instructions = self.soup.findAll("li", {"class": "preparation-step"})
-
-        return "\n".join(
-            [normalize_string(instruction.get_text()) for instruction in instructions]
-        )
+        return self.schema.instructions()
 
     def ratings(self):
-        rating = self.soup.find("span", {"class": "rating"})
-        rating = rating.get_text().split("/")[0] if rating is not None else None
-        rating = float(rating) if rating is not None else None
-        return rating
-
-    def reviews(self):
-        result = []
-        reviews = self.soup.find("div", {"class": "reviews"})
-        if len(reviews.find_all("li")):
-            for li in reviews.find_all("li"):
-                rating = li.find("img", {"class": "fork-rating"})
-                try:
-                    rating_value = int(rating.get("src").split("_forks.png")[0][-1])
-                except Exception:
-                    rating_value = 0
-
-                result.append(
-                    {
-                        "review_text": normalize_string(li.find("p").get_text()),
-                        "rating": rating_value,
-                    }
-                )
-
-        return result
+        return self.schema.ratings()
