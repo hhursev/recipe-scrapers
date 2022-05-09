ecda7b43a44eb814976d14893d7d5f0f757ea44d
Daniel Schulz <schulz.daniel@outlook.com>
2022-05-10 01:14:23 +0200

    fix thepioneerwoman scraper by using schema.org

--- tests/test_thepioneerwoman.py
+++ tests/test_thepioneerwoman.py
@@ -11,7 +11,7 @@ class TestThePioneerWomanScraper(ScraperTest):
 
     def test_canonical_url(self):
         self.assertEqual(
-            "http://thepioneerwoman.com/cooking/patty-melts/",
+            "https://www.thepioneerwoman.com/food-cooking/recipes/a10720/patty-melts/",
             self.harvester_class.canonical_url(),
         )
 
@@ -26,26 +26,50 @@ class TestThePioneerWomanScraper(ScraperTest):
 
     def test_image(self):
         self.assertEqual(
-            "https://pioneerwoman.files.wordpress.com/2012/08/pattymelt2.jpg",
+            "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/patty-melt-1597698088.jpg",
             self.harvester_class.image(),
         )
 
     def test_ingredients(self):
         self.assertCountEqual(
             [
-                "1 stick Butter",
-                "1 whole Large Onion, Halved And Sliced",
-                "1-1/2 pound Ground Beef",
-                "Salt And Pepper, to taste",
-                "5 dashes Worcestershire Sauce",
-                "8 slices Swiss Cheese",
-                "8 slices Rye Bread",
+                "1 stick butter",
+                "1 whole large onion, halved and sliced",
+                "1 1/2 lb. ground beef",
+                "salt and pepper, to taste",
+                "5 dashes Worcestershire sauce",
+                "8 slices Swiss cheese",
+                "8 slices rye bread",
             ],
             self.harvester_class.ingredients(),
         )
 
     def test_instructions(self):
         return self.assertEqual(
-            "In a medium skillet, melt 2 tablespoons of butter over medium-low heat.\n Throw in the sliced onions and cook slowly for 20 to 25 minutes, stirring occasionally, until the onions are golden brown and soft.\n In a medium bowl, mix together the ground beef, salt & pepper, and Worcestershire.\n Form into 4 patties.\nMelt 2 tablespoons butter in a separate skillet over medium heat.\n Cook the patties on both sides until totally done in the middle.\n Assemble patty melts this way: Slice of bread, slice of cheese, hamburger patty, 1/4 of the cooked onions, another slice of cheese, and another slice of bread.\n On a clean griddle or in a skillet, melt 2 tablespoons butter and grill the sandwiches over medium heat until golden brown.\n Remove the sandwiches and add the remaining 2 tablespoons of butter to the skillet.\n Turn the sandwiches to the skillet, flipping them to the other side.\n Cook until golden brown and crisp, and until cheese is melted.\n Slice in half and serve immediately!",
+            "In a medium skillet, melt 2 tablespoons of butter over medium-low heat. Throw in the sliced onions and cook slowly for 20 to 25 minutes, stirring occasionally, until the onions are golden brown and soft.\nIn a medium bowl, mix together the ground beef, salt & pepper, and Worcestershire. Form into 4 patties.\nMelt 2 tablespoons butter in a separate skillet over medium heat. Cook the patties on both sides until totally done in the middle.\nAssemble patty melts this way: Slice of bread, slice of cheese, hamburger patty, 1/4 of the cooked onions, another slice of cheese, and another slice of bread. On a clean griddle or in a skillet, melt 2 tablespoons butter and grill the sandwiches over medium heat until golden brown. Remove the sandwiches and add the remaining 2 tablespoons of butter to the skillet. Turn the sandwiches to the skillet, flipping them to the other side. Cook until golden brown and crisp, and until cheese is melted.\nSlice in half and serve immediately!",
             self.harvester_class.instructions(),
+        )
+
+    def test_category(self):
+        return self.assertEqual("main dish,meat", self.harvester_class.category())
+
+    def test_cook_time(self):
+        return self.assertEqual(25, self.harvester_class.cook_time())
+
+    def test_prep_time(self):
+        return self.assertEqual(10, self.harvester_class.prep_time())
+
+    def test_ratings(self):
+        return self.assertEqual(5.0, self.harvester_class.ratings())
+
+    def test_author(self):
+        return self.assertEqual("Ree Drummond", self.harvester_class.author())
+
+    def test_cuisine(self):
+        return self.assertEqual("", self.harvester_class.cuisine())
+
+    def test_description(self):
+        return self.assertEqual(
+            "Who doesn’t love a good old-fashioned patty melt? Here's the easiest way to make one at home that's beyond delicious.",
+            self.harvester_class.description(),
         )
