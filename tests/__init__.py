83d96c1cd34d3848aff76f82a6d407228fa2f2c7
James Addison <james@reciperadar.com>
2022-05-31 20:14:53 +0100

    Migrate test suite to use 'responses' library to mock website response content

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,8 @@
 import os
+from typing import Dict
 import unittest
 
-import pytest
+import responses
 
 
 class ScraperTest(unittest.TestCase):
@@ -11,27 +12,27 @@ class ScraperTest(unittest.TestCase):
     test_file_name = None
     test_file_extension = "testhtml"
 
+    @property
+    def expected_requests(self) -> Dict[str, str]:
+        """
+        A mapping of expected HTTP GET URLs to filenames that contain response content.
+        """
+        filename = self.test_file_name or self.scraper_class.__name__.lower()
+        path = f"tests/test_data/{filename}.{self.test_file_extension}"
+        yield "https://test.example.com", path
+
     def setUp(self):
         os.environ[
             "RECIPE_SCRAPERS_SETTINGS"
         ] = "tests.test_data.test_settings_module.test_settings"
 
-        test_file_name = (
-            self.test_file_name
-            if self.test_file_name
-            else self.scraper_class.__name__.lower()
-        )
-        with open(
-            f"tests/test_data/{test_file_name}.{self.test_file_extension}",
-            encoding="utf-8",
-        ) as testfile:
-            self.harvester_class = self.scraper_class(
-                url="https://test.example.com/", html=testfile.read()
-            )
-            canonical_url = self.harvester_class.canonical_url()
-            if self.online:
-                if not canonical_url:
-                    pytest.skip(
-                        f"could not find canonical url for online test of scraper '{self.scraper_class.__name__}'"
-                    )
-                self.harvester_class = self.scraper_class(url=canonical_url)
+        with responses.RequestsMock() as rsps:
+            start_url = None
+            for url, path in self.expected_requests:
+                start_url = start_url or url
+                content = open(path, encoding="utf-8").read()
+                response = responses.Response(responses.GET, url, body=content)
+                response.passthrough = self.online
+                rsps.add(response)
+
+            self.harvester_class = self.scraper_class(url=start_url)
