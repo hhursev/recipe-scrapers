4d00351877928c3495643b7edd88dfd72c1c9bd5
Andrew Pachuilo <andrew.pachuilo@gmail.com>
2022-07-13 07:25:26 -0500

    Fixed error causing certain MadeWithLau recipes to crash (#573)

--- tests/test_madewithlau.py
+++ tests/test_madewithlau.py
@@ -16,12 +16,12 @@ class TestMadeWithLauScraper(ScraperTest):
     def test_title(self):
         self.assertEqual(
             self.harvester_class.title(),
-            "Egg Rolls (春捲)",
+            "Salt & Pepper Tofu (椒鹽豆腐)",
         )
 
     def test_description(self):
         self.assertEqual(
-            "A customizable, vegetarian version of this crispy worldwide favorite!",
+            "Fragrant deep-fried tofu that'll disappear from the table before you know it!",
             self.harvester_class.description(),
         )
 
@@ -39,38 +39,32 @@ class TestMadeWithLauScraper(ScraperTest):
 
     def test_image(self):
         self.assertEqual(
-            "https://cdn.sanity.io/images/2r0kdewr/production/b4a1b1a373b898fe19bd894d1dfd041761eca5e6-1000x1000.jpg",
+            "https://cdn.sanity.io/images/2r0kdewr/production/3172f1134f6ad9f09fafa7770328db9066b7215a-500x500.jpg",
             self.harvester_class.image(),
         )
 
     def test_ingredients(self):
         self.assertCountEqual(
             [
-                "0.5 oz dried shiitake mushroom",
-                "2 oz vermicelli noodles",
-                "0.25 oz cloud ear fungus",
-                "8 oz cabbage",
-                "4 oz celery",
-                "3 oz carrot",
-                "1 oz snow pea",
-                "3 cloves garlic",
-                "20 pieces egg roll wrapper",
-                "16 oz oil",
-                "2 tbsp flour",
-                "2 tbsp water",
-                "1 tbsp oil",
+                "16 oz firm tofu",
+                "3 stalks green onion",
+                "2 oz mini bell pepper",
+                "2 cloves garlic",
+                "4 cups boiling water",
                 "1 tsp salt",
-                "1 tsp sugar",
-                "1 tbsp Kikkoman® Vegetarian Oyster Sauce",
+                "0.5 egg",
+                "6 tbsp cornstarch",
+                "16 oz oil",
+                "2 tbsp oil",
+                "1 tbsp garlic salt",
                 "0.25 tsp white pepper",
-                "1 tbsp Kikkoman® Sesame Oil",
             ],
             self.harvester_class.ingredients(),
         )
 
     def test_instructions(self):
         return self.assertEqual(
-            "Step 1 - Prepare dried ingredients\nSoak the vermicelli noodles (2 oz) in warm water to rehydrate for about 10 minutes.\nSoak the dried shiitake mushrooms (0.50 oz) in hot water for 10-15 minutes.\nSimilarly, soak the cloud ear fungus (0.25 oz) in hot water for 10-15 minutes.\nStep 2 - Cut ingredients\nFirst, prepare the cabbage (8 oz). Cut away and discard the hard stalk in the center, where it's very thick and dense. Stack the leaves and chop into very thin strips.\nCut the rehydrated cloud ear fungus and shiitake mushrooms into very thin strips as well.\nCut the celery (4 oz) into 2-inch segments first, then into thin strips.\nChop the snow peas (1 oz) into thin strips.\nStabilize the carrot (3 oz) by slicing off one side and creating a flat surface for it to lay on. If the carrot is long, cut it into 2-inch segments. Then, cut into thin slices, and then into thin strips.\nRoughly mince the garlic (3 clove).\nCreate glue\nIn a small bowl, mix together flour (2 tbsp) and water (2 tbsp).\nStep 3 - Stir-fry filling\nHeat your wok on high heat until it's hot, or about a minute.\nAdd oil (1 tbsp) and give it a swirl to coat the wok. Let it heat up for 30-40 seconds.\nAdd garlic, and fry it until it's aromatic.\nAdd the shiitake mushrooms, and cook for 20-30 seconds.\nAs you add each additional ingredient, mix them in thoroughly as they cook.\nAdd the celery and the cabbage. Cook for 30 seconds.\nAdd the carrot and cloud ear fungus. Cook for 30-40 seconds.\nUse scissors or kitchen shears to cut the vermicelli a few times. They will be added later.\nSeason the filling with salt (1 tsp), sugar (1 tsp), white pepper (0.25 tsp), and oyster sauce (1 tbsp).\nMix the seasoning in well to distribute, and then add the snow peas.\nFinally, push all the ingredients to the sides of the wok to make a small hole in the center. Add the cut-up vermicelli and mix it in the center to give it a good head start on soaking up the moisture, and then mix it in with the rest of the filling.\nAdd sesame oil (1 tbsp) and stir-fry for another 30-40 seconds, making sure everything is well-combined.\nTurn off the heat and plate the filling to help it cool down for the wrapping step.\nStep 4 - Wrap rolls\nPeel each individual wrapper apart and create a loose stack. Do this for the number of egg rolls you're making.\nGently put your stack of wrappers in a plastic bag or container. Take one wrapper out when you're ready to wrap it, and leave the rest covered.\nLay the wrapper with a corner pointing at you, like a diamond. Put 2 tbsp of filling in the wrapper, between the middle and the corner that's pointing to you.\nBring the wrapper corner that's closest to you and wrap it up and over the mound of filling, then tuck the tip under and firmly pull the covered section back towards yourself to tighten. Keep enough pressure on it to press out air pockets and keep the filling together, but be gentle as to not rip the wrapper.\nKeep the pressure pushing on the filling as you roll towards the middle. Be sure not to let the filling spread out too far horizontally.\nAt the halfway point, make a crease where the filling ends on the left, and fold that side of the wrapper up and over. Do the same crease and fold with the right.\nDab some of the flour-and-water glue on the top corner of the wrapper. As always, keep firm pressure on the roll as you roll it up to the end. The glue will seal the end of the roll.\nRepeat with the rest of the filling and wrappers until all your egg rolls have been wrapped up.\nStep 5 - Deep-fry rolls\nHeat oil (16 oz) in a deep pot over high heat. Let the oil reach 330°F (165°C), then turn the heat to low.\nAdd the egg rolls in batches of 6 as to not overcrowd the pot. As they cook, move them around and flip them so they fry evenly.\nFry on low heat for about a minute and a half, then turn the heat to high.\nFry at high heat for a minute and a half, until they're golden brown and crispy. Remove the cooked egg rolls from the oil.\nPlate the cooked egg rolls. Remember to lower the heat when you start the next batch.\nEnjoy quickly for maximum crunch!",
+            "Step 1 - Cut ingredients\nFirst, cut to separate the green and the white parts of the green onions (3 stalks). You're only using the firmer, more pungent whites for this dish. Dice the green onion whites.\nHalve the bell peppers (2 oz) and discard the core and seeds. Cut into strips, and then dice them.\nSmash, peel, and finely mince the garlic (2 clove).\nStep 2 - Cut tofu\nDrain and rinse the firm tofu (16 oz). Cut the tofu into cubes.\n\nStep 3 - Boil tofu\nBoil hot water (4 cup) in a pot with the heat on high. Add salt (1 tsp) and stir to dissolve. When water is boiling, add the tofu cubes. Use chopsticks to shift the tofu around so they’re all fully submerged.\nCook for 3 minutes with the lid on, and then lower the heat to low and uncover the pot. Cook for another 2 minutes.\nCarefully pour the tofu into a colander to drain the water out. Let it start to cool.\nStep 4 - Dry & coat tofu\nLine a plate with a paper towel or a clean kitchen towel. Transfer the tofu cubes onto the lined plate in an even layer. Lay another towel on top and gently press to remove excess moisture.\nSpread a layer of cornstarch (4.8 tbsp) on a separate, large plate. Transfer the tofu into a bowl and add the beaten egg (0.50 ). Gently mix to evenly coat the tofu.\nTransfer the egg-covered tofu cubes to the cornstarch plate to begin coating them.\nSprinkle more cornstarch (1.2 tbsp) over the top of the tofu. Use chopsticks to flip and mix the tofu until all the pieces are evenly coated in cornstarch.\nStep 5 - Fry tofu\nAdd oil (16 oz) to a large pot and set the heat on high.\nAs the oil heats, mix together garlic salt (1 tbsp) and white pepper (0.25 tsp) in a separate bowl.\nPrepare a batch of dredged tofu in a spider strainer.\nWhen the oil reaches 400°F (or 200°C), carefully set the tofu in the oil. At the beginning, let the tofu cook undisturbed so that the crust can set.\nWhen the tofu has started to set and the oil has started to recover from the temperature drop, you can add the rest of the tofu. Set them down along the sides of the pot so they don't stick to the other pieces. Gently mix the pieces around so they fry evenly.\nWhen the tofu is golden brown, or after about 5 minutes of frying at 360°-380°F (180-190°C), turn off the heat and carefully remove the tofu. Let the excess oil drain from the tofu.\nStep 6 - Stir-fry everything\nHeat a wok on high. Add oil (2 tbsp) (take this from the deep-frying oil). Before the oil gets too hot (before it starts to shimmer or smoke), add the dried chili and stir immediately. Fry for just 8 seconds to extract the flavor, and then remove them.\nAdd the minced garlic and fry it for 15 seconds, stirring the entire time.\nAdd the diced bell peppers and green onion whites. Fry for 20-25 seconds, still stirring constantly.\nAdd the fried tofu and stir-fry for 10 seconds.\nWhile stirring constantly, sprinkle just enough of the garlic salt+white pepper seasoning mix over all the ingredients in the wok to evenly coat the tofu cubes.\nStep 7 - Plate & serve\nTurn off the heat and plate. Make sure to scoop up all the little bits of vegetables; they're delicious. Enjoy!",
             self.harvester_class.instructions(),
         )
 
@@ -78,4 +72,4 @@ class TestMadeWithLauScraper(ScraperTest):
         self.assertEqual("Chinese", self.harvester_class.cuisine())
 
     def test_category(self):
-        self.assertEqual("appetizer, snack", self.harvester_class.category())
+        self.assertEqual("side dish", self.harvester_class.category())
