c4683193faa7fe94629f81ce9f949fc9db80ccc8
Aditya2704 <adityachoudhary.jsr@gmail.com>
2022-08-19 18:17:58 +0530

    removed unused imports in food.com scraper

--- recipe_scrapers/food.py
+++ recipe_scrapers/food.py
@@ -1,5 +1,4 @@
 from ._abstract import AbstractScraper
-from ._utils import get_minutes, get_yields, normalize_string
 
 
 class Food(AbstractScraper):
@@ -8,22 +7,19 @@ class Food(AbstractScraper):
         return "food.com"
 
     def title(self):
-        return self.soup.find("h1").get_text()
+        return self.schema.title()
 
     def total_time(self):
-        return get_minutes(self.soup.find("div", {"class": "recipe-facts__time"}))
+        return self.schema.total_time()
 
     def yields(self):
-        return get_yields(
-            self.soup.find("div", {"class": "recipe-facts__servings"}).get_text()
-        )
+        return self.schema.yields()
+
+    def image(self):
+        return self.schema.image()
 
     def ingredients(self):
-        ingredients = self.soup.findAll("li", {"class": "recipe-ingredients__item"})
-
-        return [normalize_string(ingredient.get_text()) for ingredient in ingredients]
+        return self.schema.ingredients()
 
     def instructions(self):
-        instructions = self.soup.findAll("li", {"class": "recipe-directions__step"})
-
-        return "\n".join([instruction.get_text() for instruction in instructions])
+        return self.schema.instructions()
