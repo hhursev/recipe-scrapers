a97807be5bd82439850e9cfd05a9853709084e5c
Aditya Choudhary <adityachoudhary.jsr@gmail.com>
2022-08-19 20:29:07 +0530

    removed unused imports in food.com scraper (#591)

--- tests/test_food.py
+++ tests/test_food.py
@@ -34,19 +34,19 @@ class TestFoodScraper(ScraperTest):
                 "1 onion, chopped",
                 "4 carrots, halved lengthwise and cut crosswise into 1-inch pieces",
                 "4 parsnips, halved lengthwise and cut crosswise into 1-inch pieces",
-                "1 1⁄2 teaspoons salt",
-                "1⁄4 teaspoon fresh ground black pepper",
+                "1 1/2 teaspoons salt",
+                "1/4 teaspoon fresh ground black pepper",
                 "1 split chicken breast",
                 "1 cup noodles (about 2 ounces)",
-                "1⁄4 cup chopped fresh dill",
-                "1⁄4 cup chopped fresh parsley",
+                "1/4 cup chopped fresh dill",
+                "1/4 cup chopped fresh parsley",
             ],
             self.harvester_class.ingredients(),
         )
 
     def test_instructions(self):
         return self.assertEqual(
-            "In a large pot, combine the broth, onion, carrots, parsnips, salt, and pepper and bring to a simmer. Add the chicken breasts to the pot and simmer until jfor about 20 minutes, until cooked. Remove the chicken and let rest.  When cool enough to handle, remove skin and bones and chop or shred intobite-size pieces.\nWhile chicken is cooling, bring the soup back to a simmer and stir the noodles into the soup. Simmer until the vegetables are tender and the noodles are done, about 5 minutes. Return the chicken pieces to the pot and then stir in the dill and the parsley.",
+            "In a large pot, combine the broth, onion, carrots, parsnips, salt, and pepper and bring to a simmer. Add the chicken breasts to the pot and simmer until jfor about 20 minutes, until cooked. Remove the chicken and let rest. When cool enough to handle, remove skin and bones and chop or shred intobite-size pieces.\nWhile chicken is cooling, bring the soup back to a simmer and stir the noodles into the soup. Simmer until the vegetables are tender and the noodles are done, about 5 minutes. Return the chicken pieces to the pot and then stir in the dill and the parsley.",
             self.harvester_class.instructions(),
         )
 
