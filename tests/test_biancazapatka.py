db61aabad148c1b31202657c56efa9b12e2e668a
yellelieder <55919613+yellelieder@users.noreply.github.com>
2022-08-20 18:53:20 +0200

    biancazapatka.com testscases

--- tests/test_biancazapatka.py
+++ tests/test_biancazapatka.py
@@ -10,34 +10,31 @@ class TestBiancaZapatkaScraper(ScraperTest):
         self.assertEqual("biancazapatka.com", self.harvester_class.host())
 
     def test_author(self):
-        self.assertEqual(None, self.harvester_class.author())
+        self.assertEqual('Bianca Zapatka', self.harvester_class.author())
 
     def test_title(self):
-        self.assertEqual(None, self.harvester_class.title())
+        self.assertEqual('Rice Noodle Salad with Tempeh and Peanut Sauce', self.harvester_class.title())
 
     def test_category(self):
-        self.assertEqual(None, self.harvester_class.category())
+        self.assertEqual('Lunch &amp; Dinner,Main Course', self.harvester_class.category())
 
     def test_total_time(self):
-        self.assertEqual(None, self.harvester_class.total_time())
+        self.assertEqual(35, self.harvester_class.total_time())
 
     def test_yields(self):
-        self.assertEqual(None, self.harvester_class.yields())
+        self.assertEqual('4 servings', self.harvester_class.yields())
 
     def test_image(self):
-        self.assertEqual(None, self.harvester_class.image())
+        self.assertEqual('https://biancazapatka.com/wp-content/uploads/2022/07/buddha-bowl-sauce.jpg', self.harvester_class.image())
 
     def test_ingredients(self):
-        self.assertEqual(None, self.harvester_class.ingredients())
+        self.assertEqual(['7 oz Tempeh (oder Tofu)', '2 garlic clo[654 chars]uts'], self.harvester_class.ingredients())
 
     def test_instructions(self):
-        self.assertEqual(None, self.harvester_class.instructions())
-
-    def test_ratings(self):
-        self.assertEqual(None, self.harvester_class.ratings())
+        self.assertEqual('Marinated Tempeh\nFor the marinade, pour[1078 chars]joy!', self.harvester_class.instructions())
 
     def test_cuisine(self):
-        self.assertEqual(None, self.harvester_class.cuisine())
+        self.assertEqual('Asian', self.harvester_class.cuisine())
 
     def test_description(self):
-        self.assertEqual(None, self.harvester_class.description())
+        self.assertEqual('This colorful Tempeh Rice Noodle Salad B[309 chars]nce!', self.harvester_class.description())
