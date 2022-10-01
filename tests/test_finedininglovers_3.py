218c271a9608cecfb795e1fd8dcfa1878d0a1220
vabene1111 <vabene1234@googlemail.com>
2022-10-01 17:57:44 +0200

    fixed test

--- tests/test_finedininglovers_3.py
+++ tests/test_finedininglovers_3.py
@@ -25,10 +25,10 @@ class TestFineDiningLoversScraper(ScraperTest):
         )
 
     def test_total_time(self):
-        self.assertEqual(0, self.harvester_class.total_time())
+        self.assertEqual(None, self.harvester_class.total_time())
 
     def test_yields(self):
-        self.assertEqual(0, self.harvester_class.yields())
+        self.assertEqual(None, self.harvester_class.yields())
 
     def test_ingredients(self):
         self.assertCountEqual(
