fb387998eaad90a5b64571bb1f6a4d6a0866c443
James Addison <jay@jp-hosting.net>
2022-10-11 13:03:04 +0100

    Revert "revertme: print length of parsed HTML root node text contents as diagnostic information"
    This reverts commit c8c03d4a06c0b623e00cb4d1ccd7f29b58d0daca.

--- recipe_scrapers/_schemaorg.py
+++ recipe_scrapers/_schemaorg.py
@@ -28,7 +28,6 @@ class SchemaOrg:
 
         print(len(page_data))
         print(len(parse_html(page_data, encoding="UTF-8")))
-        print(len(parse_html(page_data, encoding="UTF-8").text_content()))
 
         data = extruct.extract(
             page_data,
