e3f53e7b28c020dc5a8a99720afbce1370ef3a2c
bickerdyke <48774580+bickerdyke@users.noreply.github.com>
2022-10-13 02:21:12 +0200

    Scraping first parts

---
+++ recipe_scrapers/weightwatchers.py
@@ -0,0 +1,67 @@
+# mypy: allow-untyped-defs
+
+import re
+
+from ._abstract import AbstractScraper
+from ._utils import get_minutes, get_yields
+
+
+class Weightwatchers(AbstractScraper):
+    @classmethod
+    def host(cls):
+        return "weightwatchers.de"
+
+    def author(self):
+        return "WeightWatchers"
+
+    def title(self):
+        return self.soup.find("h1").get_text().strip()
+
+    def category(self):
+        return "WeightWatchers"
+
+    def total_time(self):
+        return get_minutes(
+            self.soup.find("div", {"class": "styles_container__3N3E8"})
+            .find("div", string=re.compile(r"minutes Total Time"))
+            .previous_sibling
+        )
+
+    def cook_time(self):
+        return get_minutes(
+            self.soup.find("div", {"class": "styles_container__3N3E8"})
+            .find("div", string=re.compile(r"minutes Preparation Time"))
+            .previous_sibling
+        )
+
+    def prep_time(self):
+        return get_minutes(
+            self.soup.find("div", {"class": "styles_container__3N3E8"})
+            .find("div", string=re.compile(r"minutes Cook Time"))
+            .previous_sibling
+        )
+
+    def yields(self):
+        return get_yields(
+            self.soup.find("div", {"class": "styles_container__3N3E8"})
+            .find("div", string=re.compile(r"Serves *{0..9} people"))
+            .previous_sibling
+        )
+
+    def image(self):
+        return self.schema.image()
+
+    def ingredients(self):
+        return self.schema.ingredients()
+
+    def instructions(self):
+        return self.schema.instructions()
+
+    def ratings(self):
+        return self.schema.ratings()
+
+    def cuisine(self):
+        return self.schema.cuisine()
+
+    def description(self):
+        return self.soup.find("div", {"class": "copy-1"}).get_text().strip()
---
+++ tests/test_weightwatchers.py
@@ -0,0 +1,54 @@
+# mypy: allow-untyped-defs
+
+from recipe_scrapers.weightwatchers import Weightwatchers
+from tests import ScraperTest
+
+
+class TestWeightwatchersScraper(ScraperTest):
+
+    scraper_class = Weightwatchers
+
+    def test_host(self):
+        self.assertEqual("weightwatchers.de", self.harvester_class.host())
+
+    def test_author(self):
+        self.assertEqual("WeightWatchers", self.harvester_class.author())
+
+    def test_title(self):
+        self.assertEqual(self.harvester_class.title(), "Würstchengulasch mit Nudeln")
+
+    def test_category(self):
+        self.assertEqual("WeightWatchers", self.harvester_class.category())
+
+    def test_total_time(self):
+        self.assertEqual(25, self.harvester_class.total_time())
+
+    def test_cook_time(self):
+        self.assertEqual(0, self.harvester_class.cook_time())
+
+    def test_prep_time(self):
+        self.assertEqual(25, self.harvester_class.prep_time())
+
+    def test_yields(self):
+        self.assertEqual("2 servings", self.harvester_class.yields())
+
+    # def test_image(self):
+    #    self.assertEqual(None, self.harvester_class.image())
+
+    # def test_ingredients(self):
+    #    self.assertEqual(None, self.harvester_class.ingredients())
+
+    # def test_instructions(self):
+    #    self.assertEqual(None, self.harvester_class.instructions())
+
+    # def test_ratings(self):
+    #    self.assertEqual(None, self.harvester_class.ratings())
+
+    # def test_cuisine(self):
+    #    self.assertEqual(None, self.harvester_class.cuisine())
+
+    def test_description(self):
+        self.assertEqual(
+            "18 Uhr und alle haben Hunger? Dann koche rasch das Würstchengulasch und alle sind happy.",
+            self.harvester_class.description(),
+        )
