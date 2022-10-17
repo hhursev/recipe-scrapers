00fe94606a4e4a9594cec9d2a8da37c5bebc8025
James Addison <jay@jp-hosting.net>
2022-10-17 14:45:59 +0100

    Experiment: compare unit test workflow timing when running single-threaded

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,5 @@
 import subprocess
 
 if __name__ == "__main__":
-    run_tests_command = (
-        "unittest-parallel -t . -s tests --coverage --coverage-rcfile .coveragerc"
-    )
+    run_tests_command = "coverage run -m unittest discover -t . -s tests"
     subprocess.run(run_tests_command.split(" "), check=True, text=True)
