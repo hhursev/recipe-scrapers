466e90ad4a424d4a7b7727ad048118935962e150
James Addison <jay@jp-hosting.net>
2022-10-27 10:20:29 +0100

    Inspiralized: fixup to remove direct access to self.page_data

--- recipe_scrapers/inspiralized.py
+++ recipe_scrapers/inspiralized.py
@@ -13,9 +13,7 @@ class Inspiralized(AbstractScraper):
         return self.soup.find("h2").get_text()
 
     def author(self):
-        if type(self.page_data) == bytes and b"Ali Maffucci" in self.page_data:
-            return "Ali Maffucci"
-        if type(self.page_data) == str and "Ali Maffucci" in self.page_data:
+        if self.soup.find(string="Ali Maffucci"):
             return "Ali Maffucci"
 
     def total_time(self):
