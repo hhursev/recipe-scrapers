ae84fa46936947693ffd59057519ee2a894cc42e
Barry Roberts <manithree@gmail.com>
2022-11-15 21:23:36 -0700

    Fix issues with new forksoverknives category
    Recently fok recipes started including anchor tags in the category,
    linking to the category page. So, the category would look like:
    
    "<a
    href=\"https:\/\/www.forksoverknives.com\/recipes\/vegan-soups-stews\/\">Soups
    &amp; Stews<\/a>"
    
    Gourmand calls schema.category() to get the category, so
    this overrides that. Also, instead of leaving it:
    Soups &amp; Stews
    import html and unescape it to return:
    Soups & Stews

--- recipe_scrapers/forksoverknives.py
+++ recipe_scrapers/forksoverknives.py
@@ -1,12 +1,43 @@
 # mypy: disallow_untyped_defs=False
 from ._abstract import AbstractScraper
 from ._utils import normalize_string
+from ._schemaorg import SchemaOrg
+from typing import Dict, List, Optional, Tuple, Union
+
+import re, html
+
+class fok_Schema(SchemaOrg):
+    def __init__(self, page_data, raw=False):
+        super().__init__(page_data)
+
+    def category(self):
+        cat = super().category()
+        r = re.compile('.*<a href="\S+">(.*)</a>')
+        m = r.match(cat)
+        if m:
+            return html.unescape(m[1])
+        return cat
 
 
 class ForksOverKnives(AbstractScraper):
     @classmethod
     def host(cls):
         return "forksoverknives.com"
+
+    def __init__(
+        self,
+        url: Union[str, None],
+        proxies: Optional[
+            Dict[str, str]
+        ] = None,  # allows us to specify optional proxy server
+        timeout: Optional[
+            Union[float, Tuple[float, float], Tuple[float, None]]
+        ] = None,  # allows us to specify optional timeout for request
+        wild_mode: Optional[bool] = False,
+        html: Union[str, bytes, None] = None,
+    ):
+        super().__init__(url=url, proxies=proxies, timeout=timeout, wild_mode=wild_mode, html=html)
+        self.schema = fok_Schema(self.page_data)
 
     def author(self):
         author = self.soup.find("div", attrs={"class": "post-info"}).find("a")
