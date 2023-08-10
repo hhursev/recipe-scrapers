f6f96726e42664121497e667862574784da16a4d
Joey <joey.kennedy1@gmail.com>
2023-08-10 18:03:10 -0400

    Update foodfidelity.py

--- recipe_scrapers/foodfidelity.py
+++ recipe_scrapers/foodfidelity.py
@@ -52,5 +52,5 @@ class FoodFidelity(AbstractScraper):
 
     def description(self):
         img_tag = self.soup.find("img", {"data-pin-description": True})
-if img_tag:
-    return img_tag["data-pin-description"]
+        if img_tag:
+            return img_tag["data-pin-description"]
