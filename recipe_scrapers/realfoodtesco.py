58d5852da43cdee838a802d0b2f671752402eb51
James Addison <james@reciperadar.com>
2023-08-10 15:48:52 +0100

    realfoodtesco: implement ingredient groups

--- recipe_scrapers/realfoodtesco.py
+++ recipe_scrapers/realfoodtesco.py
@@ -26,6 +26,14 @@ class RealFoodTesco(AbstractScraper):
     def ingredients(self):
         return self.schema.ingredients()
 
+    def ingredient_groups(self):
+        return group_ingredients(
+            ingredients_list=self.ingredients(),
+            soup=self.soup,
+            group_heading="h3.recipe-detail__subheading",
+            group_element="li.recipe-detail__list-item",
+        )
+
     def instructions(self):
         return self.schema.instructions()
 
