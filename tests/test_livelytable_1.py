6b37a58e9c7c2347a426548be75faabaab6c6674
Joey <joey.kennedy1@gmail.com>
2023-08-29 18:47:36 -0400

    Updated original test to non-grouped url

--- tests/test_livelytable_1.py
+++ tests/test_livelytable_1.py
@@ -12,50 +12,45 @@ class TestLivelyTableScraper(ScraperTest):
 
     def test_canonical_url(self):
         self.assertEqual(
-            "https://livelytable.com/easy-chipotle-shrimp-tacos/",
+            "https://livelytable.com/parmesan-zucchini-casserole/",
             self.harvester_class.canonical_url(),
         )
 
     def test_title(self):
-        self.assertEqual(self.harvester_class.title(), "Easy Chipotle Shrimp Tacos")
+        self.assertEqual(self.harvester_class.title(), "Parmesan Zucchini Casserole")
 
     def test_author(self):
-        self.assertEqual(self.harvester_class.author(), "Kaleigh")
+        self.assertEqual(self.harvester_class.author(), "LINDSAY DELK")
 
     def test_yields(self):
-        self.assertEqual("6 servings", self.harvester_class.yields())
+        self.assertEqual("4 servings", self.harvester_class.yields())
 
     def test_image(self):
         self.assertEqual(
-            "https://livelytable.com/wp-content/uploads/2017/05/chipotle-shrimp-tacos-225x225.jpg",
+            "https://livelytable.com/wp-content/uploads/2023/01/Parmesan-zucchini-casserole-1-225x225.jpg",
             self.harvester_class.image(),
         )
 
     def test_ingredients(self):
         self.assertEqual(
             [
-                "1 lb raw large shrimp, peeled and deveined",
-                "1/2 tsp chipotle powder",
-                "1/2 tsp garlic powder",
-                "1/2 tsp ground cumin",
-                "1 tbsp olive or avocado oil",
-                "Juice from 1/2 lime",
-                "1/2 jalapeño, seeded and diced",
-                "1/4 cup cilantro leaves",
-                "2/3 cup nonfat plain greek yogurt",
-                "Juice from 1/2 lime",
-                "6 corn tortillas, warmed",
-                "Shredded purple cabbage",
-                "Cilantro",
-                "Crumbled cotija cheese",
-                "Lime wedges",
-                "Guacamole or diced avocado",
+                "2 small zucchini",
+                "2 eggs",
+                "1 cup grated parmesan cheese + ½ cup parmesan cut in thin strips",
+                "½ cup grated breadcrumbs with garlic powder and dried parsley",
+                "1 white onion",
+                "1 garlic clove",
+                "2 tablespoons olive oil",
+                "1 ½ teaspoons salt",
+                "1 teaspoon dried rosemary",
+                "½ teaspoon black pepper",
+                "1 bunch of fresh thyme",
             ],
             self.harvester_class.ingredients(),
         )
 
     def test_instructions(self):
         return self.assertEqual(
-            "Place shrimp in a medium, nonreactive bowl.\nIn a small bowl, combine chipotle, garlic powder, and cumin. Sprinkle over shrimp. Add oil and lime juice and stir well until shrimp are evenly coated. Set aside while you prepare the cilantro jalapeño sauce.\nIn a food processor or blender, combine jalapeño, cilantro, yogurt, and lime juice. Process until smooth and pour into a small bowl for serving.\nPrepare remaining taco toppings.\nHeat a large non-stick skillet over medium heat. Add shrimp and cook about 3 minutes per side or until cooked through.\nServe with warm corn tortillas and top as desired. Enjoy!",
+            "Preheat the oven to 395F.\nWash the zucchinis (alternatively you can also peel them) and chop the ends.\nIn a big bowl place the box grater and grate the zucchini in the biggest hole setting.\nDice thinly the onion and mince the garlic clove.\nAdd a bit of salt to the bowl with the grated zucchini, this will help them to lose their water. Let rest while you cook the rest of the ingredients.\nIn a pan add the olive oil and minced garlic and chopped onion. Season with a bit of salt, pepper, rosemary, and thyme. Cook until soft and fragrant.\nDrain the zucchinis squeezing them with your hands.\nTransfer the cooked onion and garlic into the bowl with the drained zucchini.\nAdd the eggs and beat them, once beaten ad the grated parmesan cheese to the mixture, rest of the seasonings, and mix well.\nTransfer the mixture to a baking dish and spread evenly.\nCover with the breadcrumbs and add a final layer of parmesan strips. Add a bit more thyme on top.\nCook in the oven for 20 minutes.",
             self.harvester_class.instructions(),
         )
