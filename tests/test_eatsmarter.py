06d47bc2b5921ab26f7f2f5a4f4a2d087f85c801
James Addison <james@reciperadar.com>
2023-09-13 13:19:44 +0100

    eatsmarter: update test HTML and unit test expectations

--- tests/test_eatsmarter.py
+++ tests/test_eatsmarter.py
@@ -11,14 +11,12 @@ class TestEatSmarter(ScraperTest):
 
     def test_canonical_url(self):
         self.assertEqual(
-            "https://eatsmarter.com/recipes/citrus-avocado-salad-with-almonds",
+            "https://eatsmarter.com/recipes/pumpkin-black-bean-salad",
             self.harvester_class.canonical_url(),
         )
 
     def test_title(self):
-        self.assertEqual(
-            self.harvester_class.title(), "Citrus Avocado Salad with Almonds"
-        )
+        self.assertEqual(self.harvester_class.title(), "Pumpkin Black Bean Salad")
 
     def test_author(self):
         self.assertEqual(self.harvester_class.author(), "EAT SMARTER")
@@ -29,26 +27,38 @@ class TestEatSmarter(ScraperTest):
     def test_ingredients(self):
         self.assertEqual(
             [
-                "2 Tbsps slivered almonds",
-                "2 stalks Celery",
-                "2 ozs green Olives (pitted)",
-                "3 Oranges",
-                "1 pink Grapefruit",
-                "1 Endive",
-                "1 Avocado",
+                "22 ozs Hokkaido pumpkin",
+                "0.25 oz ginger (1 piece)",
                 "2 Tbsps olive oil",
+                "salt",
+                "peppers",
+                "1 oz Baby spinach (1 lg handful)",
+                "3 shallots",
+                "2 garlic cloves",
+                "8 ozs black Beans (can; drained)",
+                "5 Tbsps Canola oil",
+                "4 Tbsps lemon juice",
+                "1 tsp honey",
+                "1 Tbsp Hazelnut oil",
             ],
             self.harvester_class.ingredients(),
         )
 
     def test_instructions(self):
         return self.assertEqual(
-            "Toast almonds in a dry pan until golden and fragrant. Place into a bowl to cool. Rinse and thinly slice celery. Cut olives into slices. Squeeze orange juice from one orange. Peel remaining oranges well and fillet, cut fillets into pieces. Repeat the same steps with grapefruit. Separate endive into individual leaves, rinse and spin dry, cut into strips. Peel and pit avocado, cut pulp into small cubes. Combine avocado cubes with orange juice in a bowl. Add all remaining prepared salad ingredients, drizzle with oil and toss salad carefully. Arrange on plates and serve.",
+            "\n".join(
+                [
+                    "Clean pumpkin, wash, cut in half, remove seeds, cut flesh into narrow wedges. Peel the ginger and chop finely. Mix olive oil with salt, pepper and ginger. Brush the pumpkin wedges with it and place on a baking tray lined with baking paper and cook in a preheated oven at 180 °C / 350 °F for about 25 minutes.",
+                    "Meanwhile, wash spinach and spin dry. Peel shallots and cut into wedges. Peel garlic and cut in half lengthwise. Rinse beans and drain in a sieve.",
+                    "Heat 2 tablespoons of oil in a frying pan. Sauté shallots and garlic in it over medium heat for 2 minutes, then remove. Pour remaining oil and heat over high heat. Add drained beans and fry for 3-4 minutes until they pop.",
+                    "Mix the lemon juice, salt, honey, pepper and nut oil to make a dressing. Arrange the pumpkin, beans, shallots and garlic on plates and drizzle with the dressing.",
+                ]
+            ),
             self.harvester_class.instructions(),
         )
 
     def test_total_time(self):
-        self.assertEqual(20, self.harvester_class.total_time())
+        self.assertEqual(35, self.harvester_class.total_time())
 
     def test_cook_time(self):
         return self.assertEqual(None, self.harvester_class.cook_time())
