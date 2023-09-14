c20c54255bd5e923c06e86bf808ed2dd041997ca
James Addison <james@reciperadar.com>
2023-09-14 17:21:21 +0100

    Revert "Revert "tudogostoso: add language test""
    This reverts commit 86241527325533dd523c857e1605fb4b4ab4572f.

--- tests/test_tudogostoso.py
+++ tests/test_tudogostoso.py
@@ -36,3 +36,6 @@ class TestTudoGostosoScraper(ScraperTest):
             ),
             self.harvester_class.instructions(),
         )
+
+    def test_language(self):
+        self.assertEqual("pt-br", self.harvester_class.language())
