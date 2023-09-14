780c3a0edbfda36f10816e7b5968721a2d522e93
James Addison <james@reciperadar.com>
2023-09-14 17:05:12 +0100

    Revert "heinzbrasil: add language test"
    This reverts commit 28c269543d158f6bdacd7dcb9a1bc96050518a1c.

--- tests/test_heinzbrasil.py
+++ tests/test_heinzbrasil.py
@@ -53,9 +53,6 @@ class TestHeinzBrasilScraper(ScraperTest):
             self.harvester_class.instructions(),
         )
 
-    def test_language(self):
-        self.assertEqual("pt-BR", self.harvester_class.language())
-
     def test_multiple_instructions(self):
         # override: this test case legitimately does only contain a single instruction in the source HTML
         pass
