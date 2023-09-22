f04725c4da218002ffb0a54cb77197494be5da95
Joey <joey.kennedy1@gmail.com>
2023-09-22 15:14:17 -0400

    Adds ingredient grouping support

--- recipe_scrapers/themagicalslowcooker.py
+++ recipe_scrapers/themagicalslowcooker.py
@@ -1,6 +1,7 @@
 # mypy: allow-untyped-defs
 
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 
 
 class TheMagicalSlowCooker(AbstractScraper):
@@ -9,11 +10,8 @@ class TheMagicalSlowCooker(AbstractScraper):
         return "themagicalslowcooker.com"
 
     def author(self):
-        return self.soup.find(
-            'span',
-            {'class': 'entry-author-name'}
-        ).get_text()
-    
+        return self.soup.find("span", {"class": "entry-author-name"}).get_text()
+
     def title(self):
         return self.schema.title()
 
@@ -31,6 +29,14 @@ class TheMagicalSlowCooker(AbstractScraper):
 
     def ingredients(self):
         return self.schema.ingredients()
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".wprm-recipe-ingredient-group h4",
+            ".wprm-recipe-ingredients li",
+        )
 
     def instructions(self):
         return self.schema.instructions()
--- tests/test_themagicalslowcooker_1.py
+++ tests/test_themagicalslowcooker_1.py
@@ -28,13 +28,28 @@ class TestTheMagicalSlowCookerScraper(ScraperTest):
         self.assertEqual("6 servings", self.harvester_class.yields())
 
     def test_image(self):
-        self.assertEqual("https://www.themagicalslowcooker.com/wp-content/uploads/2023/08/crockpot-ground-beef-8.jpg", self.harvester_class.image())
+        self.assertEqual(
+            "https://www.themagicalslowcooker.com/wp-content/uploads/2023/08/crockpot-ground-beef-8.jpg",
+            self.harvester_class.image(),
+        )
 
     def test_ingredients(self):
-        self.assertEqual(['2 lbs. ground beef ((7-20 % fat))', '1 ½ tsp. salt', '½ tsp. garlic powder', '½ tsp. onion powder', '1 small white onion (diced)'], self.harvester_class.ingredients())
+        self.assertEqual(
+            [
+                "2 lbs. ground beef ((7-20 % fat))",
+                "1 ½ tsp. salt",
+                "½ tsp. garlic powder",
+                "½ tsp. onion powder",
+                "1 small white onion (diced)",
+            ],
+            self.harvester_class.ingredients(),
+        )
 
     def test_instructions(self):
-        self.assertEqual("Crumble the beef slightly with your fingers and add to the slow cooker.\nSprinkle over the salt, pepper, onion powder and garlic powder.\nAdd the diced white onion. Stir.\nCook on low for 5 hours or on high for 3 hours. Stirring occasionally, when stirring break up the meat with the spatula.\nServe and enjoy.", self.harvester_class.instructions())
+        self.assertEqual(
+            "Crumble the beef slightly with your fingers and add to the slow cooker.\nSprinkle over the salt, pepper, onion powder and garlic powder.\nAdd the diced white onion. Stir.\nCook on low for 5 hours or on high for 3 hours. Stirring occasionally, when stirring break up the meat with the spatula.\nServe and enjoy.",
+            self.harvester_class.instructions(),
+        )
 
     def test_ratings(self):
         self.assertEqual(5.0, self.harvester_class.ratings())
@@ -43,4 +58,7 @@ class TestTheMagicalSlowCookerScraper(ScraperTest):
         self.assertEqual("American", self.harvester_class.cuisine())
 
     def test_description(self):
-        self.assertEqual("Whip up the best-tasting ground beef using just 5 ingredients. This Slow Cooker Ground Beef can be used in just about every recipe!", self.harvester_class.description())
+        self.assertEqual(
+            "Whip up the best-tasting ground beef using just 5 ingredients. This Slow Cooker Ground Beef can be used in just about every recipe!",
+            self.harvester_class.description(),
+        )
