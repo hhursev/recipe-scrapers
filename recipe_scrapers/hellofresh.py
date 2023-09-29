85ecd3b9da49a2e49c1e2bd68b099a40ed78862f
Joey <joey.kennedy1@gmail.com>
2023-09-29 13:18:41 -0400

    Updated total_time to include prep_time

--- recipe_scrapers/hellofresh.py
+++ recipe_scrapers/hellofresh.py
@@ -1,5 +1,8 @@
 # mypy: disallow_untyped_defs=False
+import re
+
 from ._abstract import AbstractScraper
+from ._utils import get_minutes
 
 
 class HelloFresh(AbstractScraper):
@@ -11,7 +14,21 @@ class HelloFresh(AbstractScraper):
         return self.schema.title()
 
     def total_time(self):
-        return self.schema.total_time()
+        script_tag = self.soup.find("script", {"id": "__NEXT_DATA__"})
+
+        if script_tag:
+            script_content = script_tag.string
+            total_time_match = re.search(r'"totalTime":"(PT\d+M)"', script_content)
+            prep_time_match = re.search(r'"prepTime":"(PT\d+M)"', script_content)
+
+            if total_time_match and prep_time_match:
+                total_time_str = total_time_match.group(1)
+                prep_time_str = prep_time_match.group(1)
+
+                total_time_minutes = get_minutes(total_time_str)
+                prep_time_minutes = get_minutes(prep_time_str)
+
+                return total_time_minutes + prep_time_minutes
 
     def yields(self):
         return self.schema.yields()
