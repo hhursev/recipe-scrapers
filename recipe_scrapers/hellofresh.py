02b9f48c100783474a26617d42e81421540673bb
Joey <joey.kennedy1@gmail.com>
2023-09-30 16:09:11 -0400

    Refactor total_time method and update comments for HelloFresh metadata

--- recipe_scrapers/hellofresh.py
+++ recipe_scrapers/hellofresh.py
@@ -13,22 +13,30 @@ class HelloFresh(AbstractScraper):
     def title(self):
         return self.schema.title()
 
-    def total_time(self):
+    def cook_time(self):
         script_tag = self.soup.find("script", {"id": "__NEXT_DATA__"})
-
         if script_tag:
             script_content = script_tag.string
             total_time_match = re.search(r'"totalTime":"(PT\d+M)"', script_content)
-            prep_time_match = re.search(r'"prepTime":"(PT\d+M)"', script_content)
-
-            if total_time_match and prep_time_match:
+            if total_time_match:
                 total_time_str = total_time_match.group(1)
+                return get_minutes(total_time_str)
+        return 0
+
+    def prep_time(self):
+        script_tag = self.soup.find("script", {"id": "__NEXT_DATA__"})
+        if script_tag:
+            script_content = script_tag.string
+            prep_time_match = re.search(r'"prepTime":"(PT\d+M)"', script_content)
+            if prep_time_match:
                 prep_time_str = prep_time_match.group(1)
+                return get_minutes(prep_time_str)
+        return 0
 
-                total_time_minutes = get_minutes(total_time_str)
-                prep_time_minutes = get_minutes(prep_time_str)
-
-                return total_time_minutes + prep_time_minutes
+    # Note: HelloFresh uses the 'totalTime' metadata field to represent only the cook time.
+    # To get the actual total time, the 'prepTime' and 'totalTime' (which is the cook time) need to be added.
+    def total_time(self):
+        return self.cook_time() + self.prep_time()
 
     def yields(self):
         return self.schema.yields()
