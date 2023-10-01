f876d1ea3dda70715723ff5c693491e4a33dda5f
Joey <joey.kennedy1@gmail.com>
2023-09-30 20:22:00 -0400

    Update hellofresh.py
    Removed "return 0" and implemented recommended adjustments.

--- recipe_scrapers/hellofresh.py
+++ recipe_scrapers/hellofresh.py
@@ -21,7 +21,6 @@ class HelloFresh(AbstractScraper):
             if total_time_match:
                 total_time_str = total_time_match.group(1)
                 return get_minutes(total_time_str)
-        return 0
 
     def prep_time(self):
         script_tag = self.soup.find("script", {"id": "__NEXT_DATA__"})
@@ -31,12 +30,13 @@ class HelloFresh(AbstractScraper):
             if prep_time_match:
                 prep_time_str = prep_time_match.group(1)
                 return get_minutes(prep_time_str)
-        return 0
 
     # Note: HelloFresh uses the 'totalTime' metadata field to represent only the cook time.
     # To get the actual total time, the 'prepTime' and 'totalTime' (which is the cook time) need to be added.
     def total_time(self):
-        return self.cook_time() + self.prep_time()
+        prep_time, cook_time = self.prep_time(), self.cook_time()
+        if prep_time or cook_time:
+            return (prep_time or 0) + (cook_time or 0)
 
     def yields(self):
         return self.schema.yields()
