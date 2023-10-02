ce54cd0535d16570b9003ffb1b0e8e8cfe031f4e
Joey <joey.kennedy1@gmail.com>
2023-10-02 16:31:39 -0400

    Update foodandwine.py
    Linting

--- recipe_scrapers/foodandwine.py
+++ recipe_scrapers/foodandwine.py
@@ -1,6 +1,6 @@
 # mypy: disallow_untyped_defs=False
 from ._abstract import AbstractScraper
-from ._utils import normalize_string, get_yields
+from ._utils import get_yields, normalize_string
 
 
 class FoodAndWine(AbstractScraper):
@@ -15,9 +15,13 @@ class FoodAndWine(AbstractScraper):
         return self.schema.total_time()
 
     def yields(self):
-        yield_label = self.soup.find('div', {'class': 'mntl-recipe-details__label'}, string='Yield:')
+        yield_label = self.soup.find(
+            "div", {"class": "mntl-recipe-details__label"}, string="Yield:"
+        )
         if yield_label:
-            yield_value = yield_label.find_next('div', {'class': 'mntl-recipe-details__value'})
+            yield_value = yield_label.find_next(
+                "div", {"class": "mntl-recipe-details__value"}
+            )
             if yield_value:
                 return get_yields(normalize_string(yield_value.text))
 
