098da0cab799901f86df130c5c45d2b3379673bc
Jaapio <jaap@ijaap.nl>
2023-10-02 23:51:27 +0200

    Add support for legacy recipe format
    The oldest recipes do not have the nice classes to find elements,
    so we do need text matching on the headers.

--- recipe_scrapers/keukenliefdenl.py
+++ recipe_scrapers/keukenliefdenl.py
@@ -1,5 +1,4 @@
 # mypy: allow-untyped-defs
-
 from ._abstract import AbstractScraper
 from ._utils import get_minutes, get_yields, normalize_string
 
@@ -36,21 +35,48 @@ class KeukenLiefdeNL(AbstractScraper):
         return self.schema.image()
 
     def ingredients(self):
-        ingredients = self.soup.find(
-            "div", {"id": "clipboard-ingredients"}
-        ).findChildren("li")
+        ingredents_container = self.soup.find("div", {"id": "clipboard-ingredients"})
+        if ingredents_container:
+            return self.process_ingredients(ingredents_container)
+
+        ingredient_header = self.soup.find("strong", string="Ingrediënten")
+        if ingredient_header:
+            return self.process_ingredients(
+                ingredient_header.parent.find_next_sibling("ul")
+            )
+
+        # Nothing found, we give up.
+        return []
+
+    def process_ingredients(self, container):
+        ingredients = container.findChildren("li")
 
         return [normalize_string(i.get_text()) for i in ingredients]
 
     def instructions(self):
         # Old recipes are written in paragraphs, new ones are a list.
-        paragraphs = self.soup.find("div", {"class": "preparation"}).find_all("p")
-        listitems = self.soup.find("div", {"class": "preparation"}).find_all("li")
+        preparation = self.soup.find("div", {"class": "preparation"})
+        if preparation:
+            return self.normalize_instructions(
+                preparation.find_all("p") + preparation.find_all("li")
+            )
 
+        # There are some really old recipes that do not have the nice classes
+        instructions_heading = self.soup.find("strong", string="Bereiding")
+        if instructions_heading:
+            # We are doing some assumptions here
+            return self.normalize_instructions(
+                [instructions_heading.parent]
+                + instructions_heading.parent.find_next_siblings("p")
+            )
+
+        return ""
+
+    def normalize_instructions(self, instructions):
         instructions = [
             normalize_string(item.get_text())
-            for item in paragraphs + listitems
-            if item.get_text()
+            for item in instructions
+            if normalize_string(item.get_text())
         ]
 
         return "\n".join(instructions)
