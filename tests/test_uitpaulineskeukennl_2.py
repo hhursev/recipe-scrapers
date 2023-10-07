91b9167e16cf0960cf33d0cc6661118e2a66a12c
ronajon <ronajon@mailbox.org>
2023-10-07 09:06:39 +0200

    modified 2nd test file to include ingredient grouping

--- tests/test_uitpaulineskeukennl_2.py
+++ tests/test_uitpaulineskeukennl_2.py
@@ -1,10 +1,12 @@
 # mypy: allow-untyped-defs
 
+from recipe_scrapers._grouping_utils import IngredientGroup
 from recipe_scrapers.uitpaulineskeukennl import UitPaulinesKeukenNl
 from tests import ScraperTest
 
 
-class TestUitPaulinesKeukenNlScraper(ScraperTest):
+class TestuitPaulinesKeukenNl(ScraperTest):
+
     scraper_class = UitPaulinesKeukenNl
     test_file_name = "uitpaulineskeukennl_2"
 
@@ -12,79 +14,127 @@ class TestUitPaulinesKeukenNlScraper(ScraperTest):
         self.assertEqual("uitpaulineskeuken.nl", self.harvester_class.host())
 
     def test_author(self):
-        self.assertEqual("Monique van Loon", self.harvester_class.author())
+        self.assertEqual("Marianne Snel", self.harvester_class.author())
 
     def test_title(self):
-        self.assertEqual("Crispy sushi bites", self.harvester_class.title())
+        self.assertEqual("Blondies met citroen", self.harvester_class.title())
 
     def test_category(self):
         self.assertEqual(
-            "Gezonde tussendoortjes, Avocado, Rijst, Zalm, Aziatische recepten, Gezonde recepten, "
-            "Lactosevrije recepten, Notenvrije recepten, Oud en nieuw, Vis recepten, Herfst recepten, Lente "
-            "recepten, Winter recepten, Zomer recepten, Borrelhapjes, Lunch, Voorgerecht",
+            "Cake recepten, High tea, Koekjes recepten, Chocolade, Verjaardag, Bakrecepten",
             self.harvester_class.category(),
         )
 
     def test_total_time(self):
-        self.assertEqual(50, self.harvester_class.total_time())
+        self.assertEqual(55, self.harvester_class.total_time())
 
     def test_yields(self):
-        self.assertEqual("10 servings", self.harvester_class.yields())
+        self.assertEqual("12 servings", self.harvester_class.yields())
 
     def test_image(self):
         self.assertEqual(
-            "https://uitpaulineskeuken.nl/wp-content/uploads/2023/07/Crispy-sushi-bites2.jpg",
+            "https://uitpaulineskeuken.nl/wp-content/uploads/2022/07/Citroen-blondies-UPK-22-11-JUL-BLOGPOST-258.jpg",
             self.harvester_class.image(),
         )
 
     def test_ingredients(self):
         self.assertEqual(
             [
-                "200 gr sushirijst",
-                "350 ml water",
-                "4 el sushi-azijn (voor de rijst)",
-                "zonnebloemolie",
-                "250 gr verse zalmfilet (zonder huid)",
-                "2 avocado's (in dunne plakjes)",
-                "0,5 komkommer (in kleine blokjes)",
-                "2 el sojasaus",
-                "1+1 el sushi-azijn",
-                "1/2 limoen (alleen het sap)",
-                "1 el sesamolie",
-                "0,5 tl chilivlokken",
-                "1 el (zwart) sesamzaadjes",
-                "sriracha mayonaise",
+                "150 gr witte chocolade",
+                "200 gr bloem",
+                "125 gr roomboter",
+                "2 eieren",
+                "150 gr suiker",
+                "1,5 tl vanille-extract",
+                "130 gr garneer amandelen",
+                "4 tl citroenrasp",
+                "4 el citroensap",
+                "snufje zout",
+                "2 eiwitten",
+                "250 gr poedersuiker",
+                "2 tl citroenrasp",
+                "3 el citroensap",
+                "1 citroen (garnering )",
+                "Bakvorm 20x20cm",
             ],
             self.harvester_class.ingredients(),
+        )
+
+    def test_ingredient_groups(self):
+        self.assertEqual(
+            [
+                IngredientGroup(
+                    ingredients=[
+                        "150 gr witte chocolade",
+                        "200 gr bloem",
+                        "125 gr roomboter",
+                        "2 eieren",
+                        "150 gr suiker",
+                        "1,5 tl vanille-extract",
+                        "130 gr garneer amandelen",
+                        "4 tl citroenrasp",
+                        "4 el citroensap",
+                        "snufje zout",
+                    ],
+                    purpose="Ingrediënten 9-12 blondies",
+                ),
+                IngredientGroup(
+                    ingredients=[
+                        "2 eiwitten",
+                        "250 gr poedersuiker",
+                        "2 tl citroenrasp",
+                        "3 el citroensap",
+                        "1 citroen (garnering )",
+                    ],
+                    purpose="Ingrediënten topping",
+                ),
+                IngredientGroup(
+                    ingredients=[
+                        "Bakvorm 20x20cm",
+                    ],
+                    purpose="Materiaal",
+                ),
+            ],
+            self.harvester_class.ingredient_groups(),
         )
 
     def test_instructions(self):
         self.assertEqual(
             "\n".join(
                 [
-                    "Maak de sushirijst zoals op de verpakking vermeld staat. Breng de rijst op smaak met sushi-azijn. Neem een ronde uitsteker van ca 5 cm. Verdeel 3 el sushirijst over het vormpje en druk het aan. Ga door totdat alle rijst verwerkt is. Je kan de rondjes eventueel een dag van tevoren maken en bewaren in de koelkast.",
-                    "Laat wat zonnebloemolie heet worden in een koekenpan. Bak de sushi rondjes aan weerszijden goudbruin en krokant. Leg apart.",
-                    "Snijd de zalm in blokjes en doe dit in een bakje. Voeg 2 el sojasaus, 1 el sushi-azijn, 1 el sesamolie, het sap van een halve limoen en een 0,5 tl chili vlokken en het sesamzaad toe en leg apart.",
-                    "Snijd de avocado in hele dunne plakjes. Snijd de komkommer in blokjes en breng deze op smaak met 1 el sushi-azijn.",
-                    "Verdeel over ieder stukje gebakken sushi een paar plakjes avocado, een lepel gemarineerde zalm, wat zoetzure komkommer en garneer met sriracha mayo.",
+                    "Verwarm de oven voor op 175 graden (boven- en onderwarmte), 160 graden (hetelucht). Bekleed de "
+                    "bakvorm met bakpapier.",
+                    "Hak de amandelen met een scherp mes in kleine stukjes.",
+                    "Breek de chocolade in stukjes en laat samen met de boter in een steelpannetje op laag vuur "
+                    "smelten tot een gladde massa. Neem de pan van het vuur en roer de suiker, snufje zout, "
+                    "vanille-extract en citroenrasp er door. Voeg 1 voor 1 de eieren toe en klop goed door, "
+                    "blijf kloppen zodat de eieren niet stollen. Schep de bloem en gehakte amandelen door het beslag.",
+                    "Verdeel het beslag over de bakvorm en strijk de bovenkant glad. Bak de blondies 35-40 minuten in "
+                    "de voorverwarmde oven tot de bovenkant goudbruin is. Haal uit de oven, prik verspreid over de "
+                    "koek met een vork gaatjes en schenk de citroensap er over en laat helemaal afkoelen.",
+                    "Maak ondertussen het glazuur. Klop de eiwitten, citroensap en rasp in een mengkom en roer de "
+                    "poedersuiker er door. Meng tot een mooi glad glazuur. Strijk het glazuur over de bovenkant van "
+                    "de blondies. Snijd in 9-12 blokjes. Snijd de citroen in dunne plakjes en leg op elk blokje een "
+                    "citroentje.",
                 ]
             ),
             self.harvester_class.instructions(),
         )
 
     def test_ratings(self):
-        self.assertEqual(5.0, self.harvester_class.ratings())
+        self.assertEqual(3.7, self.harvester_class.ratings())
 
     def test_cuisine(self):
-        self.assertEqual("Aziatische recepten", self.harvester_class.cuisine())
+        self.assertEqual("", self.harvester_class.cuisine())
 
     def test_description(self):
         self.assertEqual(
-            "Zin in sushi, maar wil je eens wat anders aan je gezin, jezelf of gasten serveren? Dan zijn "
-            "deze crispy sushi bites met zalm, avocado & komkommer het perfecte hapje voor jou! Lekker "
-            "om te dippen in sojasaus. Deze bites zijn eenvoudig te maken en perfect voor een feestje. "
-            "Bak de sushirijst goudbruin en krokant in zonnebloemolie, en voeg vervolgens de "
-            "gemarineerde zalm, avocado en zoetzure komkommer toe. Garneer met een vleugje sriracha mayo "
-            "voor extra pit. Geniet van deze kleurrijke crispy sushi!",
+            "Zin in iets lekkers? Probeer dan eens deze heerlijke\xa0blondies met citroen. Blondies zijn "
+            "brownies maar dan van witte chocolade. Om ze lekker fris en zomers te maken hebben we in "
+            "dit blondie recept veel citroen verwerkt. Zo bestrijk je de koek na het bakken direct met "
+            "citroensap, dan kan de citroensmaak er goed in trekken. En daarna besmeer je het met "
+            "citroenglazuur. Op en top zomer! Met dit recept maak je een koek die je in 9 tot 12 stukjes "
+            "kunt snijden. Lekker voor bij de thee of koffie, ze staan geweldig op een high tea tafel en "
+            "zijn een superleuke afsluiter voor een gezellige picknick! Zin in?",
             self.harvester_class.description(),
         )
