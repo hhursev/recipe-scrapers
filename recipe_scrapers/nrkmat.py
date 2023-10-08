016d185d0991379b9367e0c35a2514de087e51da
Mia Pernille Treland <mia.treland@gmail.com>
2023-10-08 18:56:41 +0200

    add support for nrk.no/mat

---
+++ recipe_scrapers/nrkmat.py
@@ -0,0 +1,41 @@
+# mypy: allow-untyped-defs
+
+from ._abstract import AbstractScraper
+from ._exceptions import SchemaOrgException
+
+
+class NRKMat(AbstractScraper):
+    @classmethod
+    def host(cls):
+        return "nrk.no"
+
+    def author(self):
+        return (
+            self.soup.find("span", {"class": "caption-cite"})
+            .getText()
+            .removeprefix("Kilde: ")
+        )
+
+    def title(self):
+        return self.schema.title()
+
+    def category(self):
+        return self.schema.category()
+
+    def yields(self):
+        try:
+            return self.schema.yields()
+        except SchemaOrgException:
+            return ""
+
+    def image(self):
+        return self.schema.image()
+
+    def ingredients(self):
+        return self.schema.ingredients()
+
+    def instructions(self):
+        return self.schema.instructions()
+
+    def description(self):
+        return self.schema.description()
