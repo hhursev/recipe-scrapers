7c35960666ec2ac12f65beeaefd6623d0ba6426e
Mia Pernille Treland <mia.treland@gmail.com>
2023-10-09 22:05:26 +0200

    add nutrients for matprat

--- recipe_scrapers/matprat.py
+++ recipe_scrapers/matprat.py
@@ -30,3 +30,15 @@ class Matprat(AbstractScraper):
 
     def description(self):
         return self.schema.description()
+
+    def nutrients(self):
+        nutrient_names = [
+            x.text for x in self.soup("td", "recipe-nutritions__table-cell-name")
+        ]
+        if not nutrient_names:
+            return None
+        nutrient_values = [
+            x.text.replace("\xa0", " ")
+            for x in self.soup("td", "recipe-nutritions__table-cell-value")
+        ]
+        return dict(zip(nutrient_names, nutrient_values))
