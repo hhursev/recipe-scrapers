31c1a72bf9ca4890f6f3be0fccb2de3f1f127daf
Joey <joey.kennedy1@gmail.com>
2023-10-09 22:08:29 -0400

    Update foodnetwork

--- recipe_scrapers/foodnetwork.py
+++ recipe_scrapers/foodnetwork.py
@@ -1,4 +1,3 @@
-
 from ._abstract import AbstractScraper
 
 
--- tests/test_foodnetwork.py
+++ tests/test_foodnetwork.py
@@ -12,7 +12,10 @@ class TestFoodNetworkScraper(ScraperTest):
         self.assertEqual("foodnetwork.co.uk", self.harvester_class.host())
 
     def test_canonical_url(self):
-        self.assertEqual("https://foodnetwork.co.uk/recipes/tuscan-mushrooms-5389", self.harvester_class.canonical_url())
+        self.assertEqual(
+            "https://foodnetwork.co.uk/recipes/tuscan-mushrooms-5389",
+            self.harvester_class.canonical_url(),
+        )
 
     def test_author(self):
         self.assertEqual("Food Network UK", self.harvester_class.author())
@@ -27,10 +30,23 @@ class TestFoodNetworkScraper(ScraperTest):
         self.assertEqual("4 servings", self.harvester_class.yields())
 
     def test_image(self):
-        self.assertEqual("https://d2vsf1hynzxim7.cloudfront.net/production/media/21503/conversions/foodnetwork-image-9b081a76-feaf-4b63-baea-077714959bc7-default.webp", self.harvester_class.image())
+        self.assertEqual(
+            "https://d2vsf1hynzxim7.cloudfront.net/production/media/21503/conversions/foodnetwork-image-9b081a76-feaf-4b63-baea-077714959bc7-default.webp",
+            self.harvester_class.image(),
+        )
 
     def test_ingredients(self):
-        expected_ingredients = ['50g diced jarred roasted sweet red peppers', '50g diced pitted green olives', '30g grated Pecorino Romano', '2 spring onions, diced', '2 tbsp extra-virgin olive oil', '1/2 tsp salt', '1/4 tsp freshly ground black pepper', '450g white button mushrooms, cleaned and stemmed', '10g finely chopped fresh basil leaves']
+        expected_ingredients = [
+            "50g diced jarred roasted sweet red peppers",
+            "50g diced pitted green olives",
+            "30g grated Pecorino Romano",
+            "2 spring onions, diced",
+            "2 tbsp extra-virgin olive oil",
+            "1/2 tsp salt",
+            "1/4 tsp freshly ground black pepper",
+            "450g white button mushrooms, cleaned and stemmed",
+            "10g finely chopped fresh basil leaves",
+        ]
         self.assertEqual(expected_ingredients, self.harvester_class.ingredients())
 
     def test_instructions(self):
@@ -43,4 +59,7 @@ class TestFoodNetworkScraper(ScraperTest):
         self.assertEqual("Italian", self.harvester_class.cuisine())
 
     def test_description(self):
-        self.assertEqual("Stuff these mushrooms with roasted red peppers and olives.", self.harvester_class.description())
+        self.assertEqual(
+            "Stuff these mushrooms with roasted red peppers and olives.",
+            self.harvester_class.description(),
+        )
