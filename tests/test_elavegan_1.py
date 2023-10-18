2711657ebf5561d73d4ddd48280ecdccf5536415
Joey <joey.kennedy1@gmail.com>
2023-10-18 17:17:20 -0400

    Ingredient grouping

--- tests/test_elavegan_1.py
+++ tests/test_elavegan_1.py
@@ -12,6 +12,12 @@ class TestElaVeganScraper(ScraperTest):
     def test_host(self):
         self.assertEqual("elavegan.com", self.harvester_class.host())
 
+    def test_canonical_url(self):
+        self.assertEqual(
+            "https://elavegan.com/red-lentil-dahl/",
+            self.harvester_class.canonical_url(),
+        )
+
     def test_author(self):
         self.assertEqual("Ela", self.harvester_class.author())
 
