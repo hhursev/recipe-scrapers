fe31c8ee6caeba92b9acb52c5615bcf689dd34de
Ryan Winter <ryanwinter@outlook.com>
2023-10-20 17:08:11 -0700

    complete second recipe version
    Signed-off-by: Ryan Winter <ryanwinter@outlook.com>

--- recipe_scrapers/juliegoodwin.py
+++ recipe_scrapers/juliegoodwin.py
@@ -2,6 +2,7 @@
 import re
 
 from ._abstract import AbstractScraper
+from ._exceptions import ElementNotFoundInHtml
 from ._utils import get_minutes, normalize_string
 
 
@@ -16,60 +17,73 @@ class JulieGoodwin(AbstractScraper):
     def title(self):
         return normalize_string(self.soup.find("h1").get_text())
 
-    def total_time(self):
-        mins = 0
-        prep = self.soup.find(string=re.compile("Prep time"))
-        if prep:
-            prep_hours_match = re.search(r"(\d+) hours", prep.parent.parent.get_text())
+    def process_total_time(self, container):
+        if container:
+            prep_hours_match = re.search(
+                r"(\d+) hour", container.next_element.get_text()
+            )
             if prep_hours_match:
-                mins += 60 * int(prep_hours_match.group(1))
-            prep_mins_match = re.search(r"(\d+) mins", prep.parent.parent.get_text())
+                return 60 * int(prep_hours_match.group(1))
+            prep_mins_match = re.search(r"(\d+) min", container.next_element.get_text())
             if prep_mins_match:
-                mins += int(prep_mins_match.group(1))
+                return int(prep_mins_match.group(1))
 
+    def total_time(self):
+        prep = self.soup.find(string=re.compile("Prep time"))
+        mins = self.process_total_time(prep)
         cooking = self.soup.find(string=re.compile("Cooking time"))
-        if cooking:
-            cooking_hours_match = re.search(
-                r"(\d+) hours", cooking.parent.parent.get_text()
-            )
-            if cooking_hours_match:
-                mins += 60 * int(cooking_hours_match.group(1))
-            cooking_mins_match = re.search(
-                r"(\d+) mins", cooking.parent.parent.get_text()
-            )
-            if cooking_mins_match:
-                mins += int(cooking_mins_match.group(1))
-
+        mins += self.process_total_time(cooking)
         return get_minutes(mins)
 
     def yields(self):
+        container = self.soup.find("i", attrs={"class", "fa-cutlery"})
+        if container:
+            return normalize_string(container.next_sibling.get_text())
+
         return normalize_string(self.soup.find("h4").get_text())
 
     def image(self):
         container = self.soup.find("div", {"class": "inner-wrap"})
-        if not container:
-            return None
-
-        image = container.find("img", {"src": True})
-        return image["src"] if image else None
+        if container:
+            image = container.findNext("img", {"src": True})
+            return image["src"] if image else None
 
     def ingredients(self):
-        ingredients = self.soup.find("ul", {"class", "ingredients"}).findAll("li")
+        ingredients = self.soup.find("ul", {"class", "ingredients"})
         if ingredients:
             return [
-                normalize_string(ingredient.get_text()) for ingredient in ingredients
+                normalize_string(ingredient.get_text())
+                for ingredient in ingredients.findAll("li")
             ]
 
-        heading = self.soup.find(string="Ingredients")
-        ingredients = heading.parent.next
-        if ingredients:
-            pass
+        ingredients_heading = self.soup.find(string="Ingredients")
+        if ingredients_heading:
+            ingredients = ingredients_heading.findNext("h5")
+            return [
+                normalize_string(ingredient.split("•", 1)[1])
+                for ingredient in ingredients.get_text().split("\n")
+            ]
+
+        raise ElementNotFoundInHtml("Could not find ingredients.")
 
     def instructions(self):
         instructions = self.soup.findAll(string=re.compile("STEP "))
-        return "\n".join(
-            [
-                normalize_string(inst.parent.parent.get_text().split("|")[1])
-                for inst in instructions
-            ]
-        )
+        if instructions:
+            return "\n".join(
+                [
+                    normalize_string(inst.parent.parent.get_text().split("|", 1)[1])
+                    for inst in instructions
+                ]
+            )
+
+        instruction_header = self.soup.find(string=re.compile("Method"))
+        if instruction_header:
+            instructions = instruction_header.parent.findNextSiblings("p")
+            return "\n".join(
+                [
+                    normalize_string(inst.get_text().split(".", 1)[1])
+                    for inst in instructions
+                ]
+            )
+
+        raise ElementNotFoundInHtml("Could not find instructions.")
