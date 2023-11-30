309372a21d29ad9e49e1f30c3836d82e8eb85f1e
James Addison <james@reciperadar.com>
2023-11-30 11:38:19 +0000

    bettybossi: remove custom __init__ method

--- recipe_scrapers/bettybossi.py
+++ recipe_scrapers/bettybossi.py
@@ -1,9 +1,6 @@
 # mypy: disallow_untyped_defs=False
-from typing import Dict, Optional, Tuple, Union
 
-from requests import Session
-
-from ._abstract import HEADERS, AbstractScraper
+from ._abstract import AbstractScraper
 
 
 class BettyBossi(AbstractScraper):
@@ -18,29 +15,6 @@ class BettyBossi(AbstractScraper):
     @classmethod
     def host(cls):
         return "bettybossi.ch"
-
-    def __init__(
-        self,
-        html: Union[str, bytes],
-        url: Union[str, None],
-        proxies: Optional[
-            Dict[str, str]
-        ] = None,  # allows us to specify optional proxy server
-        timeout: Optional[
-            Union[float, Tuple[float, float], Tuple[float, None]]
-        ] = None,  # allows us to specify optional timeout for request
-        wild_mode: Optional[bool] = False,
-    ) -> None:
-        if html is None:
-            with Session() as session:
-                session.proxies.update(proxies or {})
-                session.headers.update(HEADERS)
-
-                session.get(url, timeout=timeout)
-                html = session.get(url, timeout=timeout).content  # reload the page
-
-        # As the html content is provided, the parent will not query the page
-        super().__init__(html, url, proxies, timeout, wild_mode)
 
     def author(self):
         return self.schema.author()
