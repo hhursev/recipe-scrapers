3e258f71bd059a9d4379c2b253dff983f158825a
Joey <joey.kennedy1@gmail.com>
2023-12-11 15:08:32 -0500

    Grouping support

--- recipe_scrapers/thepalatablelife.py
+++ recipe_scrapers/thepalatablelife.py
@@ -1,6 +1,7 @@
 # mypy: allow-untyped-defs
 
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 
 
 class ThePalatableLife(AbstractScraper):
@@ -28,6 +29,14 @@ class ThePalatableLife(AbstractScraper):
 
     def ingredients(self):
         return self.schema.ingredients()
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".wprm-recipe-ingredient-group h4",
+            ".wprm-recipe-ingredient",
+        )
 
     def instructions(self):
         return self.schema.instructions()
