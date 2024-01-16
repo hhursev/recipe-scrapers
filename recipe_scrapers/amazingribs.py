3062ff5a7f371eb89ea07c50227de18d1891778d
Joey <joey.kennedy1@gmail.com>
2024-01-16 18:03:20 -0500

    reverts amazingribs

--- recipe_scrapers/amazingribs.py
+++ recipe_scrapers/amazingribs.py
@@ -37,7 +37,9 @@ class AmazingRibs(AbstractScraper):
         return self.schema.instructions()
 
     def equipment(self):
-        return {
-            normalize_string(e.get_text())
-            for e in self.soup.find_all("div", class_="wprm-recipe-equipment-name")
-        }
+        return sorted(
+            {
+                normalize_string(e.get_text())
+                for e in self.soup.find_all("div", class_="wprm-recipe-equipment-name")
+            }
+        )
