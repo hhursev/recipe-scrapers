d07d9957788ea334e5ff83a4d17aec1a9dda229a
Brandon Sprague <brandon00sprague@gmail.com>
2024-02-09 12:20:43 -0800

    Add the 'description' field for seven existing parsers (#999)
    See [a downstream PR for more context](https://github.com/poundifdef/plainoldrecipe/pull/176), which is where I came across this.
    
    This adds the description field for a few parsers that definitely support it. These are only ones in-common with the downstream app linked above, there are likely plenty more that could benefit from this.
    
    This is broadly similar to #554

--- recipe_scrapers/thewoksoflife.py
+++ recipe_scrapers/thewoksoflife.py
@@ -10,6 +10,9 @@ class Thewoksoflife(AbstractScraper):
     def title(self):
         return self.schema.title()
 
+    def description(self):
+        return self.schema.description()
+
     def total_time(self):
         return self.schema.total_time()
 
