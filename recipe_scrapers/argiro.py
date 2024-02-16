429e47d43b9aa3f2e0318952028deed955edbcb2
Fagogenis <72733006+Fagogenis@users.noreply.github.com>
2024-02-16 23:20:48 +0200

    Add scraper for argiro.gr (#958)

---
+++ recipe_scrapers/argiro.py
@@ -0,0 +1,40 @@
+from ._abstract import AbstractScraper
+
+
+class Argiro(AbstractScraper):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def host(cls):
+        return "argiro.gr"
+
+    def author(self):
+        return self.schema.author()
+
+    def title(self):
+        return self.schema.title()
+
+    def category(self):
+        return self.schema.category()
+
+    def total_time(self):
+        return self.schema.total_time()
+
+    def yields(self):
+        return self.schema.yields()
+
+    def image(self):
+        return self.schema.image()
+
+    def ingredients(self):
+        return self.schema.ingredients()
+
+    def instructions(self):
+        return self.schema.instructions()
+
+    def cuisine(self):
+        return self.schema.cuisine()
+
+    def description(self):
+        return self.schema.description()
