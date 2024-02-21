7a8201e66603debf92339f7ed4beed6b00c3b836
Joey <joey.kennedy1@gmail.com>
2024-02-21 18:28:35 -0500

    Adds equipment coverage to argiro (#1002)

--- recipe_scrapers/argiro.py
+++ recipe_scrapers/argiro.py
@@ -1,4 +1,5 @@
 from ._abstract import AbstractScraper
+from ._utils import get_equipment, normalize_string
 
 
 class Argiro(AbstractScraper):
@@ -35,3 +36,10 @@ class Argiro(AbstractScraper):
 
     def description(self):
         return self.schema.description()
+
+    def equipment(self):
+        equipment_items = [
+            normalize_string(e.get_text())
+            for e in self.soup.find_all("div", class_="equipment-title")
+        ]
+        return get_equipment(equipment_items)
