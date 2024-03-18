b4c2a33c41b56723b84d185fd45e18eacca3d464
Joey <joey.kennedy1@gmail.com>
2024-03-18 14:21:39 -0400

    Update test_utils.py

--- tests/library/test_utils.py
+++ tests/library/test_utils.py
@@ -7,15 +7,30 @@ class TestUtilsRefactored(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.iso8601_fixtures = {
-            "PT1H": 60, "PT20M": 20, "PT2H10M": 130, "PT0H9M30S": 10,
+            "PT1H": 60,
+            "PT20M": 20,
+            "PT2H10M": 130,
+            "PT0H9M30S": 10,
         }
         cls.minutes_fixtures = [
-            ("1 hour 15 mins", 75), ("1h and 15mins", 75), ("3h10m", 190),
-            ("PT2H30M", 150), ("P0DT1H10M", 70), ("90", 90),
-            ("1.5 hours", 90), ("2 days", 2880), ("1½ hours", 90),
-            ("1¾ hours", 105), ("1¼ hours", 75), ("1⅔ hours", 100),
-            ("1 1/2 hours", 90), ("1 3/4 hours", 105), ("1 1/4 hours", 75),
-            ("1 2/3 hours", 100), ("15 - 20 minutes", 20), ("15 to 20 minutes", 20),
+            ("1 hour 15 mins", 75),
+            ("1h and 15mins", 75),
+            ("3h10m", 190),
+            ("PT2H30M", 150),
+            ("P0DT1H10M", 70),
+            ("90", 90),
+            ("1.5 hours", 90),
+            ("2 days", 2880),
+            ("1½ hours", 90),
+            ("1¾ hours", 105),
+            ("1¼ hours", 75),
+            ("1⅔ hours", 100),
+            ("1 1/2 hours", 90),
+            ("1 3/4 hours", 105),
+            ("1 1/4 hours", 75),
+            ("1 2/3 hours", 100),
+            ("15 - 20 minutes", 20),
+            ("15 to 20 minutes", 20),
             ("Pá-Pum", None),
         ]
 
@@ -59,8 +74,10 @@ class TestUtilsRefactored(unittest.TestCase):
             "yields",
         ]
         public_methods = [
-            method for method in dir(AbstractScraper)
-            if callable(getattr(AbstractScraper, method)) and not method.startswith("_")
+            method
+            for method in dir(AbstractScraper)
+            if callable(getattr(AbstractScraper, method))
+            and not method.startswith("_")
             and method not in ["soup", "links", "to_json"]
         ]
         self.assertEqual((expected_methods), (public_methods))
