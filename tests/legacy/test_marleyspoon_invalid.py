c24cc7b2b0f8d11fe79c66149e97577b8cbcc6ae
James Addison <james@reciperadar.com>
2024-04-19 17:47:02 +0100

    MarleySpoon: tests: brevity: rename 'valid_url' to 'url'.

--- tests/legacy/test_marleyspoon_invalid.py
+++ tests/legacy/test_marleyspoon_invalid.py
@@ -10,32 +10,32 @@ class TestFaultyAPIURLResponse(unittest.TestCase):
 
     @responses.activate
     def test_faulty_response(self):
-        valid_url = "https://marleyspoon.de/menu/113813-glasierte-veggie-burger-mit-roestkartoffeln-und-apfel-gurken-salat"
+        url = "https://marleyspoon.de/menu/113813-glasierte-veggie-burger-mit-roestkartoffeln-und-apfel-gurken-salat"
         with open("tests/legacy/test_data/faulty.testhtml") as faulty_data:
             faulty_response = faulty_data.read()
 
         responses.add(
             method=responses.GET,
-            url=valid_url,
+            url=url,
             body=faulty_response,
         )
 
         with self.assertRaises(RecipeScrapersExceptions):
-            MarleySpoon(url=valid_url)
+            MarleySpoon(url=url)
 
     @responses.activate
     def test_relative_api_url(self):
-        valid_url = "https://marleyspoon.de/menu/113813-glasierte-veggie-burger-mit-roestkartoffeln-und-apfel-gurken-salat"
+        url = "https://marleyspoon.de/menu/113813-glasierte-veggie-burger-mit-roestkartoffeln-und-apfel-gurken-salat"
         with open("tests/legacy/test_data/relative_url.testhtml") as relative_url_data:
             relative_url_response = relative_url_data.read()
 
         responses.add(
             method=responses.GET,
-            url=valid_url,
+            url=url,
             body=relative_url_response,
         )
 
         with self.assertRaises(Exception):
             MarleySpoon(
-                url=valid_url
+                url=url
             )  # currently this raises an requests.exceptions.MissingSchema exception
