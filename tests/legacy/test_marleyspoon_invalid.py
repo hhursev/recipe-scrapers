ca2154fb53d508bf28e0d036ff91ceff6c45981e
James Addison <james@reciperadar.com>
2024-04-19 17:46:22 +0100

    MarleySpoon: tests: add coverage relative-URL API host case.

--- tests/legacy/test_marleyspoon_invalid.py
+++ tests/legacy/test_marleyspoon_invalid.py
@@ -22,3 +22,20 @@ class TestFaultyAPIURLResponse(unittest.TestCase):
 
         with self.assertRaises(RecipeScrapersExceptions):
             MarleySpoon(url=valid_url)
+
+    @responses.activate
+    def test_relative_api_url(self):
+        valid_url = "https://marleyspoon.de/menu/113813-glasierte-veggie-burger-mit-roestkartoffeln-und-apfel-gurken-salat"
+        with open("tests/legacy/test_data/relative_url.testhtml") as relative_url_data:
+            relative_url_response = relative_url_data.read()
+
+        responses.add(
+            method=responses.GET,
+            url=valid_url,
+            body=relative_url_response,
+        )
+
+        with self.assertRaises(Exception):
+            MarleySpoon(
+                url=valid_url
+            )  # currently this raises an requests.exceptions.MissingSchema exception
