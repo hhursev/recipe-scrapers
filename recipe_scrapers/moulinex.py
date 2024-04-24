9d2c97bb31e969cc771b12e53efdd6365e41ecd6
Antoine Pangas <70067970+SANTASLAYERS@users.noreply.github.com>
2024-04-24 14:26:15 -0400

    Update recipe_scrapers/moulinex.py regex
    Co-authored-by: James Addison <55152140+jayaddison@users.noreply.github.com>

--- recipe_scrapers/moulinex.py
+++ recipe_scrapers/moulinex.py
@@ -28,7 +28,7 @@ class Moulinex(AbstractScraper):
         ingredients = self.schema.ingredients()
         spaced_ingredients = [
             re.sub(
-                r"(\d)(?![ .\d])", r"\1 ", ingredient
+                r"([0-9])([^ .0-9])", r"\1 \2", ingredient
             )  # Insert space if number is not followed by a space, period, or another number
             for ingredient in ingredients
         ]
