91cceaa3f70f729bb54251be431c8ada3ff2afd0
Michael Duffett <duffett.michael@gmail.com>
2024-04-29 11:24:52 +0930

    Improve test README code

--- tests/library/test_readme.py
+++ tests/library/test_readme.py
@@ -1,30 +1,40 @@
 import re
 import unittest
-from typing import List
+from typing import List, Optional
 
 from recipe_scrapers import SCRAPERS
 
-SEPARATOR = "Scrapers available for:"
+START_LIST = "-----------------------"
+END_LIST = "(*) offline saved files only"
+
+
+def parse_list_line(line: str) -> Optional[str]:
+    match = re.search(r"^- `https?://(?:www\.)?([^/\s]+)", line)
+
+    if not match:
+        return None
+
+    return match.group(1)
 
 
 def get_documented_scrapers() -> List[str]:
     documented_scrapers = []
     with open("README.rst") as f:
-        found = False
+        started_list = False
         for line in f:
-            if line.strip() == SEPARATOR:
-                found = True
-                next(f)  # Skip the underline
-                continue
-            if not found or not line.strip():
+            if line.strip() == START_LIST:
+                started_list = True
                 continue
 
-            match = re.search(r"^- `https?://(?:www\.)?([^/\s]+)", line)
-            if match:
-                documented_scrapers.append(match.group(1))
-            else:
-                # Stops reading if no longer within the scraper list section
-                if found and line.startswith('(*) offline saved files only'):
+            if not started_list or not line.strip():
+                continue
+
+            if line.strip() == END_LIST:
+                break
+
+            documented_scraper = parse_list_line(line)
+            if documented_scraper:
+                documented_scrapers.append(documented_scraper)
 
     return documented_scrapers
 
@@ -41,4 +51,6 @@ class TestReadme(unittest.TestCase):
             if domain not in documented_scrapers:
                 missing_scrapers.append(domain)
 
-        self.assertFalse(missing_scrapers, f"Missing scrapers in README.rst: {missing_scrapers}")
+        self.assertFalse(
+            missing_scrapers, f"Missing scrapers in README.rst: {missing_scrapers}"
+        )
