8cc4a0c8995708347e64731c291392c2b5bf09ec
heathrampazis <mistydual@gmail.com>
2024-04-30 14:47:49 +0930

    added code review changes from jayaddison

--- recipe_scrapers/bakels.py
+++ recipe_scrapers/bakels.py
@@ -30,7 +30,7 @@ class Bakels(AbstractScraper):
             for ingredient in ingredients:
                 text = ingredient.get_text(strip=True)
                 if text == "Ingredient" or text.startswith("Group"):
-                    continue
+                    continue  # Ensures these headings are not included as ingredients
                 results.append(text)
         return results
 
@@ -51,23 +51,29 @@ class Bakels(AbstractScraper):
             for ingredient in ingredients:
                 text = ingredient.get_text(strip=True)
                 if text == "Ingredient" or text.startswith("Group"):
-                    continue
+                    continue  # Ensures these headings are not included as ingredients
                 ingredient_group.ingredients.append(text)
             results.append(ingredient_group)
         if len(results) == 1:
-            results[0].purpose = None
+            results[0].purpose = (
+                None  # If there is only 1 group, remove the group title
+            )
         return results
 
     def instructions(self):
         div = self.soup.find("div", id="tab-method_1")
-        if div:
-            tag = div.find("p")
-            if tag:
-                instructions = tag.get_text(separator="\n", strip=True)
-                instructions = "\n".join(
-                    step.split(". ", 1)[-1] for step in instructions.split("\n")
-                )  # Removes the instruction number from each step
-                return instructions
+        if not div:
+            return None
+
+        tag = div.find("p")
+        if not tag:
+            return None
+
+        instructions = tag.get_text(separator="\n", strip=True)
+        instructions = "\n".join(
+            step.split(". ", 1)[-1] for step in instructions.split("\n")
+        )  # Removes the instruction number from each step
+        return instructions
 
     def description(self):
         description_meta = self.soup.find("meta", {"property": "og:title"})
@@ -77,5 +83,11 @@ class Bakels(AbstractScraper):
 
     def category(self):
         category_h4 = self.soup.find("h4", string="Category")
+        if not category_h4:
+            return None
+
         category_p = category_h4.find_next_sibling("p")
+        if not category_p:
+            return None
+
         return category_p.text
