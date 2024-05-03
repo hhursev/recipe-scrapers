6ea883a295b0257bb5425a60e7ddb32d3c6b7255
James Addison <james@reciperadar.com>
2024-05-03 21:53:19 +0100

    kuchniadomowa: use standard schema.org-based ingredient retrieval.

--- recipe_scrapers/kuchniadomowa.py
+++ recipe_scrapers/kuchniadomowa.py
@@ -14,10 +14,6 @@ class KuchniaDomowa(AbstractScraper):
         urls = self.soup.findAll("img", {"class": "article-img", "id": "article-img-1"})
         return f"https:{urls[1]['src']}"
 
-    def ingredients(self):
-        # TODO: add implementation
-        raise NotImplementedError("This should be implemented.")
-
     def instructions(self):
         instructions = self.soup.find("div", {"id": "recipe-instructions"}).findAll(
             "li"
