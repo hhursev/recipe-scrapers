d12b9f050f464b30f4ea38b3bf5dbbb320c2421d
James Addison <james@reciperadar.com>
2024-05-07 22:55:16 +0100

    Tests: use py3.8-compatible string-prefix-removal implementation.
    Co-authored-by: Joey <7505194+jknndy@users.noreply.github.com>

--- tests/library/test_readme.py
+++ tests/library/test_readme.py
@@ -38,7 +38,7 @@ def get_scraper_index() -> ScraperIndex:
         scraper_index[primary_domain] = (
             scraper,
             [
-                domain.removeprefix(shared_prefix)
+                domain[len(shared_prefix):] if domain.startswith(shared_prefix) else domain
                 for domain in domains
                 if domain != shared_prefix
             ],
