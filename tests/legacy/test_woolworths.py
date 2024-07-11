453b5c9dacc9048397339e944e020351616588e8
James Addison <james@reciperadar.com>
2024-07-11 14:08:41 +0100

    Reapply "Language: consistency: retrieve dynamically from HTML where possible. (#1112)"
    This reverts commit d12320fe36fd63826016e3990f3b0493b79043b7.

--- tests/legacy/test_woolworths.py
+++ tests/legacy/test_woolworths.py
@@ -9,15 +9,15 @@ class TestWoolworthsScraper(ScraperTest):
 
     @classmethod
     def expected_requests(cls):
-        yield GET, "https://www.woolworths.com.au/shop/recipes/asparagus-salad-with-lemon-vinaigrette", "tests/legacy/test_data/woolworths.testhtml"
-        yield GET, "https://foodhub.woolworths.com.au/content/woolworths-foodhub/en/asparagus-salad-with-lemon-vinaigrette.model.json", "tests/legacy/test_data/woolworths.testhtml"
+        yield GET, "https://www.woolworths.com.au/shop/recipes/smoothie-fruit-cubes", "tests/legacy/test_data/woolworths.testhtml"
+        yield GET, "https://foodhub.woolworths.com.au/content/woolworths-foodhub/en/smoothie-fruit-cubes.model.json", "tests/legacy/test_data/woolworths.testjson"
 
     def test_host(self):
         self.assertEqual("woolworths.com.au", self.harvester_class.host())
 
     def test_canonical_url(self):
         self.assertEqual(
-            "https://www.woolworths.com.au/shop/recipes/asparagus-salad-with-lemon-vinaigrette",
+            "https://www.woolworths.com.au/shop/recipes/smoothie-fruit-cubes",
             self.harvester_class.canonical_url(),
         )
 
@@ -25,11 +25,11 @@ class TestWoolworthsScraper(ScraperTest):
         self.assertEqual("Fresh Ideas", self.harvester_class.author())
 
     def test_category(self):
-        self.assertEqual("Salads", self.harvester_class.category())
+        self.assertEqual("Drinks", self.harvester_class.category())
 
     def test_title(self):
         self.assertEqual(
-            "Asparagus Salad With Lemon Vinaigrette Recipe | Woolworths",
+            "Smoothie Fruit Cubes Recipe | Woolworths",
             self.harvester_class.title(),
         )
 
@@ -37,52 +37,54 @@ class TestWoolworthsScraper(ScraperTest):
         self.assertEqual(15, self.harvester_class.total_time())
 
     def test_prep_time(self):
-        self.assertEqual(10, self.harvester_class.prep_time())
+        self.assertEqual(15, self.harvester_class.prep_time())
 
     def test_yields(self):
-        self.assertEqual("4 servings", self.harvester_class.yields())
+        self.assertEqual("12 servings", self.harvester_class.yields())
 
     def test_image(self):
         self.assertEqual(
-            "https://foodhub.scene7.com/is/image/woolworthsltdprod/2010-asparagus-salad-with-lemon-vinaigrette:Square-1300x1300",
+            "https://foodhub.scene7.com/is/image/woolworthsltdprod/fi-2401-smoothie-fruit-cubes:Square-1300x1300",
             self.harvester_class.image(),
         )
 
     def test_ingredients(self):
         self.assertEqual(
             [
-                "2 bunches asparagus, trimmed, halved diagonally",
-                "100g snow peas, trimmed, halved diagonally",
-                "2 gem lettuce, trimmed, leaves separated",
-                "2 radishes, trimmed, thinly sliced into rounds",
-                "80g feta, crumbled",
-                "0.5 cup small basil leaves",
-                "2 tbs natural sliced almonds, toasted",
-                "1 tbs white wine vinegar",
-                "2 tsp Dijon mustard",
-                "0.25 tsp sea salt flakes",
-                "0.25 tsp caster sugar",
-                "0.25 cup extra virgin olive oil",
+                "300g fruit of choice (see tip)",
             ],
             self.harvester_class.ingredients(),
         )
 
     def test_instructions(self):
         self.assertEqual(
-            """Bring a medium saucepan of water to the boil over high heat. Cook asparagus and snow peas for 2 minutes or until just tender. Drain. Refresh under cold water. Drain.
-Meanwhile, to make the vinaigrette, whisk all the ingredients together in a small jug until combined, then season.
-Arrange lettuce, asparagus, snow peas and radish on a platter. Scatter over feta, drizzle with dressing, top with basil and almonds, then serve.""",
+            (
+                "Place fruit in a blender and blitz until smooth. Transfer to a small jug.\n"
+                "Pour mixture equally into a 12-hole (1 tbs-/20ml-capacity) ice-cube tray and freeze for 6 hours or overnight until set.\n"
+                "Transfer cubes to an airtight container and store in the freezer until required. (Once ready to use, place cubes in a blender with yoghurt and milk, then blitz until smooth.)"
+            ),
             self.harvester_class.instructions(),
         )
 
     def test_cuisine(self):
-        self.assertEqual("French", self.harvester_class.cuisine())
+        self.assertEqual("Australian", self.harvester_class.cuisine())
 
     def test_cook_time(self):
-        self.assertEqual(5, self.harvester_class.cook_time())
+        self.assertEqual(None, self.harvester_class.cook_time())
 
     def test_nutrients(self):
-        self.assertEqual({}, self.harvester_class.nutrients())
+        self.assertEqual(
+            {
+                "calories": "6 calories",
+                "carbohydrateContent": "1.1 grams",
+                "fatContent": "0.1 grams",
+                "fiberContent": "0.2 grams",
+                "proteinContent": "0.2 grams",
+                "sodiumContent": "10 milligrams",
+                "sugarContent": "1.1 grams",
+            },
+            self.harvester_class.nutrients(),
+        )
 
     def test_language(self):
         self.assertEqual("en-AU", self.harvester_class.language())
