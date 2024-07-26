301c9385895a4834f9ffff25559a73bc71e4ae96
James Addison <55152140+jayaddison@users.noreply.github.com>
2024-07-26 12:22:55 +0100

    v14: make `wild_mode` opt-in (as usual) when using `scrape_html` interface. (#1187)

--- tests/library/test_exceptions.py
+++ tests/library/test_exceptions.py
@@ -3,14 +3,19 @@ import unittest
 from recipe_scrapers import (
     NoSchemaFoundInWildMode,
     WebsiteNotImplementedError,
+    scrape_html,
     scrape_me,
 )
 
 
 class TestExceptions(unittest.TestCase):
-    def test_website_not_implemented_error(self):
+    def test_website_not_implemented_error_legacy(self):
         with self.assertRaises(WebsiteNotImplementedError):
             scrape_me("https://example.com/recipe")
+
+    def test_website_not_implemented_error(self):
+        with self.assertRaises(WebsiteNotImplementedError):
+            scrape_html(html="<html></html>", org_url="http://example.com/recipe")
 
     def test_no_schema_found_in_wild_mode_error(self):
         exception = NoSchemaFoundInWildMode("example.com")
