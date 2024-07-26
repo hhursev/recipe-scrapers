3e567598e01c041fa6025fdbdc56f9340cbedc26
James Addison <james@reciperadar.com>
2024-07-26 12:12:42 +0100

    Refactor: extract `_emit_options_deprecation_notice` internal helper method.

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -752,21 +752,25 @@ def scraper_exists_for(url_path: str) -> bool:
     return host_name in get_supported_urls()
 
 
+def _emit_options_deprecation_notice():
+    msg = (
+        "Scraper options arguments (e.g. proxies=, timeout=) are deprecated, and "
+        "support for them will be dropped in future.  To migrate, please:\n"
+        "\n"
+        " * Use an HTTP client (such as 'requests' or 'httpx') configured with "
+        "the proxies/timeout settings you want.\n"
+        " * Retrieve recipe HTML using the appropriately-configured HTTP client.\n"
+        " * Scrape retrieved recipe HTML using the 'recipe_scrapers.scrape_html' "
+        "function.\n"
+    )
+    warnings.warn(msg, DeprecationWarning)
+
+
 def scrape_me(url_path: str, **options: Any) -> AbstractScraper:
     host_name = get_host_name(url_path)
 
     if options:
-        msg = (
-            "Scraper options arguments (e.g. proxies=, timeout=) are deprecated, and "
-            "support for them will be dropped in future.  To migrate, please:\n"
-            "\n"
-            " * Use an HTTP client (such as 'requests' or 'httpx') configured with "
-            "the proxies/timeout settings you want.\n"
-            " * Retrieve recipe HTML using the appropriately-configured HTTP client.\n"
-            " * Scrape retrieved recipe HTML using the 'recipe_scrapers.scrape_html' "
-            "function.\n"
-        )
-        warnings.warn(msg, DeprecationWarning)
+        _emit_options_deprecation_notice()
 
     try:
         scraper = SCRAPERS[host_name]
@@ -806,15 +810,7 @@ def scrape_html(
     host_name = get_host_name(org_url) if org_url is not None else None
 
     if options:
-        msg = (
-            "Scraper options arguments (e.g. proxies=, timeout=) are deprecated, and "
-            "support for them will be dropped in future.  To migrate, please:\n"
-            "\n"
-            " * Use an HTTP client (such as 'requests' or 'httpx') configured with "
-            "the proxies/timeout settings you want.\n"
-            " * Retrieve recipe HTML using the appropriately-configured HTTP client.\n"
-        )
-        warnings.warn(msg, DeprecationWarning)
+        _emit_options_deprecation_notice()
 
     scraper = None
     if host_name:
