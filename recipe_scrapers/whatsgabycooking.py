6de1e09b0417953bc03ef518e0f2dd046ae463d8
Joey <joey.kennedy1@gmail.com>
2024-08-01 19:47:41 -0400

    groups and equipment

--- recipe_scrapers/whatsgabycooking.py
+++ recipe_scrapers/whatsgabycooking.py
@@ -1,7 +1,24 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
+from ._utils import get_equipment, normalize_string
 
 
 class WhatsGabyCooking(AbstractScraper):
     @classmethod
     def host(cls):
         return "whatsgabycooking.com"
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".wprm-recipe-ingredient-group h4",
+            ".wprm-recipe-ingredient",
+        )
+
+    def equipment(self):
+        equipment_items = [
+            normalize_string(e.get_text())
+            for e in self.soup.find_all("div", class_="wprm-recipe-equipment-name")
+        ]
+        return sorted(get_equipment(equipment_items))
