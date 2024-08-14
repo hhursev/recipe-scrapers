88dbb7851bdadfa095e90aaa26f6163b340430ac
Joey <joey.kennedy1@gmail.com>
2024-08-14 17:06:06 -0400

    ingoodflavor

--- recipe_scrapers/ingoodflavor.py
+++ recipe_scrapers/ingoodflavor.py
@@ -14,3 +14,14 @@ class InGoodFlavor(AbstractScraper):
             ".dr-title",
             ".dr-ingredient-name",
         )
+
+    def author(self):
+        signature_div = self.soup.find("div", class_="text-signature")
+        signature_text = signature_div.get_text(strip=True) if signature_div else ""
+
+        schema_author = self.schema.author()
+
+        if signature_text:
+            return f"{signature_text} | {schema_author}"
+
+        return schema_author
