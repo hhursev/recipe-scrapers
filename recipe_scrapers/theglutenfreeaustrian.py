c3290f6310e463351745ce03735f4df14548f992
Joey <joey.kennedy1@gmail.com>
2024-08-16 13:02:36 -0400

    ingredient_grouping improvements

--- recipe_scrapers/theglutenfreeaustrian.py
+++ recipe_scrapers/theglutenfreeaustrian.py
@@ -8,9 +8,17 @@ class TheGlutenFreeAustrian(AbstractScraper):
         return "theglutenfreeaustrian.com"
 
     def ingredient_groups(self):
-        return group_ingredients(
+        ingredient_groups = group_ingredients(
             self.ingredients(),
             self.soup,
             ".mv-create-ingredients h4",
             ".mv-create-ingredients ul li",
         )
+
+        schema_title = self.schema.title()
+
+        for group in ingredient_groups:
+            if group.purpose and schema_title in group.purpose:
+                group.purpose = None
+
+        return ingredient_groups
