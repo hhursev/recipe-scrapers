c1614504288f3d3415c1d9b403d7c85a22199bf1
Joey <7505194+jknndy@users.noreply.github.com>
2024-09-02 18:16:15 -0400

    Update projectgezond.py

--- recipe_scrapers/projectgezond.py
+++ recipe_scrapers/projectgezond.py
@@ -16,15 +16,17 @@ class ProjectGezond(AbstractScraper):
     def cuisine(self):
         # Not listed on site
         return None
-    
+
     def category(self):
-        category_script = self.soup.find("script", text=lambda text: text and "var dataLayer_content =" in text)
+        category_script = self.soup.find(
+            "script", text=lambda text: text and "var dataLayer_content =" in text
+        )
         if category_script:
             category_text = category_script.string
             start = category_text.find('"pageCategory":[')
             if start != -1:
                 start += len('"pageCategory":[')
-                end = category_text.find(']', start)
+                end = category_text.find("]", start)
                 if end != -1:
                     categories = category_text[start:end].strip('"').split('","')
                     return categories if categories else None
