3e1e137cc6ef9fbe58801048975e213093fd708a
jaspervzwi <jasperzwieten@gmail.com>
2024-09-12 15:58:45 +0200

    Add test fields miljuschka.nl

--- recipe_scrapers/miljuschka.py
+++ recipe_scrapers/miljuschka.py
@@ -1,4 +1,5 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 
 
 class Miljuschka(AbstractScraper):
@@ -38,3 +39,11 @@ class Miljuschka(AbstractScraper):
 
     def description(self):
         return self.schema.description()
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".wprm-recipe-group-name",
+            ".wprm-recipe-ingredient",
+        )
