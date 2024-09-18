7f11101be8d4452a30ee7db822f496dfd1cbbb0c
jaspervzwi <jasperzwieten@gmail.com>
2024-09-18 15:24:54 +0200

    Fix init duplicate classname

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -498,7 +498,6 @@ SCRAPERS = {
     LeitesCulinaria.host(): LeitesCulinaria,
     McCormick.host(): McCormick,
     Miljuschka.host(): Miljuschka,
-    Miljuschka.host(): Miljuschka,
     ModernHoney.host(): ModernHoney,
     MomOnTimeout.host(): MomOnTimeout,
     Moulinex.host(): Moulinex,
