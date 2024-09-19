666b9372f55adbff0e9932535af24e4683116d4b
Joey <7505194+jknndy@users.noreply.github.com>
2024-09-19 18:30:51 -0400

    update abeautifulmess

--- recipe_scrapers/abeautifulmess.py
+++ recipe_scrapers/abeautifulmess.py
@@ -18,12 +18,8 @@ class ABeautifulMess(AbstractScraper):
 
     def equipment(self):
         equipment_container = self.soup.select_one(".wprm-recipe-equipment-container")
-        if not equipment_container:
-            return []
-
-        equipment_items = [
-            item.select_one(".wprm-recipe-equipment-name").get_text(strip=True)
-            for item in equipment_container.select(".wprm-recipe-equipment-item")
-            if item.select_one(".wprm-recipe-equipment-name")
-        ]
-        return get_equipment(equipment_items)
+        equipment = []
+        if equipment_container:
+            equipment_items = [item.get_text() for item in equipment_container.select(".wprm-recipe-equipment-name")]
+            equipment = get_equipment(equipment_items)
+        return equipment
