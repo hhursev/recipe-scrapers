acfb0f5e9d03c9bae4a6dd29628d99419fa4bde8
Joey <7505194+jknndy@users.noreply.github.com>
2024-10-02 17:57:44 -0400

    improve nutrients output

--- recipe_scrapers/barefootinthepines.py
+++ recipe_scrapers/barefootinthepines.py
@@ -16,16 +16,41 @@ class BarefootInThePines(AbstractScraper):
         )
 
     def nutrients(self):
-        nutrition_container = self.soup.select_one(".mv-create-nutrition-box")
-        if not nutrition_container:
+        nutrition_section = self.soup.select_one(".mv-create-nutrition-box")
+        if not nutrition_section:
             return None
 
-        nutrition_data = {}
-        for item in nutrition_container.select(".mv-create-nutrition-item"):
-            label = item.select_one(".mv-create-nutrition-label")
-            if label:
-                label_text = label.get_text(strip=True)
-                quantity = item.get_text(strip=True).replace(label_text, "").strip()
-                nutrition_data[label_text] = quantity
+        raw_nutrition_data = {
+            item.select_one(".mv-create-nutrition-label")
+            .get_text(strip=True)
+            .lower(): item.get_text(strip=True)
+            .replace(
+                item.select_one(".mv-create-nutrition-label").get_text(strip=True), ""
+            )
+            .strip()
+            for item in nutrition_section.select(".mv-create-nutrition-item")
+            if item.select_one(".mv-create-nutrition-label")
+        }
 
-        return nutrition_data
+        nutrition_label_mapping = {
+            "calories:": "calories",
+            "carbohydrates:": "carbohydrateContent",
+            "cholesterol:": "cholesterolContent",
+            "total fat:": "fatContent",
+            "fiber:": "fiberContent",
+            "protein:": "proteinContent",
+            "saturated fat:": "saturatedFatContent",
+            "serving size:": "servingSize",
+            "sodium:": "sodiumContent",
+            "sugar:": "sugarContent",
+            "trans fat:": "transFatContent",
+            "unsaturated fat:": "unsaturatedFatContent",
+        }
+
+        standardized_nutrition_data = {
+            schema_label: raw_nutrition_data[custom_label.lower()]
+            for custom_label, schema_label in nutrition_label_mapping.items()
+            if custom_label.lower() in raw_nutrition_data
+        }
+
+        return standardized_nutrition_data
