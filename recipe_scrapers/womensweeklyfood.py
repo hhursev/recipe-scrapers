25a78d158d1696abfae37f01a7e4775d47acbe05
Joey <7505194+jknndy@users.noreply.github.com>
2024-10-13 19:34:29 -0400

    custom ingredient implementation & groupings

--- recipe_scrapers/womensweeklyfood.py
+++ recipe_scrapers/womensweeklyfood.py
@@ -1,6 +1,7 @@
 import re
 
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 
 
 class WomensWeeklyFood(AbstractScraper):
@@ -10,3 +11,17 @@ class WomensWeeklyFood(AbstractScraper):
 
     def instructions(self):
         return re.sub(r"\d+\.\n", "", self.schema.instructions())
+
+    def ingredients(self):
+        ingredients_elements = self.soup.select(
+            ".recipe-ingredients__item span[itemprop='ingredients']"
+        )
+        return [ingredient.get_text(strip=True) for ingredient in ingredients_elements]
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".recipe-ingredients__title",
+            ".recipe-ingredients__item span[itemprop='ingredients']",
+        )
