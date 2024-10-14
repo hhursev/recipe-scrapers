4bba6a64e67565b17fd4473af3aaa4dac872dd61
Joey <joey.kennedy1@gmail.com>
2024-10-14 12:59:07 -0400

    grouping and equip

--- recipe_scrapers/thekitchenmagpie.py
+++ recipe_scrapers/thekitchenmagpie.py
@@ -1,7 +1,29 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 
 
 class TheKitchenMagPie(AbstractScraper):
     @classmethod
     def host(cls):
         return "thekitchenmagpie.com"
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".wprm-recipe-ingredient-group h4",
+            ".wprm-recipe-ingredient",
+        )
+
+    def equipment(self):
+        equipment_container = self.soup.select_one(".wprm-recipe-details-container")
+        if equipment_container:
+            equipment_text = equipment_container.find("dt", string="Equipment")
+            if equipment_text:
+                equipment_list = equipment_text.find_next_sibling("dd")
+                if equipment_list:
+                    return [
+                        item.strip()
+                        for item in equipment_list.get_text(strip=True).split(",")
+                    ]
+        return None
