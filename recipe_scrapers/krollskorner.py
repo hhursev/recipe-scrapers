0366026901e9af23bfd705ac5e52d28892e3984b
Joey <7505194+jknndy@users.noreply.github.com>
2024-10-14 11:56:51 -0400

    Update recipe_scrapers/krollskorner.py
    Co-authored-by: James Addison <55152140+jayaddison@users.noreply.github.com>

--- recipe_scrapers/krollskorner.py
+++ recipe_scrapers/krollskorner.py
@@ -25,7 +25,7 @@ class KrollsKorner(AbstractScraper):
     def equipment(self):
         equipment_container = self.soup.select_one(".wprm-recipe-equipment-container")
         if not equipment_container:
-            return []
+            return None
 
         equipment_items = [
             item.select_one(".wprm-recipe-equipment-name").get_text(strip=True)
