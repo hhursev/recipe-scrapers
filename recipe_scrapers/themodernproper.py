11cd2c9e43715ba0649fcfd9e29b5f5694f55f68
Joey <7505194+jknndy@users.noreply.github.com>
2024-10-17 18:06:30 -0400

    cleanup nutrient output

--- recipe_scrapers/themodernproper.py
+++ recipe_scrapers/themodernproper.py
@@ -1,7 +1,33 @@
 from ._abstract import AbstractScraper
+from ._exceptions import ElementNotFoundInHtml
 
 
 class TheModernProper(AbstractScraper):
     @classmethod
     def host(cls):
         return "themodernproper.com"
+
+    def nutrients(self):
+        container = self.schema.nutrients()
+        if not container:
+            raise ElementNotFoundInHtml("Could not find nutritional info container")
+
+        results = {}
+        nutrient_mapping = {
+            "calories": "calories",
+            "fatContent": "fat",
+            "saturatedFatContent": "saturated fat",
+            "carbohydrateContent": "carbohydrates",
+            "sugarContent": "sugar",
+            "proteinContent": "protein",
+            "sodiumContent": "sodium",
+            "fiberContent": "fiber",
+            "cholesterolContent": "cholesterol",
+        }
+
+        for key, value in container.items():
+            if key in nutrient_mapping:
+                content = value.replace(nutrient_mapping[key], "").strip()
+                results[key] = content
+
+        return results
