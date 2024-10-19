dbf8d88c774df2a62d515d6df8c676f67a4bd7db
Joey <7505194+jknndy@users.noreply.github.com>
2024-10-18 22:11:50 -0400

    groups

--- recipe_scrapers/sallysblog.py
+++ recipe_scrapers/sallysblog.py
@@ -1,4 +1,5 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import IngredientGroup, group_ingredients
 
 
 class SallysBlog(AbstractScraper):
@@ -41,3 +42,18 @@ class SallysBlog(AbstractScraper):
 
     def author(self):
         return "Sally's Blog"
+
+    def ingredient_groups(self):
+        groups = group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".shop-studio-recipes-recipe-detail-tabs-description-ingredients__content__ingredient-list__title",
+            ".shop-studio-recipes-recipe-detail-tabs-description-ingredients__content__ingredient-list__ingredient",
+        )
+
+        schema_title = self.schema.title()
+
+        if len(groups) == 1 and schema_title.lower() == groups[0].purpose.lower():
+            return [IngredientGroup(ingredients=self.ingredients(), purpose=None)]
+
+        return groups
