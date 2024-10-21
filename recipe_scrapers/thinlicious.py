dabe122a1fb2ba519b627f097fab28593fbb33f1
Joey <7505194+jknndy@users.noreply.github.com>
2024-10-21 16:22:08 -0700

    Update thinlicious.py
    Co-authored-by: James Addison <55152140+jayaddison@users.noreply.github.com>

--- recipe_scrapers/thinlicious.py
+++ recipe_scrapers/thinlicious.py
@@ -17,4 +17,4 @@ class Thinlicious(AbstractScraper):
 
     def equipment(self):
         equipment_list = self.soup.select(".wprm-recipe-equipment-name")
-        return [equipment.get_text() for equipment in equipment_list]
+        return get_equipment(item.get_text() for item in equipment_list)
