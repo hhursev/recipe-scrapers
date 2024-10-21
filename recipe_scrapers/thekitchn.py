78060661787ada11387b2ca4f64840b2bd3640e9
James Addison <james@reciperadar.com>
2024-10-21 23:39:10 +0100

    thekitchn: include definite article (`The`) in `site_name` result

--- recipe_scrapers/thekitchn.py
+++ recipe_scrapers/thekitchn.py
@@ -1,7 +1,11 @@
 from ._abstract import AbstractScraper
+from ._exceptions import StaticValueException
 
 
 class TheKitchn(AbstractScraper):
     @classmethod
     def host(cls):
         return "thekitchn.com"
+
+    def site_name(self):
+        raise StaticValueException(return_value="The Kitchn")
