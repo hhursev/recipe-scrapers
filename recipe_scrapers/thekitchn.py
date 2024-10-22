cfad7d5e4c7f3339fd312ef5664f8200b8ff9d78
Joey <7505194+jknndy@users.noreply.github.com>
2024-10-21 18:29:31 -0700

    Ingredient grouping support for thekitchn (#1297)

--- recipe_scrapers/thekitchn.py
+++ recipe_scrapers/thekitchn.py
@@ -1,11 +1,20 @@
 from ._abstract import AbstractScraper
 from ._exceptions import StaticValueException
+from ._grouping_utils import group_ingredients
 
 
 class TheKitchn(AbstractScraper):
     @classmethod
     def host(cls):
         return "thekitchn.com"
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".Recipe__ingredientsGroupName",
+            ".Recipe__ingredient",
+        )
 
     def site_name(self):
         raise StaticValueException(return_value="The Kitchn")
