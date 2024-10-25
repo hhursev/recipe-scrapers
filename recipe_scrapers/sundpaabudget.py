54df216ee63cfd22750898ca363fcb4aef0d2763
James Addison <james@reciperadar.com>
2024-10-25 12:52:39 +0100

    sundpaabudget: remove support for nutritional information

--- recipe_scrapers/sundpaabudget.py
+++ recipe_scrapers/sundpaabudget.py
@@ -9,3 +9,9 @@ class SundPaaBudget(AbstractScraper):
     def description(self):
         # Schema returns empty string
         return self.soup.head.find("meta", {"property": "og:description"})["content"]
+
+    def nutrients(self):
+        # Some schema.org nutrition info exists in this site's recipe webpages,
+        # but the content seems unreliable
+        # Ref: https://github.com/hhursev/recipe-scrapers/issues/1346
+        return None
