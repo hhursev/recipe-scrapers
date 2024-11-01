5618d7adf1b17ca1a08ad6e95fb5955f72ea2e09
ReneNulschDE <ReneNulschDE.github@Nulsch.de>
2024-11-01 20:05:23 +0100

    Adjust author method

--- recipe_scrapers/aldinord.py
+++ recipe_scrapers/aldinord.py
@@ -1,12 +1,16 @@
 from ._abstract import AbstractScraper
 from ._exceptions import StaticValueException
 
+
 class AldiNord(AbstractScraper):
     @classmethod
     def host(cls, domain: str = "aldi-nord.de"):
         return domain
 
     def author(self):
+        if author_from_schema := self.schema.author():
+            return author_from_schema
+
         raise StaticValueException(return_value="ALDI")
 
     def site_name(self):
