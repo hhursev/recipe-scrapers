de3befb545b06676efd473b90a7fc80ba79829bd
Joey <7505194+jknndy@users.noreply.github.com>
2024-11-03 12:05:00 -0800

    Ingredient grouping support for sundpaabudget (#1329)

--- recipe_scrapers/sundpaabudget.py
+++ recipe_scrapers/sundpaabudget.py
@@ -1,4 +1,5 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 
 
 class SundPaaBudget(AbstractScraper):
@@ -9,6 +10,14 @@ class SundPaaBudget(AbstractScraper):
     def description(self):
         # Schema returns empty string
         return self.soup.head.find("meta", {"property": "og:description"})["content"]
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".wprm-recipe-ingredient-group h4",
+            ".wprm-recipe-ingredient",
+        )
 
     def nutrients(self):
         # Some schema.org nutrition info exists in this site's recipe webpages,
