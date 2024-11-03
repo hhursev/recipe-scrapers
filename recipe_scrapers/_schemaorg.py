a025706a4d09987c9ecc32db5580d614bac6c89a
Joey <joey.kennedy1@gmail.com>
2024-11-03 13:43:12 -0500

    Update _schemaorg.py

--- recipe_scrapers/_schemaorg.py
+++ recipe_scrapers/_schemaorg.py
@@ -262,7 +262,11 @@ class SchemaOrg:
         return instructions_gist
 
     def instructions(self):
-        instructions = self.data.get("recipeInstructions") or ""
+        instructions = (
+            self.data.get("recipeInstructions")
+            or self.data.get("RecipeInstructions")
+            or ""
+        )
 
         if (
             instructions
