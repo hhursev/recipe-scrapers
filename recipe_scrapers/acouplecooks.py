fa11404faab4d1c731482f724a4f70ab8d0d2467
Joey <7505194+jknndy@users.noreply.github.com>
2024-12-01 17:22:44 -0500

    grouping

--- recipe_scrapers/acouplecooks.py
+++ recipe_scrapers/acouplecooks.py
@@ -1,7 +1,16 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 
 
 class ACoupleCooks(AbstractScraper):
     @classmethod
     def host(cls):
         return "acouplecooks.com"
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".tasty-recipes-ingredients-body p b",
+            ".tasty-recipes-ingredients-body ul li",
+        )
