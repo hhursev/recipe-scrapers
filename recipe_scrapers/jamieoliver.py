5fc6dedd8eb52bf14a9f65636f225c8927bc9294
Joey <7505194+jknndy@users.noreply.github.com>
2024-12-14 14:54:37 -0500

    Groupings and Improved Ingredient Parsing for JamieOliver (#1434)

--- recipe_scrapers/jamieoliver.py
+++ recipe_scrapers/jamieoliver.py
@@ -1,4 +1,5 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 from ._utils import normalize_string
 
 
@@ -12,3 +13,17 @@ class JamieOliver(AbstractScraper):
         instructions_list = method_heading.find_next("ol")
         instructions = instructions_list.find_all("li")
         return "\n".join([normalize_string(inst.get_text()) for inst in instructions])
+
+    def ingredients(self):
+        ingredients_list = self.soup.select(".ingredients-rich-text p.type-body")
+        return [
+            normalize_string(ingredient.get_text()) for ingredient in ingredients_list
+        ]
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".ingredients-rich-text p.type-h5",
+            ".ingredients-rich-text p.type-body",
+        )
