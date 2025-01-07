11218bc33a716e72a7bf4433a05d228721d01cfe
Eric <25854764+eric-hoffmann@users.noreply.github.com>
2025-01-06 19:21:58 -0800

    add better nutrition parsing for cookwell.com

--- recipe_scrapers/cookwell.py
+++ recipe_scrapers/cookwell.py
@@ -1,7 +1,25 @@
 from ._abstract import AbstractScraper
+import re
 
 
 class CookWell(AbstractScraper):
     @classmethod
     def host(cls):
         return "cookwell.com"
+    
+    def nutrients(self):
+        nutrition = None
+        nutrition_pattern = re.compile(r'{\\\"calories\\\":(?P<calories>\d+),\\\"carbohydrates\\\":(?P<carbohydrates>\d+),\\\"fat\\\":(?P<fat>\d+),\\\"protein\\\":(?P<protein>\d+)}')
+        
+        nutrition_script = self.soup.find("script",string=nutrition_pattern)
+        if nutrition_script:
+            nutrition_info = nutrition_pattern.search(nutrition_script.string).groupdict()
+            if nutrition_info:
+                nutrition = {
+                    "calories": f'{nutrition_info["calories"]} calories',
+                    "carbohydrateContent": f'{nutrition_info["carbohydrates"]} g',
+                    "fatContent": f'{nutrition_info["fat"]} g',
+                    "proteinContent": f'{nutrition_info["protein"]} g'
+                }
+        
+        return nutrition if nutrition else super().nutrients()
