08a1cebf194f607c31cb8fb2ff4874b48ca02c15
Eric Hoffmann <25854764+eric-hoffmann@users.noreply.github.com>
2025-01-07 21:24:48 -0800

    Apply suggestions from code review
    Co-authored-by: Joey <7505194+jknndy@users.noreply.github.com>

--- recipe_scrapers/cookwell.py
+++ recipe_scrapers/cookwell.py
@@ -10,20 +10,19 @@ class CookWell(AbstractScraper):
     def nutrients(self):
         nutrition = None
         nutrition_pattern = re.compile(
-            r"{\\\"calories\\\":(?P<calories>\d+),\\\"carbohydrates\\\":(?P<carbohydrates>\d+),\\\"fat\\\":(?P<fat>\d+),\\\"protein\\\":(?P<protein>\d+)}"
+            r'{\\"calories\\":(?P<calories>\d+),\\"carbohydrates\\":(?P<carbohydrates>\d+),\\"fat\\":(?P<fat>\d+),\\"protein\\":(?P<protein>\d+)}'
         )
 
         nutrition_script = self.soup.find("script", string=nutrition_pattern)
         if nutrition_script:
-            nutrition_info = nutrition_pattern.search(
-                nutrition_script.string
-            ).groupdict()
-            if nutrition_info:
-                nutrition = {
-                    "calories": f'{nutrition_info["calories"]} calories',
+            match = nutrition_pattern.search(nutrition_script.string)
+            if match:
+                nutrition_info = match.groupdict()
+                return {
+                    "calories": nutrition_info["calories"],
                     "carbohydrateContent": f'{nutrition_info["carbohydrates"]} g',
                     "fatContent": f'{nutrition_info["fat"]} g',
                     "proteinContent": f'{nutrition_info["protein"]} g',
                 }
 
-        return nutrition if nutrition else super().nutrients()
+        return self.schema.nutrients()
