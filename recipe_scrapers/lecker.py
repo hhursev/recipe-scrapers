18d267f3fc6b78e3701d1cc40f0206fa2b8a5c06
Joey <7505194+jknndy@users.noreply.github.com>
2025-01-21 18:05:23 -0500

    Custom Instructions handling

--- recipe_scrapers/lecker.py
+++ recipe_scrapers/lecker.py
@@ -1,7 +1,17 @@
 from ._abstract import AbstractScraper
+from ._utils import normalize_string
 
 
 class Lecker(AbstractScraper):
     @classmethod
     def host(cls):
         return "lecker.de"
+
+    def instructions(self):
+        instruction_steps = self.soup.select(
+            ".bx-instruction-group__instruction-step + div p"
+        )
+        normalized_instructions = [
+            normalize_string(step.get_text()) for step in instruction_steps
+        ]
+        return "\n".join(normalized_instructions)
