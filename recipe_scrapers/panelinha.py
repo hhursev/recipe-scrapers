9f9c61ce0c494c6612f69f1f228ea3fb22252680
Joey <7505194+jknndy@users.noreply.github.com>
2025-02-02 15:07:59 -0800

    html update (#1467)

--- recipe_scrapers/panelinha.py
+++ recipe_scrapers/panelinha.py
@@ -1,50 +1,13 @@
 import re
 
 from ._abstract import AbstractScraper
-from ._utils import get_minutes, get_yields, normalize_string
-
-INSTRUCTIONS_NUMBERING_REGEX = re.compile(r"^\d{1,2}\.\s*")  # noqa
+from ._utils import get_minutes, get_yields
 
 
 class Panelinha(AbstractScraper):
     @classmethod
     def host(cls):
         return "panelinha.com.br"
-
-    def ingredients(self):
-        ingredients = self.soup.find("h5", string="Ingredientes").nextSibling.findAll(
-            "li"
-        )
-
-        return [
-            normalize_string(ingredient.get_text().replace("\u00C2", ""))
-            for ingredient in ingredients
-        ]
-
-    def instructions(self):
-        instructions = self.soup.find(
-            "h5", string="Modo de preparo"
-        ).nextSibling.findAll("li")
-
-        instructions = [
-            normalize_string(instruction.get_text()) for instruction in instructions
-        ]
-
-        # Some recipes pages have a different html structure.
-        if not instructions:
-            instructions = self.soup.find(
-                "h4", string="Modo de preparo"
-            ).nextSibling.p.strings
-
-            instructions = (
-                normalize_string(instruction) for instruction in instructions
-            )
-
-            instructions = map(
-                lambda step: INSTRUCTIONS_NUMBERING_REGEX.sub("", step), instructions
-            )
-
-        return "\n".join(instructions)
 
     def yields(self):
         main_element = self.soup.find("main")
