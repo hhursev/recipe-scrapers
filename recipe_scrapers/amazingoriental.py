341ca4cbbe16fe3fea8f294d9cbff546f1909546
Reyo Stallenberg <reyostallenberg@gmail.com>
2025-04-18 11:16:26 +0200

    Corrections on amazingoriental.com scraper
    Co-authored-by: Joey <7505194+jknndy@users.noreply.github.com>

--- recipe_scrapers/amazingoriental.py
+++ recipe_scrapers/amazingoriental.py
@@ -1,5 +1,5 @@
 from ._abstract import AbstractScraper
-
+from ._utils import get_minutes, get_yields
 
 class AmazingOriental(AbstractScraper):
     @classmethod
@@ -13,18 +13,18 @@ class AmazingOriental(AbstractScraper):
         return self.soup.select("div.recipe-info .meal-type")[0].get_text()
 
     def total_time(self):
-        return self.soup.select("dl.prepare-time dd")[0].get_text()
+        return get_minutes(self.soup.select("dl.prepare-time dd")[0].get_text())
 
     def yields(self):
-        return self.soup.select(".person-amount")[0].get_text()
+        return get_yields(self.soup.select(".person-amount")[0].get_text())
 
     def ingredients(self):
         ingredients = self.soup.select(".sidebar .ingredients-wrap ul li")
-        return [ingredient.get_text() for ingredient in ingredients]
+        return [' '.join(ingredient.get_text().split()) for ingredient in ingredients]
 
     def instructions(self):
         instructions = self.soup.select(".main-wrap ul li")
-        return [instruction.get_text() for instruction in instructions]
+        return "\n".join(instruction.get_text() for instruction in instructions)
 
     def cuisine(self):
         return self.soup.select("div.sidebar span.category")[0].get_text()
