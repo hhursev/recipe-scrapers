cc7b7f1b7b69951b05e4fca96e738f1960e052a9
Joey <7505194+jknndy@users.noreply.github.com>
2025-04-18 16:52:25 -0400

    Adds support for bakewithzoha

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -66,6 +66,7 @@ from .arla import Arla
 from .atelierdeschefs import AtelierDesChefs
 from .averiecooks import AverieCooks
 from .bakels import Bakels
+from .bakewithzoha import BakeWithZoha
 from .bakingmischief import BakingMischief
 from .bakingsense import BakingSense
 from .barefeetinthekitchen import BarefeetInTheKitchen
@@ -505,6 +506,7 @@ SCRAPERS = {
     BBCFood.host(): BBCFood,
     BBCFood.host(domain="co.uk"): BBCFood,
     BBCGoodFood.host(): BBCGoodFood,
+    BakeWithZoha.host(): BakeWithZoha,
     Bakels.host(): Bakels,
     Bakels.host(domain="co.uk"): Bakels,
     BakingSense.host(): BakingSense,
