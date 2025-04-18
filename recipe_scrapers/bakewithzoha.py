c15247e08d3ef23e4f41568cf3834fc3c55d20ba
Joey <7505194+jknndy@users.noreply.github.com>
2025-04-18 17:02:26 -0400

    Ingredient fix

--- recipe_scrapers/bakewithzoha.py
+++ recipe_scrapers/bakewithzoha.py
@@ -1,11 +1,21 @@
+import re
 from ._abstract import AbstractScraper
 from ._grouping_utils import group_ingredients
+from ._utils import normalize_string
 
 
 class BakeWithZoha(AbstractScraper):
     @classmethod
     def host(cls):
         return "bakewithzoha.com"
+
+    def ingredients(self):
+        return [
+            normalize_string(
+                re.sub(r"\(\s*(.*?)\s*\)", r"(\1)", li.get_text(" ", strip=True))
+            )
+            for li in self.soup.select(".tasty-recipes-ingredients li")
+        ]
 
     def ingredient_groups(self):
         return group_ingredients(
