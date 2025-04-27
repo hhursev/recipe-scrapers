fb9d724009103803f36d4b56a0470922f2d89fa8
Bryan Brady <bryan.brady@gmail.com>
2025-04-26 23:49:59 -0700

    updates per PR review

--- recipe_scrapers/thepioneerwoman.py
+++ recipe_scrapers/thepioneerwoman.py
@@ -18,6 +18,8 @@ class ThePioneerWoman(AbstractScraper):
     def instructions(self):
         instructions = self.schema.instructions()
         if instructions == "":
-            instructions = self.soup.select_one(".directions").get_text()
+            instructions_element = self.soup.select_one(".directions")
+            if instructions_element:
+                instructions = instructions_element.get_text(separator="\n")
         return instructions
 
