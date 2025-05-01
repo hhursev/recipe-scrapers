47a1f0b158415cdb4a3f82cd61b31ff6af774ce8
Hristo Harsev <hhursev@gmail.com>
2025-05-01 23:02:57 +0300

    Fix linter issuers with tastinghistory post merge

--- recipe_scrapers/tastinghistory.py
+++ recipe_scrapers/tastinghistory.py
@@ -1,6 +1,6 @@
 from ._abstract import AbstractScraper
 from ._grouping_utils import group_ingredients
-from bs4 import BeautifulSoup
+
 
 class TastingHistory(AbstractScraper):
     @classmethod
@@ -19,7 +19,7 @@ class TastingHistory(AbstractScraper):
     def category(self):
         tags = self.soup.find_all("a", {"class": "blog-item-tag"})
         if tags:
-            return ', '.join([tag.text.strip() for tag in tags])
+            return ", ".join([tag.text.strip() for tag in tags])
         return None
 
     def total_time(self):
@@ -34,7 +34,9 @@ class TastingHistory(AbstractScraper):
 
     def ingredients(self):
         ingredients = []
-        ingredients_lists = self.soup.select("div.sqs-col-4.span-4 .sqs-block-content ul")
+        ingredients_lists = self.soup.select(
+            "div.sqs-col-4.span-4 .sqs-block-content ul"
+        )
         if ingredients_lists:
             for ingredients_list in ingredients_lists:
                 for li in ingredients_list.find_all("li"):
@@ -51,7 +53,9 @@ class TastingHistory(AbstractScraper):
 
     def instructions(self):
         instructions = []
-        instructions_list = self.soup.select_one("div.sqs-col-8.span-8 .sqs-block-content ol")
+        instructions_list = self.soup.select_one(
+            "div.sqs-col-8.span-8 .sqs-block-content ol"
+        )
         if instructions_list:
             for li in instructions_list.find_all("li"):
                 instructions.append(li.text.strip())
