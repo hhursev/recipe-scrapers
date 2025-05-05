999b1d85ae4ac478e16a6c01d33b6f6d29a3a91e
Bryan Brady <bryan.brady@gmail.com>
2025-05-04 21:15:43 -0700

    Add support for justinesnacks.com

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -224,6 +224,7 @@ from .juliegoodwin import JulieGoodwin
 from .jumbo import Jumbo
 from .justataste import JustATaste
 from .justbento import JustBento
+from .justinesnacks import JustineSnacks
 from .justonecookbook import JustOneCookbook
 from .kalejunkie import KaleJunkie
 from .kellyscleankitchen import KellysCleanKitchen
@@ -597,6 +598,7 @@ SCRAPERS = {
     JoshuaWeissman.host(): JoshuaWeissman,
     JoyTheBaker.host(): JoyTheBaker,
     Jumbo.host(): Jumbo,
+    JustineSnacks.host(): JustineSnacks,
     KaleJunkie.host(): KaleJunkie,
     KellysCleanKitchen.host(): KellysCleanKitchen,
     KitchenAidAustralia.host(): KitchenAidAustralia,
