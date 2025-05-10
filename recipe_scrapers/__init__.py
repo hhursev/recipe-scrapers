9811913bbc6efa182b012bf4d0f57d2ea0958e72
Bryan Brady <bryan.brady@gmail.com>
2025-05-09 21:51:24 -0700

    Add support for bakerbynature.com

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -70,6 +70,7 @@ from .bakels import Bakels
 from .bakewithzoha import BakeWithZoha
 from .bakerbynature import BakerByNature
 from .bakewithzoha import BakeWithZoha
+from .bakerbynature import BakerByNature
 from .bakingmischief import BakingMischief
 from .bakingsense import BakingSense
 from .barefeetinthekitchen import BarefeetInTheKitchen
