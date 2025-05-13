db758cf35b53518d82410c081e5ecb6bd5b51a20
Bryan Brady <bryan.brady@gmail.com>
2025-05-13 15:16:59 -0700

    Add support for justalittlebitofbacon.com (#1571)

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -233,6 +233,7 @@ from .joyfoodsunshine import Joyfoodsunshine
 from .joythebaker import JoyTheBaker
 from .juliegoodwin import JulieGoodwin
 from .jumbo import Jumbo
+from .justalittlebitofbacon import JustALittleBitOfBacon
 from .justataste import JustATaste
 from .justbento import JustBento
 from .justinesnacks import JustineSnacks
@@ -637,6 +638,7 @@ SCRAPERS = {
     JoshuaWeissman.host(): JoshuaWeissman,
     JoyTheBaker.host(): JoyTheBaker,
     Jumbo.host(): Jumbo,
+    JustALittleBitOfBacon.host(): JustALittleBitOfBacon,
     JustineSnacks.host(): JustineSnacks,
     KaleJunkie.host(): KaleJunkie,
     KellysCleanKitchen.host(): KellysCleanKitchen,
