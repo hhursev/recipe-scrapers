f38b820829adb77331f9182b33a6157e12142610
Bryan Brady <bryan.brady@gmail.com>
2025-05-13 15:02:30 -0700

    Add support for corriecooks.com (#1574)

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -123,6 +123,7 @@ from .cooksillustrated import CooksIllustrated
 from .cooktalk import CookTalk
 from .cookwell import CookWell
 from .copykat import CopyKat
+from .corriecooks import CorrieCooks
 from .costco import Costco
 from .countryliving import CountryLiving
 from .creativecanning import CreativeCanning
@@ -585,6 +586,7 @@ SCRAPERS = {
     CooksCountry.host(): CooksCountry,
     CooksIllustrated.host(): CooksIllustrated,
     CopyKat.host(): CopyKat,
+    CorrieCooks.host(): CorrieCooks,
     Costco.host(): Costco,
     CountryLiving.host(): CountryLiving,
     CreativeCanning.host(): CreativeCanning,
