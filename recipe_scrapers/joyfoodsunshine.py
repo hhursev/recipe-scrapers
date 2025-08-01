06ab64ffed531eb5272106f6d1224565c23b452d
Joey <7505194+jknndy@users.noreply.github.com>
2025-08-01 18:37:57 -0400

    update scrapers

--- recipe_scrapers/joyfoodsunshine.py
+++ recipe_scrapers/joyfoodsunshine.py
@@ -1,8 +1,9 @@
 from ._abstract import AbstractScraper
-from ._utils import get_equipment, normalize_string
+from ._wprm import WPRMMixin
+from ._utils import normalize_string
 
 
-class Joyfoodsunshine(AbstractScraper):
+class Joyfoodsunshine(WPRMMixin, AbstractScraper):
     @classmethod
     def host(cls):
         return "joyfoodsunshine.com"
@@ -11,11 +12,3 @@ class Joyfoodsunshine(AbstractScraper):
         ingredients = self.soup.findAll("li", {"class": "wprm-recipe-ingredient"})
 
         return [normalize_string(ingredient.get_text()) for ingredient in ingredients]
-
-    def equipment(self):
-        equipment_items = [
-            text
-            for equip in self.soup.find_all("div", class_="wprm-recipe-equipment-name")
-            if (text := equip.get_text())
-        ]
-        return get_equipment(equipment_items)
