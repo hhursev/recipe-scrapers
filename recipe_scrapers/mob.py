64db37e01ea297db55232594a51dbf037fc27611
Joey <7505194+jknndy@users.noreply.github.com>
2025-08-01 17:54:12 -0400

    author rework and remove custom override

--- recipe_scrapers/mob.py
+++ recipe_scrapers/mob.py
@@ -1,20 +1,11 @@
-import json
-
 from ._abstract import AbstractScraper
 
 
 class Mob(AbstractScraper):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.recipe_json = json.loads(
-            self.soup.find("script", {"id": "__NEXT_DATA__"}).get_text()
-        )["props"]["pageProps"]["recipe"]
-
     @classmethod
     def host(cls):
         return "mob.co.uk"
 
     def author(self):
-        chefs = self.recipe_json.get("chefs", [])
-        return " & ".join([chef["title"] for chef in chefs]) if chefs else "Mob Team"
+        author_tag = self.soup.select_one("div.RecipeHero__detailsContainer h3")
+        return author_tag.get_text(strip=True)
