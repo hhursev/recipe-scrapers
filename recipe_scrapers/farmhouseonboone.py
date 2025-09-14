72285b207918eb35549bf2a7352ab65584378d48
Joey <7505194+jknndy@users.noreply.github.com>
2025-09-13 20:51:22 -0400

    none-group and equip

--- recipe_scrapers/farmhouseonboone.py
+++ recipe_scrapers/farmhouseonboone.py
@@ -5,3 +5,14 @@ class FarmhouseOnBoone(AbstractScraper):
     @classmethod
     def host(cls):
         return "farmhouseonboone.com"
+
+    def equipment(self):
+        return list(
+            dict.fromkeys(
+                (equip.find("a").get_text())
+                for equip in self.soup.find_all(
+                    "div", class_="wprm-recipe-equipment-name"
+                )
+                if equip.find("a") and equip.find("a").get_text()
+            )
+        )
