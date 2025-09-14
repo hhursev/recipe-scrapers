69e178c07d53f4bb49403baa933aa7b9fa4fa1bc
Joey <7505194+jknndy@users.noreply.github.com>
2025-09-13 20:17:58 -0400

    Grouping fix

--- recipe_scrapers/ohsweetbasil.py
+++ recipe_scrapers/ohsweetbasil.py
@@ -1,4 +1,5 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 
 
 class OhSweetBasil(AbstractScraper):
@@ -10,12 +11,24 @@ class OhSweetBasil(AbstractScraper):
         return [
             " ".join(
                 span.get_text(strip=True)
-                for span in i.select(
+                for span in ingredient.select(
                     ".wprm-recipe-ingredient-amount, "
                     ".wprm-recipe-ingredient-unit, "
                     ".wprm-recipe-ingredient-name, "
                     ".wprm-recipe-ingredient-notes"
                 )
+                if span.get_text(strip=True)
+            ).strip()
+            for ingredient in self.soup.select(
+                ".wprm-recipe-ingredient-group li.wprm-recipe-ingredient"
             )
-            for i in self.soup.select(".wprm-recipe-ingredient")
+            if ingredient.select_one(".wprm-recipe-ingredient-name")
         ]
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".wprm-recipe-ingredient-group h4",
+            ".wprm-recipe-ingredient-group li.wprm-recipe-ingredient:has(.wprm-recipe-ingredient-name)",
+        )
