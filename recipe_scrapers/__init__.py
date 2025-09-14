e988b50985283c4087d3015377f671008300f8ff
Joey <7505194+jknndy@users.noreply.github.com>
2025-09-13 21:11:58 -0400

    Adds support for ourbestbites (#1703)

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -356,6 +356,7 @@ from .onehundredonecookbooks import OneHundredOneCookBooks
 from .onesweetappetite import OneSweetAppetite
 from .organicallyaddison import OrganicallyAddison
 from .ottolenghibooks import OttolenghiBooks
+from .ourbestbites import OurBestBites
 from .owenhan import OwenHan
 from .paleorunningmomma import PaleoRunningMomma
 from .panelinha import Panelinha
@@ -884,6 +885,7 @@ SCRAPERS = {
     OneSweetAppetite.host(): OneSweetAppetite,
     OrganicallyAddison.host(): OrganicallyAddison,
     OttolenghiBooks.host(): OttolenghiBooks,
+    OurBestBites.host(): OurBestBites,
     OwenHan.host(): OwenHan,
     PaleoRunningMomma.host(): PaleoRunningMomma,
     Panelinha.host(): Panelinha,
