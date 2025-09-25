8203f6b859b3662464de8e72a1afb9f0fa96603f
Ruben172 <rubenthetoad@gmail.com>
2025-09-25 18:59:26 +0200

    Squashed commit of the following:
    commit 27296eba5133a7189f53d4dddbb074c2166f0c85
    Author: Ruben172 <rubenthetoad@gmail.com>
    Date:   Thu Sep 25 18:59:16 2025 +0200
    
        format
    
    commit 620770d2d5477aba72fb20a6e176ea2d1c9904a7
    Author: Ruben172 <rubenthetoad@gmail.com>
    Date:   Thu Sep 25 18:59:05 2025 +0200
    
        fix

--- recipe_scrapers/picnic.py
+++ recipe_scrapers/picnic.py
@@ -1,35 +1,84 @@
 from ._abstract import AbstractScraper
 from ._utils import normalize_string
+import re
+import json
 
 
 class Picnic(AbstractScraper):
+    def __init__(self, html: str, url: str):
+        super().__init__(html, url)
+        self.recipe_data = None
+        script_tag = self.soup.find("script", string=re.compile(r'\{\\"recipe\\":\{\\"id\\"'))
+
+        if recipe_data := re.search(r'({\\"recipe\\":{.*locale.*?})', script_tag.string):
+            recipe_match = recipe_data.group(1).encode("utf-8").decode("unicode_escape")
+            fixed_recipe = recipe_match.encode("latin1").decode("utf-8")  # Fix for encoding issues
+            self.recipe_data = json.loads(fixed_recipe)["recipe"]
+
     @classmethod
     def host(cls):
         return "picnic.app"
 
     def instructions(self):
-        # Picnic names the steps "Schritt 1" ("step 1") and the current verison of
-        # instructions() add the name and the text as separate steps.
-        # That's why we extract the steps here manually to skip this.
         new_instructions = []
-        for instruction in self.schema.data.get("recipeInstructions"):
-            new_instructions.append(instruction["text"])
+
+        recipe_data = self.recipe_data
+        if recipe_data is None:
+            return None
+
+        for instruction in recipe_data["preparationInstructions"]:
+            new_instructions.append(instruction["body"])
         return "\n".join(
             normalize_string(instruction) for instruction in new_instructions
         )
 
-    def site_name(self):
-        # Picnic recipes have the site name as author and mentioned nowhere else alone
-        # That's why we return author here
-        return self.author()
+    def ingredients(self):
+        new_ingredients = []
 
-    def canonical_url(self):
-        # Picnic "hides" the canonical recipe URL in the instructions, so we grab it from there
-        # As it's only relative to the root, we have to combine it with https:// and the host.
-        instructions = self.schema.data.get("recipeInstructions")
-        return "https://{}{}".format(self.host(), instructions[0]["url"])
+        recipe_data = self.recipe_data
+        if recipe_data is None:
+            return None
+
+        for ingredient in recipe_data["ingredients"]:
+            qty = ingredient["displayIngredientQuantity"]
+            unit = ingredient["displayUnitOfMeasurement"]
+            name = ingredient["name"]
+            qty = f"{qty} " if qty is not None else ""
+            unit = f"{unit} " if unit is not None else ""
+            new_ingredients.append(f"{qty}{unit}{name}")
+        return [normalize_string(ingredient) for ingredient in new_ingredients]
+
+    def author(self):
+        return "Picnic"
+
+    def site_name(self):
+        return "Picnic"
+
+    def title(self):
+        if recipe_data := self.recipe_data:
+            return recipe_data["name"]
+        return None
+
+    def description(self):
+        if recipe_data := self.recipe_data:
+            return recipe_data["description"]
+        return None
+
+    def total_time(self):
+        if recipe_data := self.recipe_data:
+            return recipe_data["preparationTimeInMinutes"]
+        return None
+
+    def prep_time(self):
+        if recipe_data := self.recipe_data:
+            return recipe_data["activePreparationTimeInMinutes"]
+        return None
+
+    def yields(self):
+        # not specified on the website, app seems to default to four servings with the same ingredients
+        return "4 servings"
 
     def language(self):
-        # The recipe's language is encoded in the URL, directly after the host name
-        # e.g. https://picnic.app/de/ or https://picnic.app/en/
-        return self.canonical_url()[19:21]
+        if recipe_data := self.recipe_data:
+            return recipe_data["locale"]
+        return None
