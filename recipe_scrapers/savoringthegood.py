e260ea25296af26e0e08149ae41ac8211ecf4ff7
Joey <7505194+jknndy@users.noreply.github.com>
2025-09-26 16:05:14 -0400

    Adds support for savoringthegood

---
+++ recipe_scrapers/savoringthegood.py
@@ -0,0 +1,16 @@
+from ._abstract import AbstractScraper
+from ._utils import get_equipment
+
+
+class SavoringTheGood(AbstractScraper):
+    @classmethod
+    def host(cls):
+        return "savoringthegood.com"
+
+    def equipment(self):
+        equipment_items = [
+            text
+            for equip in self.soup.find_all("div", class_="wprm-recipe-equipment-name")
+            if (text := equip.get_text())
+        ]
+        return get_equipment(equipment_items)
