0ee1e0b0f6ff85145e717cefbc568c3986a563b1
Joey <7505194+jknndy@users.noreply.github.com>
2025-10-04 15:32:23 -0400

    Adds support for receptiindex

---
+++ recipe_scrapers/receptiindex.py
@@ -0,0 +1,23 @@
+from ._abstract import AbstractScraper
+
+
+class ReceptiIndex(AbstractScraper):
+    @classmethod
+    def host(cls):
+        return "recepti.index.hr"
+
+    def instructions(self):
+        container = self.soup.select_one(
+            "div.preparation-step-new__text div.html-container"
+        )
+        if not container:
+            return
+
+        instructions = []
+
+        for tag in container.find_all(["p"]):
+            text = tag.get_text(" ", strip=True)
+            if text:
+                instructions.append(text)
+
+        return "\n".join(instructions)
