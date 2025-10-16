4ac1f6dde76c9a539fe62c73654d0b3a7ee359c2
Joey <7505194+jknndy@users.noreply.github.com>
2025-10-16 18:33:48 -0400

    groups

--- recipe_scrapers/americastestkitchen.py
+++ recipe_scrapers/americastestkitchen.py
@@ -1,4 +1,5 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 
 
 class AmericasTestKitchen(AbstractScraper):
@@ -11,3 +12,11 @@ class AmericasTestKitchen(AbstractScraper):
         return [
             ingredient.replace(" ,", ",") for ingredient in self.schema.ingredients()
         ]
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            "h3.mise-text",
+            'span[class^="recipePrintBody_ingredient__"]',
+        )
