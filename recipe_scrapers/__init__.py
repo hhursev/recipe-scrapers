14781a1390a1e922c30e88dcb4e7271b46c98e71
Joey <7505194+jknndy@users.noreply.github.com>
2025-10-17 18:39:11 -0400

    Adds support for polishfoodies (#1739)

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -379,6 +379,7 @@ from .pingodoce import PingoDoce
 from .pinkowlkitchen import PinkOwlKitchen
 from .platingpixels import PlatingPixels
 from .plowingthroughlife import PlowingThroughLife
+from .polishfoodies import PolishFoodies
 from .poppycooks import PoppyCooks
 from .popsugar import PopSugar
 from .potatorolls import PotatoRolls
@@ -921,6 +922,7 @@ SCRAPERS = {
     PinkOwlKitchen.host(): PinkOwlKitchen,
     PlatingPixels.host(): PlatingPixels,
     PlowingThroughLife.host(): PlowingThroughLife,
+    PolishFoodies.host(): PolishFoodies,
     PoppyCooks.host(): PoppyCooks,
     PopSugar.host(): PopSugar,
     PotatoRolls.host(): PotatoRolls,
