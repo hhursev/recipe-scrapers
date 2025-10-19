df9bff2a5e51a14da099ee8fdf08586955a933f5
Joey <7505194+jknndy@users.noreply.github.com>
2025-10-19 16:49:38 -0400

    equipment

--- recipe_scrapers/thesuburbansoapbox.py
+++ recipe_scrapers/thesuburbansoapbox.py
@@ -5,3 +5,16 @@ class TheSuburbanSoapBox(AbstractScraper):
     @classmethod
     def host(cls):
         return "thesuburbansoapbox.com"
+
+    def equipment(self):
+        equipment = []
+        seen = set()
+        for equip_div in self.soup.find_all("div", class_="wprm-recipe-equipment-name"):
+            if equip_div.find("a"):
+                name = equip_div.find("a").get_text(strip=True)
+            else:
+                name = equip_div.get_text(strip=True)
+            if name and name not in seen:
+                equipment.append(name)
+                seen.add(name)
+        return equipment
