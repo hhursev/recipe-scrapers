6b87a827dd450f15384c0373358e9f89634bc2ba
Joey <7505194+jknndy@users.noreply.github.com>
2025-10-19 16:36:27 -0400

    equipment

--- recipe_scrapers/littleferrarokitchen.py
+++ recipe_scrapers/littleferrarokitchen.py
@@ -5,3 +5,14 @@ class LittleFerraroKitchen(AbstractScraper):
     @classmethod
     def host(cls):
         return "littleferrarokitchen.com"
+
+    def equipment(self):
+        return list(
+            dict.fromkeys(
+                (equip.find("a").get_text())
+                for equip in self.soup.find_all(
+                    "div", class_="wprm-recipe-equipment-name"
+                )
+                if equip.find("a") and equip.find("a").get_text()
+            )
+        )
