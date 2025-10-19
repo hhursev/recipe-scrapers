446a869c0bd5d4484a25fa93a443d2544978a5ef
Joey <7505194+jknndy@users.noreply.github.com>
2025-10-19 16:44:49 -0400

    equipment

--- recipe_scrapers/realmomnutrition.py
+++ recipe_scrapers/realmomnutrition.py
@@ -14,3 +14,11 @@ class RealMomNutrition(AbstractScraper):
             ".mv-create-ingredients h4",
             ".mv-create-ingredients ul li",
         )
+
+    def equipment(self):
+        return [
+            item.get_text(strip=True)
+            for item in self.soup.select(
+                "ul.mv-create-products-list div.mv-create-products-product-name"
+            )
+        ]
