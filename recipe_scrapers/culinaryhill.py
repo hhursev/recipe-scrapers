a5426d8314c887e4f396faa0ef246432f39fdd04
Joey <7505194+jknndy@users.noreply.github.com>
2025-10-30 18:09:21 -0400

    ingredients improvments

--- recipe_scrapers/culinaryhill.py
+++ recipe_scrapers/culinaryhill.py
@@ -7,6 +7,14 @@ class CulinaryHill(AbstractScraper):
     def host(cls):
         return "culinaryhill.com"
 
+    def ingredients(self):
+        return [
+            li.get_text(" ", strip=True).replace("▢", "").strip()
+            for li in self.soup.select(
+                "ul.wprm-recipe-ingredients > li.wprm-recipe-ingredient"
+            )
+        ]
+
     def equipment(self):
         equipment_items = [
             text
