8e61a3b39c2337666976a63ceab306ad653f775d
Joey <7505194+jknndy@users.noreply.github.com>
2025-11-12 07:18:28 -0500

    Equipment WPRM mixin (#1653)
    * Create _wprm.py
    
    * update scrapers
    
    * test updates
    
    * New Scrapers

--- recipe_scrapers/amazingribs.py
+++ recipe_scrapers/amazingribs.py
@@ -1,15 +1,8 @@
 from ._abstract import AbstractScraper
-from ._utils import get_equipment, normalize_string
+from ._wprm import WPRMMixin
 
 
-class AmazingRibs(AbstractScraper):
+class AmazingRibs(WPRMMixin, AbstractScraper):
     @classmethod
     def host(cls):
         return "amazingribs.com"
-
-    def equipment(self):
-        equipment_items = [
-            normalize_string(e.get_text())
-            for e in self.soup.find_all("div", class_="wprm-recipe-equipment-name")
-        ]
-        return sorted(get_equipment(equipment_items))
