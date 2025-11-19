d1e1d13f0fd1ab1ec57158dfd60929c53de6ca2a
Joey <7505194+jknndy@users.noreply.github.com>
2025-11-19 17:03:12 -0500

    Adds support for sugarspunrun (#1776)

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -480,6 +480,7 @@ from .streetkitchen import StreetKitchen
 from .strongrfastr import StrongrFastr
 from .sudachirecipes import SudachiRecipes
 from .sugarhero import SugarHero
+from .sugarspunrun import SugarSpunRun
 from .sunbasket import SunBasket
 from .sundpaabudget import SundPaaBudget
 from .sunset import Sunset
@@ -1049,6 +1050,7 @@ SCRAPERS = {
     StrongrFastr.host(): StrongrFastr,
     SudachiRecipes.host(): SudachiRecipes,
     SugarHero.host(): SugarHero,
+    SugarSpunRun.host(): SugarSpunRun,
     SunBasket.host(): SunBasket,
     SundPaaBudget.host(): SundPaaBudget,
     Sunset.host(): Sunset,
