616f57d4ca30c3556f3d4cf14d0a7b2059284b33
Joey <7505194+jknndy@users.noreply.github.com>
2025-11-19 16:57:06 -0500

    Adds support for tastefullygrace

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -488,6 +488,7 @@ from .swissmilk import SwissMilk
 from .tableanddish import TableAndDish
 from .tasteatlas import TasteAtlas
 from .tasteau import TasteAU
+from .tastefullygrace import TastefullyGrace
 from .tasteofhome import TasteOfHome
 from .tastesbetterfromscratch import TastesBetterFromScratch
 from .tastesoflizzyt import TastesOfLizzyT
@@ -1056,6 +1057,7 @@ SCRAPERS = {
     TableAndDish.host(): TableAndDish,
     TasteAtlas.host(): TasteAtlas,
     TasteAU.host(): TasteAU,
+    TastefullyGrace.host(): TastefullyGrace,
     TasteOfHome.host(): TasteOfHome,
     TastesBetterFromScratch.host(): TastesBetterFromScratch,
     TastesOfLizzyT.host(): TastesOfLizzyT,
