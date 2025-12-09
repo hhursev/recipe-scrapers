a89331806d09b68e6bf9bed2641b39291fa36133
Joey <7505194+jknndy@users.noreply.github.com>
2025-12-09 14:29:47 -0500

    Adds support for goldnplump (#1793)
    * Schema
    
    * ingredients
    
    * refactor ingredients and add groups
    
    * small ingredient refactor
    
    * total_time
    
    * edge case
    
    * author

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -220,6 +220,7 @@ from .globo import Globo
 from .gloriousrecipes import GloriousRecipes
 from .glutenfreeonashoestring import GlutenFreeOnAShoeString
 from .godt import Godt
+from .goldnplump import GoldnPlump
 from .gonnawantseconds import GonnaWantSeconds
 from .goodfooddiscoveries import GoodFoodDiscoveries
 from .goodhousekeeping import GoodHousekeeping
@@ -779,6 +780,7 @@ SCRAPERS = {
     GloriousRecipes.host(): GloriousRecipes,
     GlutenFreeOnAShoeString.host(): GlutenFreeOnAShoeString,
     Godt.host(): Godt,
+    GoldnPlump.host(): GoldnPlump,
     GonnaWantSeconds.host(): GonnaWantSeconds,
     GoodFoodDiscoveries.host(): GoodFoodDiscoveries,
     GoodHousekeeping.host(): GoodHousekeeping,
