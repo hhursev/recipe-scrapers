5ef1d6e7d8031bed283b1139998d07fbe2bfd597
Joey <7505194+jknndy@users.noreply.github.com>
2026-03-01 14:01:00 -0500

    Adds support for blogghetti

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -100,6 +100,7 @@ from .bigoven import BigOven
 from .billyparisi import BillyParisi
 from .bitsofcarey import BitsOfCarey
 from .blessthismessplease import BlessThisMessPlease
+from .blogghetti import Blogghetti
 from .blueapron import BlueApron
 from .bluejeanchef import BlueJeanChef
 from .bodybuilding import Bodybuilding
@@ -692,6 +693,7 @@ SCRAPERS = {
     BillyParisi.host(): BillyParisi,
     BitsOfCarey.host(): BitsOfCarey,
     BlessThisMessPlease.host(): BlessThisMessPlease,
+    Blogghetti.host(): Blogghetti,
     BlueApron.host(): BlueApron,
     BlueJeanChef.host(): BlueJeanChef,
     Bodybuilding.host(): Bodybuilding,
