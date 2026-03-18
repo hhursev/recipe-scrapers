656cd70e46d39011ce31576fb552fa2bb3b56232
Joey <7505194+jknndy@users.noreply.github.com>
2026-03-18 16:03:03 -0400

    groupings

--- recipe_scrapers/tasteofhome.py
+++ recipe_scrapers/tasteofhome.py
@@ -1,7 +1,26 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 
 
 class TasteOfHome(AbstractScraper):
     @classmethod
     def host(cls):
         return "tasteofhome.com"
+
+    def ingredients(self):
+        ingredient_elements = self.soup.select(
+            ".ingredient-item:not(.sub-ingredient-title)"
+        )
+        ingredients = []
+        for ingredient in ingredient_elements:
+            ingredient_text = ingredient.get_text(strip=True)
+            ingredients.append(ingredient_text)
+        return ingredients
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".ingredient-item.sub-ingredient-title",
+            ".ingredients-list > .ingredient-item:not(.sub-ingredient-title), .sub-ingredients .ingredient-item",
+        )
