18e71db849e4c989acf19b15d33c871798db3dde
Joey <7505194+jknndy@users.noreply.github.com>
2026-03-18 16:30:46 -0400

    Update postpunkkitchen.py

--- recipe_scrapers/postpunkkitchen.py
+++ recipe_scrapers/postpunkkitchen.py
@@ -7,35 +7,5 @@ class PostPunkKitchen(AbstractScraper):
     def host(cls):
         return "theppk.com"
 
-    def author(self):
-        return self.schema.author()
-
-    def title(self):
-        return self.schema.title()
-
-    def category(self):
-        return self.schema.category()
-
     def total_time(self):
         raise FieldNotProvidedByWebsiteException(return_value=None)
-
-    def yields(self):
-        return self.schema.yields()
-
-    def image(self):
-        return self.schema.image()
-
-    def ingredients(self):
-        return self.schema.ingredients()
-
-    def instructions(self):
-        return self.schema.instructions()
-
-    def ratings(self):
-        return self.schema.ratings()
-
-    def cuisine(self):
-        return self.schema.cuisine()
-
-    def description(self):
-        return self.schema.description()
