cfc6652050b5c99613ba68728a802bc20d86950b
zdenek-stursa <90236729+zdenek-stursa@users.noreply.github.com>
2026-04-23 16:10:30 +0200

    Adds support for kalcirecept.hu

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -314,6 +314,7 @@ from .justataste import JustATaste
 from .justbento import JustBento
 from .justinesnacks import JustineSnacks
 from .justonecookbook import JustOneCookbook
+from .kalcirecept import KalciRecept
 from .kalejunkie import KaleJunkie
 from .kellyscleankitchen import KellysCleanKitchen
 from .kennethtemple import KennethTemple
@@ -946,6 +947,7 @@ SCRAPERS = {
     JustBento.host(): JustBento,
     JustineSnacks.host(): JustineSnacks,
     JustOneCookbook.host(): JustOneCookbook,
+    KalciRecept.host(): KalciRecept,
     KaleJunkie.host(): KaleJunkie,
     KellysCleanKitchen.host(): KellysCleanKitchen,
     KennethTemple.host(): KennethTemple,
