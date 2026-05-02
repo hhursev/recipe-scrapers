a16dcc923a6e84a09b48b6cfcfcf28711ffd4b45
Joey <7505194+jknndy@users.noreply.github.com>
2026-05-02 15:36:53 -0400

    `ingredients` override

--- recipe_scrapers/cdkitchen.py
+++ recipe_scrapers/cdkitchen.py
@@ -5,3 +5,12 @@ class CdKitchen(AbstractScraper):
     @classmethod
     def host(cls):
         return "cdkitchen.com"
+
+    def ingredients(self):
+        ingredients = []
+        for el in self.soup.select('p.ml-30 span[itemprop="recipeIngredient"]'):
+            if el.select_one("span.uline.big2"):
+                continue
+            text = " ".join(el.get_text(strip=True).split())
+            ingredients.append(text)
+        return ingredients
