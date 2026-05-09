6d59b88b64881b97ddae6deadcb6c75a0dbcb49c
riconem <enrico_nemitz@hotmail.de>
2026-05-09 13:04:44 +0200

    initial zuckerjagdwurst generation

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -642,6 +642,7 @@ from .zaubertopf import ZauberTopf
 from .zeitwochenmarkt import ZeitWochenmarkt
 from .zenbelly import ZenBelly
 from .zestfulkitchen import ZestfulKitchen
+from .zuckerjagdwurst import ZuckerJagdwurst
 
 SCRAPERS = {
     ABeautifulMess.host(): ABeautifulMess,
@@ -1284,6 +1285,7 @@ SCRAPERS = {
     ZeitWochenmarkt.host(): ZeitWochenmarkt,
     ZenBelly.host(): ZenBelly,
     ZestfulKitchen.host(): ZestfulKitchen,
+    ZuckerJagdwurst.host(): ZuckerJagdwurst,
 }
 
 
