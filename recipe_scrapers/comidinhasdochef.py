c0e47d71084ef67f6f2940606709bf737ca6c0d9
Joey <7505194+jknndy@users.noreply.github.com>
2026-05-12 12:08:05 -0400

    Remove step count

--- recipe_scrapers/comidinhasdochef.py
+++ recipe_scrapers/comidinhasdochef.py
@@ -14,3 +14,14 @@ class ComidinhasDoChef(AbstractScraper):
             ".content-text-shadow h2.subtitle",
             ".content-text-shadow ul li",
         )
+
+    def instructions(self):
+        instructions = self.schema.instructions()
+
+        filtered_instructions = [
+            line
+            for line in instructions.split("\n")
+            if not line.lower().startswith("passo")
+        ]
+
+        return "\n".join(filtered_instructions)
