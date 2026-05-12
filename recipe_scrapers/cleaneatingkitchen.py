a9394868f3b7c232ca1aa4cec93baf03ac3edb4f
Joey <7505194+jknndy@users.noreply.github.com>
2026-05-12 12:00:50 -0400

    Groups: cleaneatingkitchen

--- recipe_scrapers/cleaneatingkitchen.py
+++ recipe_scrapers/cleaneatingkitchen.py
@@ -1,7 +1,25 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 
 
 class CleanEatingKitchen(AbstractScraper):
     @classmethod
     def host(cls):
         return "cleaneatingkitchen.com"
+
+    def ingredients(self):
+        ingredients = [
+            ingredient.get_text(" ", strip=True)
+            for ingredient in self.soup.select(
+                ".tasty-recipes-ingredients ul li:not(:has(strong))"
+            )
+        ]
+        return ingredients
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".tasty-recipes-ingredients ul li:has(strong)",
+            ".tasty-recipes-ingredients ul li:not(:has(strong))",
+        )
