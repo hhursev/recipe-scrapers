bd3879a46c7826509a3e39d0e33ffb9c0f178e6f
Joey <7505194+jknndy@users.noreply.github.com>
2026-05-21 16:39:12 -0400

    groups and update test 1

--- recipe_scrapers/greatbritishchefs.py
+++ recipe_scrapers/greatbritishchefs.py
@@ -1,7 +1,21 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients, IngredientGroup
 
 
 class GreatBritishChefs(AbstractScraper):
     @classmethod
     def host(cls):
         return "greatbritishchefs.com"
+
+    def ingredient_groups(self):
+        groups = group_ingredients(
+            self.ingredients(),
+            self.soup,
+            "#recipe-ingredients-start div[id] > h4",
+            "#recipe-ingredients-start div[id] > ul > li",
+        )
+
+        if len(groups) == 1 and groups[0].purpose == self.title():
+            return [IngredientGroup(ingredients=self.ingredients())]
+
+        return groups
