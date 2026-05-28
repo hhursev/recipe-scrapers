3843a07be657564905b8feee10bae48d35a5fba5
Joey <7505194+jknndy@users.noreply.github.com>
2026-05-28 15:20:27 -0400

    Groups: keytomylime (#1979)

--- recipe_scrapers/keytomylime.py
+++ recipe_scrapers/keytomylime.py
@@ -1,7 +1,16 @@
 from ._abstract import AbstractScraper
+from ._grouping_utils import group_ingredients
 
 
 class KeyToMyLime(AbstractScraper):
     @classmethod
     def host(cls):
         return "keytomylime.com"
+
+    def ingredient_groups(self):
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".mv-create-ingredients h3",
+            ".mv-create-ingredients ul li",
+        )
