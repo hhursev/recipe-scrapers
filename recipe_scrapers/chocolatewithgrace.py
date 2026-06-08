d69310393027ae3ce14db7a96faada5794213da0
Joey <7505194+jknndy@users.noreply.github.com>
2026-06-08 17:35:21 -0400

    site_name

--- recipe_scrapers/chocolatewithgrace.py
+++ recipe_scrapers/chocolatewithgrace.py
@@ -1,7 +1,11 @@
 from ._abstract import AbstractScraper
+from ._exceptions import StaticValueException
 
 
 class ChocolateWithGrace(AbstractScraper):
     @classmethod
     def host(cls):
         return "chocolatewithgrace.com"
+
+    def site_name(self):
+        raise StaticValueException(return_value="Chocolate with Grace")
