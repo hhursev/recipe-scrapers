d9944c2370ba23a883051f2c736ff9f03802a170
Joey <7505194+jknndy@users.noreply.github.com>
2026-08-12 16:59:22 -0400

    Adds support for justapinch (#2039)

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -319,6 +319,7 @@ from .juliasalbum import JuliasAlbum
 from .juliegoodwin import JulieGoodwin
 from .jumbo import Jumbo
 from .justalittlebitofbacon import JustALittleBitOfBacon
+from .justapinch import JustAPinch
 from .justataste import JustATaste
 from .justbento import JustBento
 from .justinesnacks import JustineSnacks
@@ -1053,6 +1054,7 @@ SCRAPERS = {
     JulieGoodwin.host(): JulieGoodwin,
     Jumbo.host(): Jumbo,
     JustALittleBitOfBacon.host(): JustALittleBitOfBacon,
+    JustAPinch.host(): JustAPinch,
     JustATaste.host(): JustATaste,
     JustBento.host(): JustBento,
     JustineSnacks.host(): JustineSnacks,
