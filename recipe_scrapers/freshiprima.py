26755e98bfaae94552d564472d159fb7452d7281
Joey <7505194+jknndy@users.noreply.github.com>
2026-08-12 17:05:30 -0400

    groups

--- recipe_scrapers/freshiprima.py
+++ recipe_scrapers/freshiprima.py
@@ -1,5 +1,6 @@
 from ._abstract import AbstractScraper
 from ._utils import get_yields
+from ._grouping_utils import group_ingredients
 
 
 class FreshiPrima(AbstractScraper):
@@ -12,3 +13,14 @@ class FreshiPrima(AbstractScraper):
             ".recipe-main-points-molecule .main-point:has(.i-person)"
         )
         return get_yields(element)
+
+    def ingredient_groups(self):
+        if len(self.soup.select(".ingredients-box")) <= 1:
+            return super().ingredient_groups()
+
+        return group_ingredients(
+            self.ingredients(),
+            self.soup,
+            ".ingredients-box h3",
+            ".ingredients-box li",
+        )
