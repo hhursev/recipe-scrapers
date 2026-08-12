20307ceab2ea191e669b4ff0a479f28e47a8c4fe
Joey <7505194+jknndy@users.noreply.github.com>
2026-08-12 17:04:12 -0400

    yields

--- recipe_scrapers/freshiprima.py
+++ recipe_scrapers/freshiprima.py
@@ -1,7 +1,14 @@
 from ._abstract import AbstractScraper
+from ._utils import get_yields
 
 
 class FreshiPrima(AbstractScraper):
     @classmethod
     def host(cls):
         return "fresh.iprima.cz"
+
+    def yields(self):
+        element = self.soup.select_one(
+            ".recipe-main-points-molecule .main-point:has(.i-person)"
+        )
+        return get_yields(element)
