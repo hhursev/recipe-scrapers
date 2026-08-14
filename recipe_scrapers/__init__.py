9b43fa603475607a67f8774480db26d071e8336f
Joey <7505194+jknndy@users.noreply.github.com>
2026-08-14 19:36:43 -0400

    Adds support for tasteline (#2043)

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -574,6 +574,7 @@ from .tasteandtellblog import TasteAndTellBlog
 from .tasteatlas import TasteAtlas
 from .tasteau import TasteAU
 from .tastefullygrace import TastefullyGrace
+from .tasteline import Tasteline
 from .tasteofhome import TasteOfHome
 from .tastesbetterfromscratch import TastesBetterFromScratch
 from .tastesoflizzyt import TastesOfLizzyT
@@ -1314,6 +1315,7 @@ SCRAPERS = {
     TasteAtlas.host(): TasteAtlas,
     TasteAU.host(): TasteAU,
     TastefullyGrace.host(): TastefullyGrace,
+    Tasteline.host(): Tasteline,
     TasteOfHome.host(): TasteOfHome,
     TastesBetterFromScratch.host(): TastesBetterFromScratch,
     TastesOfLizzyT.host(): TastesOfLizzyT,
