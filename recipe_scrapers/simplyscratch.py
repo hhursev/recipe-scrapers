607c9889579f2dc8388581a5f684e61642eca1d4
Joey <7505194+jknndy@users.noreply.github.com>
2026-08-19 17:31:21 -0400

    equip

--- recipe_scrapers/simplyscratch.py
+++ recipe_scrapers/simplyscratch.py
@@ -1,7 +1,8 @@
 from ._abstract import AbstractScraper
+from ._wprm import WPRMMixin
 
 
-class SimplyScratch(AbstractScraper):
+class SimplyScratch(WPRMMixin, AbstractScraper):
     @classmethod
     def host(cls):
         return "simplyscratch.com"
